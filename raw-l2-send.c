d9ca27103135a1ca3b525ce14e78279aef141921
Vladimir Oltean <olteanv@gmail.com>
2019-10-08 02:14:04 +0300

    raw-l2-send: Fix SCHED_DEADLINE scheduler attributes
    Remove a previous workaround which makes sched_setattr not fail even if
    the user does not specify any --advance-time. That means the
    advance-time is implicitly zero, which is fundamentally wrong: there is
    no time for raw-l2-send to do its work, because it is already too late.
    Passing 0 to the sched_deadline would make the kernel validator return
    -EINVAL. So the workaround was to reverse the meaning of the advance
    time when specifying to the scheduler.
    
    The correct fix is to change the default advance-time value, from 0 to
    the length of the cycle itself. That has been done, so we can remove the
    workaround.
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- raw-l2-send.c
+++ raw-l2-send.c
@@ -155,8 +155,8 @@ static int prog_configure_rt(struct prog_data *prog)
 	struct sched_attr attr = {
 		.size = sizeof(struct sched_attr),
 		.sched_policy = SCHED_DEADLINE,
-		.sched_runtime = prog->cycle_time - prog->advance_time,
-		.sched_deadline = prog->cycle_time - prog->advance_time,
+		.sched_runtime = prog->advance_time,
+		.sched_deadline = prog->advance_time,
 		.sched_period = prog->cycle_time,
 	};
 	int rc;
