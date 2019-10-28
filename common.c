NXP/isochron

commit 17c848f043f312faa4fd77339f4605635407a288

raw-l2-rcv: Avoid truncation on last output line when killed
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

raw-l2-rcv.c
@@ -14,6 +14,7 @@
 #include <net/if.h>
 #include <netinet/ether.h>
 #include <unistd.h>
+#include <signal.h>
 #include <errno.h>
 #include <time.h>
 #include "common.h"
@@ -35,6 +36,8 @@ struct app_private {
 struct app_header {
 	short seqid;
 };
+
+int signal_received = 0;
 
 #define app_fmt \
 	"[%ld.%09ld] src %02x:%02x:%02x:%02x:%02x:%02x dst %02x:%02x:%02x:%02x:%02x:%02x ethertype 0x%04x seqid %d rxtstamp %ld.%09ld\n"
@@ -158,7 +161,14 @@ static int server_loop(struct prog_data *prog, void *app_data)
 		rc = app_loop(app_data, prog->rcvbuf, len, &hwts);
 		if (rc < 0)
 			break;
+		if (signal_received)
+			break;
 	} while (1);
+
+	/* Avoid the nanosecond portion of last output line
+	 * from getting truncated when process is killed
+	 */
+	fflush(stdout);
 
 	close(prog->fd);
 
@@ -175,11 +185,33 @@ static void app_init(void *data)
 	priv->clkid = CLOCK_REALTIME;
 }
 
+void sig_handler(int signo)
+{
+	switch (signo) {
+	case SIGTERM:
+	case SIGINT:
+		signal_received = 1;
+		break;
+	default:
+		break;
+	}
+}
+
 static int prog_init(struct prog_data *prog)
 {
 	struct sockaddr_ll addr;
 	int sockopt = 1;
 	int rc;
+
+	if (signal(SIGTERM, sig_handler) == SIG_ERR) {
+		fprintf(stderr, "can't catch SIGTERM: %s\n", strerror(errno));
+		return -errno;
+	}
+
+	if (signal(SIGINT, sig_handler) == SIG_ERR) {
+		fprintf(stderr, "can't catch SIGINT: %s\n", strerror(errno));
+		return -errno;
+	}
 
 	prog->if_index = if_nametoindex(prog->if_name);
 	if (!prog->if_index) {

commit 3303f8c87c2780d7d822ff78c0e4fcb9575ee38b

raw-l2-rcv: Fix signal handling
Replace the simpler signal() function with sigaction(), because signal()
makes all system calls by default re-entrable upon a signal, which is
exactly what we don't want.

We want a system call (the recvmsg, to be precise) to exit immediately
with -EINTR and not have the program wait until it gets more data on the
socket before it closes itself.

This way we can kill the program with start-stop-daemon from
time-test.sh.

Fixes: 17c848f043f3 ("raw-l2-rcv: Avoid truncation on last output line when killed")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

common.c
@@ -143,7 +143,8 @@ int sk_receive(int fd, void *buf, int buflen, struct timespec *hwts, int flags)
 	}
 
 	len = recvmsg(fd, &msg, flags);
-	if (len < 1)
+	/* Suppress "Interrupted system call" message */
+	if (len < 1 && errno != EINTR)
 		fprintf(stderr, "recvmsg%sfailed: %s\n",
 			flags == MSG_ERRQUEUE ? " tx timestamp " : " ",
 			strerror(errno));

raw-l2-rcv.c
@@ -149,7 +149,8 @@ static int server_loop(struct prog_data *prog, void *app_data)
 
 	do {
 		len = sk_receive(prog->fd, prog->rcvbuf, BUF_SIZ, &hwts, 0);
-		if (len < 0) {
+		/* Suppress "Interrupted system call" message */
+		if (len < 0 && errno != EINTR) {
 			fprintf(stderr, "recvfrom returned %d: %s\n",
 				errno, strerror(errno));
 			rc = -errno;
@@ -200,15 +201,21 @@ void sig_handler(int signo)
 static int prog_init(struct prog_data *prog)
 {
 	struct sockaddr_ll addr;
+	struct sigaction sa;
 	int sockopt = 1;
 	int rc;
 
-	if (signal(SIGTERM, sig_handler) == SIG_ERR) {
+	sa.sa_handler = sig_handler;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+
+	rc = sigaction(SIGTERM, &sa, NULL);
+	if (rc < 0) {
 		fprintf(stderr, "can't catch SIGTERM: %s\n", strerror(errno));
 		return -errno;
 	}
-
-	if (signal(SIGINT, sig_handler) == SIG_ERR) {
+	rc = sigaction(SIGINT, &sa, NULL);
+	if (rc < 0) {
 		fprintf(stderr, "can't catch SIGINT: %s\n", strerror(errno));
 		return -errno;
 	}