NXP/isochron

commit 939e2f2a94d8b9862993e177af44e18fa90acf3f

raw-l2: Use native fixed-size integer types exported by the Linux kernel UAPI
It turns out it isn't necessary to typedef these, as long as we compile
for the Linux kernel headers.

Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

raw-l2-common.c
@@ -22,7 +22,7 @@ int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags)
 	return syscall(__NR_sched_setattr, pid, attr, flags);
 }
 
-int mac_addr_from_string(u8 *to, char *from)
+int mac_addr_from_string(__u8 *to, char *from)
 {
 	unsigned long byte;
 	char *p = from;
@@ -30,7 +30,7 @@ int mac_addr_from_string(u8 *to, char *from)
 
 	for (i = 0; i < ETH_ALEN; i++) {
 		byte = strtoul(p, &p, 16);
-		to[i] = (u8 )byte;
+		to[i] = (__u8 )byte;
 		if (i == (ETH_ALEN - 1) && *p != 0)
 			/* 6 bytes processed but more are present */
 			return -EFBIG;
@@ -41,7 +41,7 @@ int mac_addr_from_string(u8 *to, char *from)
 	return 0;
 }
 
-static int get_time_from_string(clockid_t clkid, s64 *to, char *from)
+static int get_time_from_string(clockid_t clkid, __s64 *to, char *from)
 {
 	struct timespec now_ts = {0};
 	__kernel_time_t sec;
@@ -49,7 +49,7 @@ static int get_time_from_string(clockid_t clkid, s64 *to, char *from)
 	char *nsec_str;
 	long nsec = 0;
 	int size, rc;
-	s64 now = 0;
+	__s64 now = 0;
 
 	if (from[0] == '+') {
 		relative = 1;
@@ -244,26 +244,26 @@ int prog_parse_np_args(int argc, char **argv, struct prog_arg *prog_args,
 	return parsed;
 }
 
-void mac_addr_sprintf(char *buf, u8 *addr)
+void mac_addr_sprintf(char *buf, __u8 *addr)
 {
 	snprintf(buf, MACADDR_BUFSIZ, "%02x:%02x:%02x:%02x:%02x:%02x",
 		 addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
 }
 
-s64 timespec_to_ns(const struct timespec *ts)
+__s64 timespec_to_ns(const struct timespec *ts)
 {
 	return ts->tv_sec * NSEC_PER_SEC + ts->tv_nsec;
 }
 
-struct timespec ns_to_timespec(s64 ns)
+struct timespec ns_to_timespec(__s64 ns)
 {
 	return (struct timespec) {
 		.tv_sec = ns / NSEC_PER_SEC,
-		.tv_nsec = ns % NSEC_PER_SEC,
+		.tv_nsec = abs(ns) % NSEC_PER_SEC,
 	};
 }
 
-void ns_sprintf(char *buf, s64 ns)
+void ns_sprintf(char *buf, __s64 ns)
 {
 	struct timespec ts = ns_to_timespec(ns);
 

raw-l2-common.h
@@ -45,12 +45,6 @@ struct sched_attr {
 	uint64_t sched_period;
 };
 
-typedef uint64_t	u64;
-typedef int64_t		s64;
-typedef uint32_t	u32;
-typedef int32_t		s32;
-typedef uint8_t		u8;
-
 int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags);
 
 /* Copied from libnfnetlink.h */
@@ -102,7 +96,7 @@ struct prog_arg_string {
 
 struct prog_arg_time {
 	clockid_t clkid;
-	s64 *ns;
+	__s64 *ns;
 };
 
 struct prog_arg_long {
@@ -133,24 +127,24 @@ void prog_usage(char *prog_name, struct prog_arg *prog_args,
 		int prog_args_size);
 
 struct app_header {
-	s64			tx_time;
+	__s64			tx_time;
 	short			seqid;
 };
 
 struct timestamp {
 	struct timespec		hw;
 	struct timespec		sw;
-	s64			tx_time;
+	__s64			tx_time;
 	short			seqid;
 };
 
-int mac_addr_from_string(u8 *to, char *from);
+int mac_addr_from_string(__u8 *to, char *from);
 int sk_timestamping_init(int fd, const char *if_name, int on);
 int sk_receive(int fd, void *buf, int buflen, struct timestamp *tstamp,
 	       int flags, int timeout);
-s64 timespec_to_ns(const struct timespec *ts);
-struct timespec ns_to_timespec(s64 ns);
-void mac_addr_sprintf(char *buf, u8 *addr);
-void ns_sprintf(char *buf, s64 ns);
+__s64 timespec_to_ns(const struct timespec *ts);
+struct timespec ns_to_timespec(__s64 ns);
+void mac_addr_sprintf(char *buf, __u8 *addr);
+void ns_sprintf(char *buf, __s64 ns);
 
 #endif

raw-l2-rcv.c
@@ -23,9 +23,9 @@
 
 struct prog_data {
 	char if_name[IFNAMSIZ];
-	u8 dest_mac[ETH_ALEN];
+	__u8 dest_mac[ETH_ALEN];
 	unsigned int if_index;
-	u8 rcvbuf[BUF_SIZ];
+	__u8 rcvbuf[BUF_SIZ];
 	int fd;
 };
 
@@ -41,9 +41,9 @@ int signal_received = 0;
  *
  * Return a u64 value of the address
  */
-static inline u64 ether_addr_to_u64(const unsigned char *addr)
+static inline __u64 ether_addr_to_u64(const unsigned char *addr)
 {
-	u64 u = 0;
+	__u64 u = 0;
 	int i;
 
 	for (i = 0; i < ETH_ALEN; i++)
@@ -64,7 +64,7 @@ static int app_loop(void *app_data, char *rcvbuf, size_t len,
 	char swts_buf[TIMESPEC_BUFSIZ];
 	char smac_buf[MACADDR_BUFSIZ];
 	char dmac_buf[MACADDR_BUFSIZ];
-	s64 hwts, swts;
+	__s64 hwts, swts;
 	int i, rc;
 
 	hwts = timespec_to_ns(&tstamp->hw);

raw-l2-send.c
@@ -36,17 +36,17 @@ struct app_private {
 };
 
 struct prog_data {
-	u8 dest_mac[ETH_ALEN];
+	__u8 dest_mac[ETH_ALEN];
 	char if_name[IFNAMSIZ];
 	char sendbuf[BUF_SIZ];
 	struct sockaddr_ll socket_address;
 	long timestamped;
 	long iterations;
 	clockid_t clkid;
-	s64 advance_time;
-	s64 shift_time;
-	s64 cycle_time;
-	s64 base_time;
+	__s64 advance_time;
+	__s64 shift_time;
+	__s64 cycle_time;
+	__s64 base_time;
 	long priority;
 	int log_buf_len;
 	char *log_buf;
@@ -86,7 +86,7 @@ static void process_txtstamp(struct prog_data *prog, struct timestamp *tstamp)
 	char scheduled_buf[TIMESPEC_BUFSIZ];
 	char hwts_buf[TIMESPEC_BUFSIZ];
 	char swts_buf[TIMESPEC_BUFSIZ];
-	s64 hwts, swts;
+	__s64 hwts, swts;
 
 	hwts = timespec_to_ns(&tstamp->hw);
 	swts = timespec_to_ns(&tstamp->sw);
@@ -100,7 +100,7 @@ static void process_txtstamp(struct prog_data *prog, struct timestamp *tstamp)
 	prog->timestamped++;
 }
 
-static int do_work(struct prog_data *prog, int iteration, s64 scheduled,
+static int do_work(struct prog_data *prog, int iteration, __s64 scheduled,
 		   clockid_t clkid)
 {
 	struct app_private *priv = &prog->priv;
@@ -168,8 +168,8 @@ static int run_nanosleep(struct prog_data *prog)
 {
 	char cycle_time_buf[TIMESPEC_BUFSIZ];
 	char base_time_buf[TIMESPEC_BUFSIZ];
-	s64 wakeup = prog->base_time;
-	s64 scheduled;
+	__s64 wakeup = prog->base_time;
+	__s64 scheduled;
 	int rc;
 	long i;
 
@@ -262,9 +262,9 @@ static int prog_configure_rt(struct prog_data *prog)
  * N >= ---------------
  *         cycle_time
  */
-static s64 future_base_time(s64 base_time, s64 cycle_time, s64 now)
+static __s64 future_base_time(__s64 base_time, __s64 cycle_time, __s64 now)
 {
-	s64 n;
+	__s64 n;
 
 	if (base_time >= now)
 		return base_time;
@@ -281,7 +281,7 @@ static int prog_init(struct prog_data *prog)
 	struct timespec now_ts;
 	struct ifreq if_idx;
 	struct ifreq if_mac;
-	s64 now;
+	__s64 now;
 	int rc;
 
 	prog->clkid = CLOCK_REALTIME;

commit c221d9ef203e161ab40231e8e521659ad84769a0

raw-l2-common: Fix printing of timestamps
The timestamps are kept internally as __s64 (signed long long int).
Because I wanted to play nice and print negative time deltas in a pretty
format, I used the abs() function thinking it is a macro.

But man 3 abs shows it is a stdlib.h function that takes an int as
argument, so overflow is occurring when feeding s64 to it. This caused
all sorts of strange behavior, such as time that appears to be going
backwards (and jumping at times).

Use the proper abs() function that takes long long ints as argument
(llabs).

Fixes: 939e2f2a94d8 ("raw-l2: Use native fixed-size integer types exported by the Linux kernel UAPI")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

raw-l2-common.c
@@ -254,7 +254,7 @@ struct timespec ns_to_timespec(__s64 ns)
 {
 	return (struct timespec) {
 		.tv_sec = ns / NSEC_PER_SEC,
-		.tv_nsec = abs(ns) % NSEC_PER_SEC,
+		.tv_nsec = llabs(ns) % NSEC_PER_SEC,
 	};
 }
 