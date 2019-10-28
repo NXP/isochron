NXP/isochron

commit 57e353f58c53059fd4d9c2be8c0049c804e04bda

time-test: Add VLAN header directly within C applications
Kernelshark says that the mere process of TX indirection through
eno0.100 (a stacked net device) adds a 16 us latency (net_dev_queue ->
net_dev_start_xmit).

We can certainly do better ourselves.

Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

raw-l2-rcv.c
@@ -56,8 +56,8 @@ static int app_loop(void *app_data, char *rcvbuf, size_t len,
 		    const struct timestamp *tstamp)
 {
 	/* Header structures */
-	struct ether_header *eth_hdr = (struct ether_header *)rcvbuf;
-	struct app_header *app_hdr = (struct app_header *)(eth_hdr + 1);
+	struct vlan_ethhdr *vlan_hdr = (struct vlan_ethhdr *)rcvbuf;
+	struct app_header *app_hdr = (struct app_header *)(vlan_hdr + 1);
 	struct app_private *priv = app_data;
 	char gate_buf[TIMESPEC_BUFSIZ];
 	char hwts_buf[TIMESPEC_BUFSIZ];
@@ -74,10 +74,11 @@ static int app_loop(void *app_data, char *rcvbuf, size_t len,
 	ns_sprintf(hwts_buf, hwts);
 	ns_sprintf(swts_buf, swts);
 	ns_sprintf(gate_buf, __be64_to_cpu(app_hdr->tx_time));
-	mac_addr_sprintf(smac_buf, eth_hdr->ether_shost);
-	mac_addr_sprintf(dmac_buf, eth_hdr->ether_dhost);
+	mac_addr_sprintf(smac_buf, vlan_hdr->h_source);
+	mac_addr_sprintf(dmac_buf, vlan_hdr->h_dest);
 	printf("[%s] src %s dst %s ethertype 0x%04x seqid %d rxtstamp %s swts %s\n",
-	       gate_buf, smac_buf, dmac_buf, ntohs(eth_hdr->ether_type),
+	       gate_buf, smac_buf, dmac_buf,
+	       ntohs(vlan_hdr->h_vlan_encapsulated_proto),
 	       ntohs(app_hdr->seqid), hwts_buf, swts_buf);
 
 	return 0;
@@ -134,7 +135,7 @@ static int multicast_listen(int fd, unsigned int if_index,
 
 static int server_loop(struct prog_data *prog, void *app_data)
 {
-	struct ether_header *eth_hdr = (struct ether_header *)prog->rcvbuf;
+	struct vlan_ethhdr *vlan_hdr = (struct vlan_ethhdr *)prog->rcvbuf;
 	struct timestamp tstamp;
 	ssize_t len;
 	int rc = 0;
@@ -150,7 +151,8 @@ static int server_loop(struct prog_data *prog, void *app_data)
 			break;
 		}
 		if (ether_addr_to_u64(prog->dest_mac) &&
-		    ether_addr_to_u64(prog->dest_mac) != ether_addr_to_u64(eth_hdr->ether_dhost))
+		    ether_addr_to_u64(prog->dest_mac) !=
+		    ether_addr_to_u64(vlan_hdr->h_dest))
 			continue;
 		rc = app_loop(app_data, prog->rcvbuf, len, &tstamp);
 		if (rc < 0)
@@ -219,8 +221,8 @@ static int prog_init(struct prog_data *prog)
 		return -errno;
 	}
 
-	/* Open PF_PACKET socket, listening for EtherType ETH_P_TSN */
-	prog->fd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_TSN));
+	/* Open PF_PACKET socket, listening for EtherType ETH_P_8021Q */
+	prog->fd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_8021Q));
 	if (prog->fd < 0) {
 		perror("listener: socket");
 		return -errno;

time-test.sh
@@ -206,7 +206,6 @@ enetc_8021qbv_config() {
 felix_8021qbv_config() {
 	local iface=$1
 
-	do_vlan_subinterface eno2 100
 	for eth in ${iface} swp4; do
 		bridge vlan add vid 100 dev "${eth}"
 		tsntool pcpmap --device "${eth}" --enable 2&>1 /dev/null
@@ -298,24 +297,21 @@ get_queue_counters() {
 do_send_traffic() {
 	local remote="root@${board2_ip}"
 	local iface=
-	local mgmt_iface=
 	local err=false
 
 	case "${scenario}" in
 	enetc)
-		iface="eno0.100"
-		mgmt_iface="eno0"
+		iface="eno0"
 		;;
 	felix)
-		iface="eno2.100"
-		mgmt_iface="eno2"
+		iface="eno2"
 		;;
 	esac
 
 	check_sync
 
 	printf "Getting destination MAC address... "
-	dmac="$(get_remote_mac ${board2_ip} iproute2 ${mgmt_iface})" || err=true
+	dmac="$(get_remote_mac ${board2_ip} iproute2 ${iface})" || err=true
 	# Make sure 0001-mscc-ocelot-don-t-duplicate-broadcast-traffic.patch
 	# is applied to the kernel.
 	if [ -z "${dmac}" ] || [ ${err} = true ]; then
@@ -333,13 +329,14 @@ do_send_traffic() {
 
 	do_8021qbv true
 
-	before=$(get_queue_counters ${mgmt_iface} ${txq})
+	before=$(get_queue_counters ${iface} ${txq})
 
 	echo "Opening transmitter process..."
 	trace-cmd record -e irq -e net -e syscalls -e sched \
 		"${TOPDIR}/raw-l2-send" \
 		--interface "${iface}" \
 		--dmac "${dmac}" \
+		--vid 100 \
 		--priority "${txq}" \
 		--base-time "${os_base_time}" \
 		--cycle-time "${cycle_time}" \
@@ -353,7 +350,7 @@ do_send_traffic() {
 
 	receiver_open=false
 
-	after=$(get_queue_counters ${mgmt_iface} ${txq})
+	after=$(get_queue_counters ${iface} ${txq})
 
 	do_8021qbv false
 
@@ -386,10 +383,10 @@ do_start_rcv_traffic() {
 
 	case "${scenario}" in
 	enetc)
-		iface="eno0.100"
+		iface="eno0"
 		;;
 	felix)
-		iface="eno2.100"
+		iface="eno2"
 		;;
 	esac
 
@@ -601,9 +598,6 @@ prerequisites() {
 
 do_prepare() {
 	case "${scenario}" in
-	enetc)
-		do_vlan_subinterface eno0 100
-		;;
 	felix)
 		[ -d /sys/class/net/br0 ] && ip link del dev br0
 		ip link add name br0 type bridge stp_state 0 vlan_filtering 1
@@ -615,6 +609,7 @@ do_prepare() {
 			ip link set ${eth} master br0
 			ip link set ${eth} up
 		done
+		;;
 	esac
 
 	for cpu in cpu0 cpu1; do
@@ -679,7 +674,6 @@ case "${board}" in
 		;;
 	teardown)
 		do_8021qbu false
-		[ -d "/sys/class/net/eno0.100" ] && ip link del dev eno0.100
 		;;
 	*)
 		usage
@@ -704,7 +698,6 @@ case "${board}" in
 		do_print_config_done ${board}
 		;;
 	teardown)
-		[ -d "/sys/class/net/eno0.100" ] && ip link del dev eno0.100
 		;;
 	*)
 		usage

raw-l2-common.c
@@ -398,7 +398,7 @@ int sk_receive(int fd, void *buf, int buflen, struct timestamp *tstamp,
 			flags == MSG_ERRQUEUE ? " tx timestamp " : " ",
 			strerror(errno));
 
-	app_hdr = (struct app_header *)(buf + sizeof(struct ether_header));
+	app_hdr = (struct app_header *)(buf + sizeof(struct vlan_ethhdr));
 	tstamp->seqid = ntohs(app_hdr->seqid);
 	tstamp->tx_time = __be64_to_cpu(app_hdr->tx_time);
 

raw-l2-common.h
@@ -82,6 +82,28 @@ int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags);
 # endif
 #endif
 
+/**
+ *	struct vlan_ethhdr - vlan ethernet header (ethhdr + vlan_hdr)
+ *	@h_dest: destination ethernet address
+ *	@h_source: source ethernet address
+ *	@h_vlan_proto: ethernet protocol
+ *	@h_vlan_TCI: priority and VLAN ID
+ *	@h_vlan_encapsulated_proto: packet type ID or len
+ */
+struct vlan_ethhdr {
+	unsigned char	h_dest[ETH_ALEN];
+	unsigned char	h_source[ETH_ALEN];
+	__be16		h_vlan_proto;
+	__be16		h_vlan_TCI;
+	__be16		h_vlan_encapsulated_proto;
+};
+
+#define VLAN_PRIO_MASK		0xe000 /* Priority Code Point */
+#define VLAN_PRIO_SHIFT		13
+#define VLAN_CFI_MASK		0x1000 /* Canonical Format Indicator / Drop Eligible Indicator */
+#define VLAN_VID_MASK		0x0fff /* VLAN Identifier */
+#define VLAN_N_VID		4096
+
 enum prog_arg_type {
 	PROG_ARG_MAC_ADDR,
 	PROG_ARG_LONG,

raw-l2-send.c
@@ -51,6 +51,7 @@ struct prog_data {
 	int log_buf_len;
 	char *log_buf;
 	long tx_len;
+	long vid;
 	int fd;
 	struct app_private priv;
 };
@@ -112,7 +113,7 @@ static int do_work(struct prog_data *prog, int iteration, __s64 scheduled,
 
 	clock_gettime(clkid, &now_ts);
 	app_hdr = (struct app_header *)(priv->sendbuf +
-					sizeof(struct ether_header));
+					sizeof(struct vlan_ethhdr));
 	app_hdr->tx_time = __cpu_to_be64(scheduled);
 	app_hdr->seqid = htons(iteration);
 
@@ -208,8 +209,8 @@ static int run_nanosleep(struct prog_data *prog)
 
 static void app_init(void *data)
 {
+	int i = sizeof(struct vlan_ethhdr);
 	struct app_private *priv = data;
-	int i = sizeof(struct ether_header);
 
 	/* Packet data */
 	while (i < priv->tx_len) {
@@ -277,7 +278,7 @@ static __s64 future_base_time(__s64 base_time, __s64 cycle_time, __s64 now)
 static int prog_init(struct prog_data *prog)
 {
 	char now_buf[TIMESPEC_BUFSIZ];
-	struct ether_header *eh;
+	struct vlan_ethhdr *hdr;
 	struct timespec now_ts;
 	struct ifreq if_idx;
 	struct ifreq if_mac;
@@ -322,11 +323,14 @@ static int prog_init(struct prog_data *prog)
 	/* Construct the Ethernet header */
 	memset(prog->sendbuf, 0, BUF_SIZ);
 	/* Ethernet header */
-	eh = (struct ether_header *) prog->sendbuf;
-	memcpy(eh->ether_shost, &if_mac.ifr_hwaddr.sa_data, ETH_ALEN);
-	memcpy(eh->ether_dhost, prog->dest_mac, ETH_ALEN);
+	hdr = (struct vlan_ethhdr *)prog->sendbuf;
+	memcpy(hdr->h_source, &if_mac.ifr_hwaddr.sa_data, ETH_ALEN);
+	memcpy(hdr->h_dest, prog->dest_mac, ETH_ALEN);
+	hdr->h_vlan_proto = htons(ETH_P_8021Q);
 	/* Ethertype field */
-	eh->ether_type = htons(ETH_P_TSN);
+	hdr->h_vlan_encapsulated_proto = htons(ETH_P_TSN);
+	hdr->h_vlan_TCI = htons((prog->priority << VLAN_PRIO_SHIFT) |
+				(prog->vid & VLAN_VID_MASK));
 
 	/* Index of the network device */
 	prog->socket_address.sll_ifindex = if_idx.ifr_ifindex;
@@ -459,6 +463,14 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 			.long_ptr = {
 				.ptr = &prog->tx_len,
 			},
+		}, {
+			.short_opt = "-v",
+			.long_opt = "--vid",
+			.type = PROG_ARG_LONG,
+			.long_ptr = {
+				.ptr = &prog->vid,
+			},
+			.optional = true,
 		},
 	};
 	char *prog_name = argv[0];

commit f6afeb56c86f7b31a4eae9184dafd2d0b0360bf5

raw-l2-rcv: Keep using VLAN sub-interface
For some reason, I am now no longer able to receive VLAN-tagged frames
unless I open the socket as ETH_P_ALL. But then the frames would have to
be filtered out at the application level.

Creating a VLAN sub-interface also deals with calling the proper
.ndo_vlan_rx_add_vid driver function, so keep that on the receive side.

Fixes: 57e353f58c53 ("time-test: Add VLAN header directly within C applications")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

raw-l2-rcv.c
@@ -56,8 +56,8 @@ static int app_loop(void *app_data, char *rcvbuf, size_t len,
 		    const struct timestamp *tstamp)
 {
 	/* Header structures */
-	struct vlan_ethhdr *vlan_hdr = (struct vlan_ethhdr *)rcvbuf;
-	struct app_header *app_hdr = (struct app_header *)(vlan_hdr + 1);
+	struct ethhdr *eth_hdr = (struct ethhdr *)rcvbuf;
+	struct app_header *app_hdr = (struct app_header *)(eth_hdr + 1);
 	struct app_private *priv = app_data;
 	char gate_buf[TIMESPEC_BUFSIZ];
 	char hwts_buf[TIMESPEC_BUFSIZ];
@@ -74,11 +74,10 @@ static int app_loop(void *app_data, char *rcvbuf, size_t len,
 	ns_sprintf(hwts_buf, hwts);
 	ns_sprintf(swts_buf, swts);
 	ns_sprintf(gate_buf, __be64_to_cpu(app_hdr->tx_time));
-	mac_addr_sprintf(smac_buf, vlan_hdr->h_source);
-	mac_addr_sprintf(dmac_buf, vlan_hdr->h_dest);
+	mac_addr_sprintf(smac_buf, eth_hdr->h_source);
+	mac_addr_sprintf(dmac_buf, eth_hdr->h_dest);
 	printf("[%s] src %s dst %s ethertype 0x%04x seqid %d rxtstamp %s swts %s\n",
-	       gate_buf, smac_buf, dmac_buf,
-	       ntohs(vlan_hdr->h_vlan_encapsulated_proto),
+	       gate_buf, smac_buf, dmac_buf, ntohs(eth_hdr->h_proto),
 	       ntohs(app_hdr->seqid), hwts_buf, swts_buf);
 
 	return 0;
@@ -135,7 +134,7 @@ static int multicast_listen(int fd, unsigned int if_index,
 
 static int server_loop(struct prog_data *prog, void *app_data)
 {
-	struct vlan_ethhdr *vlan_hdr = (struct vlan_ethhdr *)prog->rcvbuf;
+	struct ethhdr *eth_hdr = (struct ethhdr *)prog->rcvbuf;
 	struct timestamp tstamp;
 	ssize_t len;
 	int rc = 0;
@@ -152,7 +151,7 @@ static int server_loop(struct prog_data *prog, void *app_data)
 		}
 		if (ether_addr_to_u64(prog->dest_mac) &&
 		    ether_addr_to_u64(prog->dest_mac) !=
-		    ether_addr_to_u64(vlan_hdr->h_dest))
+		    ether_addr_to_u64(eth_hdr->h_dest))
 			continue;
 		rc = app_loop(app_data, prog->rcvbuf, len, &tstamp);
 		if (rc < 0)
@@ -221,8 +220,8 @@ static int prog_init(struct prog_data *prog)
 		return -errno;
 	}
 
-	/* Open PF_PACKET socket, listening for EtherType ETH_P_8021Q */
-	prog->fd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_8021Q));
+	/* Open PF_PACKET socket, listening for EtherType ETH_P_TSN */
+	prog->fd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_TSN));
 	if (prog->fd < 0) {
 		perror("listener: socket");
 		return -errno;

time-test.sh
@@ -383,7 +383,7 @@ do_start_rcv_traffic() {
 
 	case "${scenario}" in
 	enetc)
-		iface="eno0"
+		iface="eno0.100"
 		;;
 	felix)
 		iface="eno2"
@@ -598,6 +598,11 @@ prerequisites() {
 
 do_prepare() {
 	case "${scenario}" in
+	enetc)
+		if [ $board = 2 ]; then
+			do_vlan_subinterface eno0 100
+		fi
+		;;
 	felix)
 		[ -d /sys/class/net/br0 ] && ip link del dev br0
 		ip link add name br0 type bridge stp_state 0 vlan_filtering 1
@@ -698,6 +703,7 @@ case "${board}" in
 		do_print_config_done ${board}
 		;;
 	teardown)
+		[ -d "/sys/class/net/eno0.100" ] && ip link del dev eno0.100
 		;;
 	*)
 		usage