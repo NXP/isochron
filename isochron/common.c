NXP/isochron

commit 5d8cba3569109d93d76ace6f498e9a4cfe5dab5d

isochron: convert log to a binary format
Makes parsing from C easier.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -446,54 +446,55 @@ int sk_receive(int fd, void *buf, int buflen, struct timestamp *tstamp,
 	return len;
 }
 
-int rtprint_init(struct rtprint *rt)
+int isochron_log_init(struct isochron_log *log)
 {
-	rt->log_buf = calloc(sizeof(char), LOGBUF_SIZ);
-	if (!rt->log_buf)
+	log->buf = calloc(sizeof(char), LOGBUF_SIZ);
+	if (!log->buf)
 		return -ENOMEM;
 
-	rt->log_buf_len = -1;
+	log->buf_len = 0;
 
 	return 0;
 }
 
-int rtprintf(struct rtprint *rt, char *fmt, ...)
+void isochron_log_data(struct isochron_log *log, void *data, int len)
 {
-	char *buf = rt->log_buf + rt->log_buf_len + 1;
-	va_list args;
+	char *p = log->buf + log->buf_len;
+
+	memcpy(p, data, len);
+	log->buf_len += len;
+}
+
+int isochron_log_xmit(struct isochron_log *log, int fd)
+{
+	int cnt = log->buf_len;
+	char *p = log->buf;
+	char *pos;
 	int rc;
 
-	va_start(args, fmt);
+	while ((rc = write(fd, p, cnt)) > 0) {
+		cnt -= rc;
+		p += rc;
+	}
 
-	rc = vsnprintf(buf, LOGBUF_SIZ - rt->log_buf_len, fmt, args);
-	rt->log_buf_len += (rc + 1);
-
-	va_end(args);
+	if (rc < 0) {
+		fprintf(stderr, "write returned %d: %s\n",
+			errno, strerror(errno));
+		rc = -errno;
+	}
 
 	return rc;
 }
 
-void rtflush(struct rtprint *rt, int fd)
+int isochron_log_recv(struct isochron_log *log, int fd)
 {
-	int rc, i = 0;
-
-	while (i < rt->log_buf_len && rt->log_buf[i]) {
-		rc = dprintf(fd, "%s", rt->log_buf + i);
-		i += (rc + 1);
-	}
-}
-
-int rtprint_rcv(struct rtprint *rt, int fd)
-{
-	char *p = rt->log_buf;
+	char *p = log->buf;
 	char *pos;
 	int rc;
 
 	while ((rc = read(fd, p, LOGBUF_SIZ)) > 0) {
 		p += rc;
-		*p = '\0';
-		p++;
-		rt->log_buf_len += rc + 1;
+		log->buf_len += rc;
 	}
 
 	if (rc < 0) {
@@ -505,7 +506,61 @@ int rtprint_rcv(struct rtprint *rt, int fd)
 	return rc;
 }
 
-void rtprint_teardown(struct rtprint *rt)
+void isochron_log_teardown(struct isochron_log *log)
 {
-	free(rt->log_buf);
+	free(log->buf);
+}
+
+void isochron_rcv_log_print(struct isochron_log *log)
+{
+	char *log_buf_end = log->buf + log->buf_len;
+	struct isochron_rcv_pkt_data *rcv_pkt;
+
+	for (rcv_pkt = (struct isochron_rcv_pkt_data *)log->buf;
+	     (char *)rcv_pkt < log_buf_end; rcv_pkt++) {
+		char scheduled_buf[TIMESPEC_BUFSIZ];
+		char smac_buf[MACADDR_BUFSIZ];
+		char dmac_buf[MACADDR_BUFSIZ];
+
+		/* Print packet */
+		ns_sprintf(scheduled_buf, rcv_pkt->tx_time);
+		mac_addr_sprintf(smac_buf, rcv_pkt->smac);
+		mac_addr_sprintf(dmac_buf, rcv_pkt->smac);
+
+		if (rcv_pkt->hwts) {
+			char hwts_buf[TIMESPEC_BUFSIZ];
+			char swts_buf[TIMESPEC_BUFSIZ];
+
+			ns_sprintf(hwts_buf, rcv_pkt->hwts);
+			ns_sprintf(swts_buf, rcv_pkt->swts);
+
+			printf("[%s] src %s dst %s ethertype 0x%04x seqid %d rxtstamp %s swts %s\n",
+			       scheduled_buf, smac_buf, dmac_buf, rcv_pkt->etype,
+			       rcv_pkt->seqid, hwts_buf, swts_buf);
+		} else {
+			printf("[%s] src %s dst %s ethertype 0x%04x seqid %d\n",
+			       scheduled_buf, smac_buf, dmac_buf, rcv_pkt->etype,
+			       rcv_pkt->seqid);
+		}
+	}
+}
+
+void isochron_send_log_print(struct isochron_log *log)
+{
+	char *log_buf_end = log->buf + log->buf_len;
+	struct isochron_send_pkt_data *send_pkt;
+
+	for (send_pkt = (struct isochron_send_pkt_data *)log->buf;
+	     (char *)send_pkt < log_buf_end; send_pkt++) {
+		char scheduled_buf[TIMESPEC_BUFSIZ];
+		char hwts_buf[TIMESPEC_BUFSIZ];
+		char swts_buf[TIMESPEC_BUFSIZ];
+
+		ns_sprintf(scheduled_buf, send_pkt->tx_time);
+		ns_sprintf(hwts_buf, send_pkt->hwts);
+		ns_sprintf(swts_buf, send_pkt->swts);
+
+		printf("[%s] seqid %d txtstamp %s swts %s\n",
+		       scheduled_buf, send_pkt->seqid, hwts_buf, swts_buf);
+	}
 }

isochron/common.h
@@ -81,16 +81,18 @@ struct vlan_ethhdr {
 	__be16		h_vlan_encapsulated_proto;
 };
 
-struct rtprint {
-	int log_buf_len;
-	char *log_buf;
+struct isochron_log {
+	int buf_len;
+	char *buf;
 };
 
-int rtprint_init(struct rtprint *rt);
-int rtprintf(struct rtprint *rt, char *fmt, ...);
-int rtprint_rcv(struct rtprint *rt, int fd);
-void rtflush(struct rtprint *rt, int fd);
-void rtprint_teardown(struct rtprint *rt);
+int isochron_log_init(struct isochron_log *log);
+void isochron_log_data(struct isochron_log *log, void *data, int len);
+int isochron_log_xmit(struct isochron_log *log, int fd);
+int isochron_log_recv(struct isochron_log *log, int fd);
+void isochron_log_teardown(struct isochron_log *log);
+void isochron_rcv_log_print(struct isochron_log *log);
+void isochron_send_log_print(struct isochron_log *log);
 
 #define ISOCHRON_STATS_PORT	5000
 
@@ -158,6 +160,23 @@ struct app_header {
 struct timestamp {
 	struct timespec		hw;
 	struct timespec		sw;
+};
+
+struct isochron_send_pkt_data {
+	__s64 tx_time;
+	__s64 hwts;
+	__s64 swts;
+	short seqid;
+};
+
+struct isochron_rcv_pkt_data {
+	char smac[ETH_ALEN];
+	char dmac[ETH_ALEN];
+	__s64 tx_time;
+	__s64 hwts;
+	__s64 swts;
+	short etype;
+	short seqid;
 };
 
 int mac_addr_from_string(__u8 *to, char *from);

isochron/rcv.c
@@ -27,7 +27,7 @@ struct prog_data {
 	__u8 dest_mac[ETH_ALEN];
 	unsigned int if_index;
 	__u8 rcvbuf[BUF_SIZ];
-	struct rtprint rt;
+	struct isochron_log log;
 	clockid_t clkid;
 	int stats_listenfd;
 	int data_fd;
@@ -42,36 +42,19 @@ static int app_loop(void *app_data, char *rcvbuf, size_t len,
 	/* Header structures */
 	struct ethhdr *eth_hdr = (struct ethhdr *)rcvbuf;
 	struct app_header *app_hdr = (struct app_header *)(eth_hdr + 1);
+	struct isochron_rcv_pkt_data rcv_pkt = {0};
 	struct prog_data *prog = app_data;
-	char gate_buf[TIMESPEC_BUFSIZ];
-	char smac_buf[MACADDR_BUFSIZ];
-	char dmac_buf[MACADDR_BUFSIZ];
-	__s64 hwts, swts;
 	int i, rc;
 
-	hwts = timespec_to_ns(&tstamp->hw);
-	swts = timespec_to_ns(&tstamp->sw);
+	rcv_pkt.tx_time = __be64_to_cpu(app_hdr->tx_time);
+	rcv_pkt.etype = ntohs(eth_hdr->h_proto);
+	memcpy(rcv_pkt.smac, eth_hdr->h_source, ETH_ALEN);
+	memcpy(rcv_pkt.dmac, eth_hdr->h_dest, ETH_ALEN);
+	rcv_pkt.seqid = ntohs(app_hdr->seqid);
+	rcv_pkt.hwts = timespec_to_ns(&tstamp->hw);
+	rcv_pkt.swts = timespec_to_ns(&tstamp->sw);
 
-	/* Print packet */
-	ns_sprintf(gate_buf, __be64_to_cpu(app_hdr->tx_time));
-	mac_addr_sprintf(smac_buf, eth_hdr->h_source);
-	mac_addr_sprintf(dmac_buf, eth_hdr->h_dest);
-
-	if (prog->do_ts) {
-		char hwts_buf[TIMESPEC_BUFSIZ];
-		char swts_buf[TIMESPEC_BUFSIZ];
-
-		ns_sprintf(hwts_buf, hwts);
-		ns_sprintf(swts_buf, swts);
-
-		rtprintf(&prog->rt, "[%s] src %s dst %s ethertype 0x%04x seqid %d rxtstamp %s swts %s\n",
-		       gate_buf, smac_buf, dmac_buf, ntohs(eth_hdr->h_proto),
-		       ntohs(app_hdr->seqid), hwts_buf, swts_buf);
-	} else {
-		rtprintf(&prog->rt, "[%s] src %s dst %s ethertype 0x%04x seqid %d\n",
-		       gate_buf, smac_buf, dmac_buf, ntohs(eth_hdr->h_proto),
-		       ntohs(app_hdr->seqid));
-	}
+	isochron_log_data(&prog->log, &rcv_pkt, sizeof(rcv_pkt));
 
 	return 0;
 }
@@ -206,8 +189,7 @@ static int server_loop(struct prog_data *prog, void *app_data)
 
 			stats_fd = rc;
 
-			rtflush(&prog->rt, stats_fd);
-			rtflush(&prog->rt, 0);
+			isochron_log_xmit(&prog->log, stats_fd);
 
 			close(stats_fd);
 		}
@@ -241,7 +223,7 @@ static int prog_init(struct prog_data *prog)
 	int sockopt = 1;
 	int rc;
 
-	rc = rtprint_init(&prog->rt);
+	rc = isochron_log_init(&prog->log);
 	if (rc < 0)
 		return rc;
 
@@ -387,8 +369,8 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 
 static int prog_teardown(struct prog_data *prog)
 {
-	rtflush(&prog->rt, STDOUT_FILENO);
-	rtprint_teardown(&prog->rt);
+	isochron_rcv_log_print(&prog->log);
+	isochron_log_teardown(&prog->log);
 	close(prog->stats_listenfd);
 	close(prog->data_fd);
 

isochron/send.c
@@ -31,7 +31,7 @@ struct prog_data {
 	char sendbuf[BUF_SIZ];
 	char stats_srv_addr[INET6_ADDRSTRLEN];
 	struct sockaddr_ll socket_address;
-	struct rtprint rt;
+	struct isochron_log log;
 	long timestamped;
 	long iterations;
 	clockid_t clkid;
@@ -49,37 +49,33 @@ struct prog_data {
 static void process_txtstamp(struct prog_data *prog, const char *buf,
 			     struct timestamp *tstamp)
 {
-	char scheduled_buf[TIMESPEC_BUFSIZ];
-	char hwts_buf[TIMESPEC_BUFSIZ];
-	char swts_buf[TIMESPEC_BUFSIZ];
+	struct isochron_send_pkt_data send_pkt = {0};
 	struct app_header *app_hdr;
 	__s64 hwts, swts;
 
 	app_hdr = (struct app_header *)(buf + sizeof(struct vlan_ethhdr));
 
-	hwts = timespec_to_ns(&tstamp->hw);
-	swts = timespec_to_ns(&tstamp->sw);
+	send_pkt.tx_time = __be64_to_cpu(app_hdr->tx_time);
+	send_pkt.seqid = ntohs(app_hdr->seqid);
+	send_pkt.hwts = timespec_to_ns(&tstamp->hw);
+	send_pkt.swts = timespec_to_ns(&tstamp->sw);
 
-	ns_sprintf(scheduled_buf, __be64_to_cpu(app_hdr->tx_time));
-	ns_sprintf(hwts_buf, hwts);
-	ns_sprintf(swts_buf, swts);
+	isochron_log_data(&prog->log, &send_pkt, sizeof(send_pkt));
 
-	rtprintf(&prog->rt, "[%s] seqid %d txtstamp %s swts %s\n",
-		 scheduled_buf, ntohs(app_hdr->seqid), hwts_buf, swts_buf);
 	prog->timestamped++;
 }
 
-static void print_no_tstamp(struct prog_data *prog, const char *buf)
+static void log_no_tstamp(struct prog_data *prog, const char *buf)
 {
-	char scheduled_buf[TIMESPEC_BUFSIZ];
+	struct isochron_send_pkt_data send_pkt = {0};
 	struct app_header *app_hdr;
 
 	app_hdr = (struct app_header *)(buf + sizeof(struct vlan_ethhdr));
 
-	ns_sprintf(scheduled_buf, __be64_to_cpu(app_hdr->tx_time));
+	send_pkt.tx_time = __be64_to_cpu(app_hdr->tx_time);
+	send_pkt.seqid = ntohs(app_hdr->seqid);
 
-	rtprintf(&prog->rt, "[%s] seqid %d\n", scheduled_buf,
-		 ntohs(app_hdr->seqid));
+	isochron_log_data(&prog->log, &send_pkt, sizeof(send_pkt));
 }
 
 static int do_work(struct prog_data *prog, int iteration, __s64 scheduled,
@@ -118,7 +114,7 @@ static int do_work(struct prog_data *prog, int iteration, __s64 scheduled,
 		 */
 		process_txtstamp(prog, err_pkt, &tstamp);
 	} else {
-		print_no_tstamp(prog, prog->sendbuf);
+		log_no_tstamp(prog, prog->sendbuf);
 	}
 
 	return 0;
@@ -321,7 +317,7 @@ static int prog_init(struct prog_data *prog)
 	ns_sprintf(now_buf, now);
 	fprintf(stderr, "%10s: %s\n", "Now", now_buf);
 
-	rc = rtprint_init(&prog->rt);
+	rc = isochron_log_init(&prog->log);
 	if (rc < 0)
 		return rc;
 
@@ -358,7 +354,7 @@ static int prog_collect_rcv_stats(struct prog_data *prog)
 		.sin_family = AF_INET,
 		.sin_port = htons(ISOCHRON_STATS_PORT),
 	};
-	struct rtprint rcv_rt;
+	struct isochron_log rcv_log;
 	int stats_fd;
 	int rc;
 
@@ -390,9 +386,9 @@ static int prog_collect_rcv_stats(struct prog_data *prog)
 	}
 
 	printf("Collecting receiver stats\n");
-	rtprint_init(&rcv_rt);
-	rtprint_rcv(&rcv_rt, stats_fd);
-	rtflush(&rcv_rt, STDOUT_FILENO);
+	isochron_log_init(&rcv_log);
+	isochron_log_recv(&rcv_log, stats_fd);
+	isochron_rcv_log_print(&rcv_log);
 
 	return 0;
 }
@@ -401,13 +397,13 @@ static int prog_teardown(struct prog_data *prog)
 {
 	int rc;
 
-	rtflush(&prog->rt, STDOUT_FILENO);
+	isochron_send_log_print(&prog->log);
 
 	rc = prog_collect_rcv_stats(prog);
 	if (rc)
 		return rc;
 
-	rtprint_teardown(&prog->rt);
+	isochron_log_teardown(&prog->log);
 
 	return rc;
 }

commit 8addcbba4e03bca5f5579c48885ab70e2e08a587

isochron: fix printing of destination MAC address
It would get printed as the source MAC address, which is incorrect.

Fixes: 5d8cba356910 ("isochron: convert log to a binary format")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -525,7 +525,7 @@ void isochron_rcv_log_print(struct isochron_log *log)
 		/* Print packet */
 		ns_sprintf(scheduled_buf, rcv_pkt->tx_time);
 		mac_addr_sprintf(smac_buf, rcv_pkt->smac);
-		mac_addr_sprintf(dmac_buf, rcv_pkt->smac);
+		mac_addr_sprintf(dmac_buf, rcv_pkt->dmac);
 
 		if (rcv_pkt->hwts) {
 			char hwts_buf[TIMESPEC_BUFSIZ];