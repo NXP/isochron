NXP/isochron

commit 079c8741ba739e9da1195eda33961952a3a8e58d

isochron: boost priority temporarily within the application
At the time of commit 540af3711ffa ("raw-l2-send: Use chrt instead of
sched_setattr"), isochron (by then raw-l2-send) was a much simpler
program. In the meantime it has gained support for collecting statistics
from the receiver automatically, which has obsoleted much of the
time-test.sh script.

The problem with setting the priority externally, as we are doing right
now with chrt, is that the statistics collection process also takes
place at this high priority. And if we have a lot of statistics to
collect (100k packets), we're going to be running a CPU intensive task
as the highest priority task in the system. If that takes more than 21
seconds, it can easily create RCU stalls in the kernel.

We only need to run with a high scheduling priority during the hot path,
not when collecting and dissecting statistics. So it makes sense to
boost the priority by ourselves, and restore it afterwards.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/rcv.c
@@ -36,6 +36,9 @@ struct prog_data {
 	long etype;
 	long port;
 	long iterations;
+	bool sched_fifo;
+	bool sched_rr;
+	long sched_priority;
 };
 
 int signal_received;
@@ -136,9 +139,29 @@ static int server_loop(struct prog_data *prog, void *app_data)
 			.events = POLLIN | POLLERR | POLLPRI,
 		},
 	};
+	__u32 sched_policy = SCHED_OTHER;
 	ssize_t len;
 	int rc = 0;
 	int cnt;
+
+	if (prog->sched_fifo)
+		sched_policy = SCHED_FIFO;
+	if (prog->sched_rr)
+		sched_policy = SCHED_RR;
+
+	if (sched_policy != SCHED_OTHER) {
+		struct sched_attr attr = {
+			.size = sizeof(struct sched_attr),
+			.sched_policy = sched_policy,
+			.sched_priority = prog->sched_priority,
+		};
+
+		if (sched_setattr(getpid(), &attr, 0)) {
+			fprintf(stderr, "sched_setattr returned %d\n",
+				errno, strerror(errno));
+			return -errno;
+		}
+	}
 
 	do {
 		cnt = poll(pfd, ARRAY_SIZE(pfd), -1);
@@ -216,6 +239,21 @@ static int server_loop(struct prog_data *prog, void *app_data)
 		if (signal_received)
 			break;
 	} while (1);
+
+	/* Restore scheduling policy */
+	if (sched_policy != SCHED_OTHER) {
+		struct sched_attr attr = {
+			.size = sizeof(struct sched_attr),
+			.sched_policy = SCHED_OTHER,
+			.sched_priority = 0,
+		};
+
+		if (sched_setattr(getpid(), &attr, 0)) {
+			fprintf(stderr, "sched_setattr returned %d\n",
+				errno, strerror(errno));
+			return -errno;
+		}
+	}
 
 	return rc;
 }
@@ -402,6 +440,30 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 				.ptr = &prog->iterations,
 			},
 			.optional = true,
+		}, {
+			.short_opt = "-H",
+			.long_opt = "--sched-priority",
+			.type = PROG_ARG_LONG,
+			.long_ptr = {
+			        .ptr = &prog->sched_priority,
+			},
+			.optional = true,
+		}, {
+			.short_opt = "-f",
+			.long_opt = "--sched-fifo",
+			.type = PROG_ARG_BOOL,
+			.boolean_ptr = {
+			        .ptr = &prog->sched_fifo,
+			},
+			.optional = true,
+		}, {
+			.short_opt = "-r",
+			.long_opt = "--sched-rr",
+			.type = PROG_ARG_BOOL,
+			.boolean_ptr = {
+			        .ptr = &prog->sched_rr,
+			},
+			.optional = true,
 		},
 	};
 	int rc;
@@ -418,6 +480,12 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 			argc - rc, argv[rc]);
 		prog_usage("isochron-rcv", args, ARRAY_SIZE(args));
 		return -1;
+	}
+
+	if (prog->sched_fifo && prog->sched_rr) {
+		fprintf(stderr,
+			"cannot have SCHED_FIFO and SCHED_RR at the same time\n");
+		return -EINVAL;
 	}
 
 	if (!prog->port)

isochron/send.c
@@ -60,6 +60,9 @@ struct prog_data {
 	bool taprio;
 	bool do_vlan;
 	int l2_header_len;
+	bool sched_fifo;
+	bool sched_rr;
+	long sched_priority;
 };
 
 static void trace(struct prog_data *prog, const char *fmt, ...)
@@ -206,10 +209,30 @@ static int run_nanosleep(struct prog_data *prog)
 {
 	char cycle_time_buf[TIMESPEC_BUFSIZ];
 	char base_time_buf[TIMESPEC_BUFSIZ];
+	__u32 sched_policy = SCHED_OTHER;
 	__s64 wakeup = prog->base_time;
 	__s64 scheduled;
 	int rc;
 	long i;
+
+	if (prog->sched_fifo)
+		sched_policy = SCHED_FIFO;
+	if (prog->sched_rr)
+		sched_policy = SCHED_RR;
+
+	if (sched_policy != SCHED_OTHER) {
+		struct sched_attr attr = {
+			.size = sizeof(struct sched_attr),
+			.sched_policy = sched_policy,
+			.sched_priority = prog->sched_priority,
+		};
+
+		if (sched_setattr(getpid(), &attr, 0)) {
+			fprintf(stderr, "sched_setattr returned %d\n",
+				errno, strerror(errno));
+			return -errno;
+		}
+	}
 
 	ns_sprintf(base_time_buf, utc_to_tai(prog->base_time));
 	ns_sprintf(cycle_time_buf, prog->cycle_time);
@@ -238,6 +261,21 @@ static int run_nanosleep(struct prog_data *prog)
 			fprintf(stderr, "clock_nanosleep returned %d: %s\n",
 				rc, strerror(rc));
 			break;
+		}
+	}
+
+	/* Restore scheduling policy */
+	if (sched_policy != SCHED_OTHER) {
+		struct sched_attr attr = {
+			.size = sizeof(struct sched_attr),
+			.sched_policy = SCHED_OTHER,
+			.sched_priority = 0,
+		};
+
+		if (sched_setattr(getpid(), &attr, 0)) {
+			fprintf(stderr, "sched_setattr returned %d\n",
+				errno, strerror(errno));
+			return -errno;
 		}
 	}
 
@@ -840,6 +878,30 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 			        .ptr = &prog->taprio,
 			},
 			.optional = true,
+		}, {
+			.short_opt = "-H",
+			.long_opt = "--sched-priority",
+			.type = PROG_ARG_LONG,
+			.long_ptr = {
+			        .ptr = &prog->sched_priority,
+			},
+			.optional = true,
+		}, {
+			.short_opt = "-f",
+			.long_opt = "--sched-fifo",
+			.type = PROG_ARG_BOOL,
+			.boolean_ptr = {
+			        .ptr = &prog->sched_fifo,
+			},
+			.optional = true,
+		}, {
+			.short_opt = "-r",
+			.long_opt = "--sched-rr",
+			.type = PROG_ARG_BOOL,
+			.boolean_ptr = {
+			        .ptr = &prog->sched_rr,
+			},
+			.optional = true,
 		},
 	};
 	int rc;
@@ -882,6 +944,12 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 	if (prog->shift_time > prog->cycle_time) {
 		fprintf(stderr,
 			"Shift time cannot be higher than cycle time\n");
+		return -EINVAL;
+	}
+
+	if (prog->sched_fifo && prog->sched_rr) {
+		fprintf(stderr,
+			"cannot have SCHED_FIFO and SCHED_RR at the same time\n");
 		return -EINVAL;
 	}
 

isochron/common.h
@@ -9,6 +9,28 @@
 #define _COMMON_H
 
 #include <sys/queue.h>
+#include <sys/syscall.h>
+#include <sched.h>
+
+struct sched_attr {
+	__u32 size;		/* Size of this structure */
+	__u32 sched_policy;	/* Policy (SCHED_*) */
+	__u64 sched_flags;	/* Flags */
+	__s32 sched_nice;	/* Nice value (SCHED_OTHER,
+				   SCHED_BATCH) */
+	__u32 sched_priority;	/* Static priority (SCHED_FIFO,
+				   SCHED_RR) */
+	/* Remaining fields are for SCHED_DEADLINE */
+	__u64 sched_runtime;
+	__u64 sched_deadline;
+	__u64 sched_period;
+};
+
+static inline int sched_setattr(pid_t pid, const struct sched_attr *attr,
+				unsigned int flags)
+{
+	return syscall(SYS_sched_setattr, pid, attr, flags);
+}
 
 struct isochron_header {
 	__s64			tx_time;

commit 6fde5e6758459885562f51e40d6fab8d2d4de636

isochron: print error string when sched_setattr fails
Gets rid of the build warnings about too many arguments being provided
for the printf format.

Fixes: 079c8741ba73 ("isochron: boost priority temporarily within the application")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/rcv.c
@@ -157,7 +157,7 @@ static int server_loop(struct prog_data *prog, void *app_data)
 		};
 
 		if (sched_setattr(getpid(), &attr, 0)) {
-			fprintf(stderr, "sched_setattr returned %d\n",
+			fprintf(stderr, "sched_setattr returned %d: %s\n",
 				errno, strerror(errno));
 			return -errno;
 		}
@@ -249,7 +249,7 @@ static int server_loop(struct prog_data *prog, void *app_data)
 		};
 
 		if (sched_setattr(getpid(), &attr, 0)) {
-			fprintf(stderr, "sched_setattr returned %d\n",
+			fprintf(stderr, "sched_setattr returned %d: %s\n",
 				errno, strerror(errno));
 			return -errno;
 		}

isochron/send.c
@@ -228,7 +228,7 @@ static int run_nanosleep(struct prog_data *prog)
 		};
 
 		if (sched_setattr(getpid(), &attr, 0)) {
-			fprintf(stderr, "sched_setattr returned %d\n",
+			fprintf(stderr, "sched_setattr returned %d: %s\n",
 				errno, strerror(errno));
 			return -errno;
 		}
@@ -273,7 +273,7 @@ static int run_nanosleep(struct prog_data *prog)
 		};
 
 		if (sched_setattr(getpid(), &attr, 0)) {
-			fprintf(stderr, "sched_setattr returned %d\n",
+			fprintf(stderr, "sched_setattr returned %d: %s\n",
 				errno, strerror(errno));
 			return -errno;
 		}