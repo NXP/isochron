NXP/isochron

commit 004c09ddbab428caf0d8c2141d6435be000f6b26

isochron: send UDP packets too
We can use isochron for use cases such as testing IP forwarding latency.
So it is useful to pack the isochron header as UDP payload. Add support
for IPv4 and IPv6.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/send.c
@@ -9,6 +9,7 @@
  */
 #include <arpa/inet.h>
 #include <linux/if_packet.h>
+#include <linux/ipv6.h>
 #include <limits.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -20,6 +21,9 @@
 #include <sys/socket.h>
 #include <net/if.h>
 #include <netinet/ether.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
 #include <errno.h>
 #include <sys/mman.h>
 #include <unistd.h>
@@ -41,7 +45,12 @@ struct prog_data {
 	struct msghdr msg;
 	char msg_control[CMSG_SPACE(sizeof(__s64))];
 	struct ip_address stats_srv;
-	struct sockaddr_ll socket_address;
+	union {
+		struct sockaddr_ll l2;
+		struct sockaddr_in udp4;
+		struct sockaddr_in6 udp6;
+	} sockaddr;
+	size_t sockaddr_size;
 	struct isochron_log log;
 	long timestamped;
 	long iterations;
@@ -68,10 +77,15 @@ struct prog_data {
 	bool deadline;
 	bool do_vlan;
 	int l2_header_len;
+	int l4_header_len;
 	bool sched_fifo;
 	bool sched_rr;
 	long sched_priority;
 	long utc_tai_offset;
+	struct ip_address ip_destination;
+	bool l2;
+	bool l4;
+	long data_port;
 };
 
 static void trace(struct prog_data *prog, const char *fmt, ...)
@@ -102,13 +116,16 @@ static void trace(struct prog_data *prog, const char *fmt, ...)
 }
 
 static void process_txtstamp(struct prog_data *prog, const char *buf,
-			     struct timestamp *tstamp)
+			     struct isochron_timestamp *tstamp)
 {
 	struct isochron_send_pkt_data send_pkt = {0};
 	struct isochron_header *hdr;
 	__s64 hwts, swts;
 
-	hdr = (struct isochron_header *)(buf + prog->l2_header_len);
+	if (prog->l2)
+		hdr = (struct isochron_header *)(buf + prog->l2_header_len);
+	else
+		hdr = (struct isochron_header *)(buf + prog->l4_header_len);
 
 	send_pkt.tx_time = __be64_to_cpu(hdr->tx_time);
 	send_pkt.wakeup = __be64_to_cpu(hdr->wakeup);
@@ -131,7 +148,10 @@ static void log_no_tstamp(struct prog_data *prog, const char *buf)
 	if (!prog->iterations)
 		return;
 
-	hdr = (struct isochron_header *)(buf + prog->l2_header_len);
+	if (prog->l2)
+		hdr = (struct isochron_header *)(buf + prog->l2_header_len);
+	else
+		hdr = (struct isochron_header *)buf;
 
 	send_pkt.tx_time = __be64_to_cpu(hdr->tx_time);
 	send_pkt.wakeup = __be64_to_cpu(hdr->wakeup);
@@ -143,7 +163,7 @@ static void log_no_tstamp(struct prog_data *prog, const char *buf)
 static int do_work(struct prog_data *prog, int iteration, __s64 scheduled)
 {
 	unsigned char err_pkt[BUF_SIZ];
-	struct timestamp tstamp = {0};
+	struct isochron_timestamp tstamp = {0};
 	struct isochron_header *hdr;
 	struct timespec now_ts;
 	__s64 now;
@@ -154,7 +174,13 @@ static int do_work(struct prog_data *prog, int iteration, __s64 scheduled)
 
 	trace(prog, "send seqid %d start\n", iteration);
 
-	hdr = (struct isochron_header *)(prog->sendbuf + prog->l2_header_len);
+	if (prog->l2) {
+		hdr = (struct isochron_header *)(prog->sendbuf +
+						 prog->l2_header_len);
+	} else {
+		hdr = (struct isochron_header *)prog->sendbuf;
+	}
+
 	hdr->tx_time = __cpu_to_be64(scheduled);
 	hdr->wakeup = __cpu_to_be64(now);
 	hdr->seqid = __cpu_to_be32(iteration);
@@ -195,7 +221,7 @@ static int do_work(struct prog_data *prog, int iteration, __s64 scheduled)
 static int wait_for_txtimestamps(struct prog_data *prog)
 {
 	unsigned char err_pkt[BUF_SIZ];
-	struct timestamp tstamp;
+	struct isochron_timestamp tstamp;
 	int rc;
 
 	if (!prog->do_ts)
@@ -337,8 +363,12 @@ static int prog_init(struct prog_data *prog)
 
 	prog->clkid = CLOCK_TAI;
 
-	/* Open RAW socket to send on */
-	prog->data_fd = socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW);
+	/* Open socket to send on */
+	if (prog->l2)
+		prog->data_fd = socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW);
+	else
+		prog->data_fd = socket(prog->ip_destination.family, SOCK_DGRAM,
+				       IPPROTO_UDP);
 	if (prog->data_fd < 0) {
 		perror("socket");
 		rc = -EINVAL;
@@ -427,12 +457,33 @@ static int prog_init(struct prog_data *prog)
 		hdr->h_proto = htons(prog->etype);
 	}
 
-	/* Index of the network device */
-	prog->socket_address.sll_ifindex = if_idx.ifr_ifindex;
-	/* Address length*/
-	prog->socket_address.sll_halen = ETH_ALEN;
-	/* Destination MAC */
-	memcpy(prog->socket_address.sll_addr, prog->dest_mac, ETH_ALEN);
+	if (prog->l2) {
+		/* Index of the network device */
+		prog->sockaddr.l2.sll_ifindex = if_idx.ifr_ifindex;
+		/* Address length */
+		prog->sockaddr.l2.sll_halen = ETH_ALEN;
+		/* Destination MAC */
+		ether_addr_copy(prog->sockaddr.l2.sll_addr, prog->dest_mac);
+		prog->sockaddr_size = sizeof(struct sockaddr_ll);
+	} else if (prog->ip_destination.family == AF_INET) {
+		prog->sockaddr.udp4.sin_addr = prog->ip_destination.addr;
+		prog->sockaddr.udp4.sin_port = htons(prog->data_port);
+		prog->sockaddr.udp4.sin_family = AF_INET;
+		prog->sockaddr_size = sizeof(struct sockaddr_in);
+	} else {
+		prog->sockaddr.udp6.sin6_addr = prog->ip_destination.addr6;
+		prog->sockaddr.udp6.sin6_port = htons(prog->data_port);
+		prog->sockaddr.udp6.sin6_family = AF_INET6;
+		prog->sockaddr_size = sizeof(struct sockaddr_in6);
+	}
+
+	if (prog->ip_destination.family == AF_INET)
+		prog->l4_header_len = sizeof(struct iphdr) + sizeof(struct udphdr);
+	else if (prog->ip_destination.family == AF_INET6)
+		prog->l4_header_len = sizeof(struct ipv6hdr) + sizeof(struct udphdr);
+
+	if (prog->l4)
+		prog->tx_len -= sizeof(struct ethhdr) + prog->l4_header_len;
 
 	rc = clock_gettime(prog->clkid, &now_ts);
 	if (rc < 0) {
@@ -486,8 +537,8 @@ static int prog_init(struct prog_data *prog)
 	prog->iov.iov_len = prog->tx_len;
 
 	memset(&prog->msg, 0, sizeof(prog->msg));
-	prog->msg.msg_name = (struct sockaddr *)&prog->socket_address;
-	prog->msg.msg_namelen = sizeof(struct sockaddr_ll);
+	prog->msg.msg_name = (struct sockaddr *)&prog->sockaddr;
+	prog->msg.msg_namelen = prog->sockaddr_size;
 	prog->msg.msg_iov = &prog->iov;
 	prog->msg.msg_iovlen = 1;
 
@@ -839,6 +890,7 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 			.mac = {
 				.buf = prog->dest_mac,
 			},
+			.optional = true,
 		}, {
 			.short_opt = "-A",
 			.long_opt = "--smac",
@@ -1032,6 +1084,38 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 			        .ptr = &prog->utc_tai_offset,
 			},
 			.optional = true,
+		}, {
+			.short_opt = "-J",
+			.long_opt = "--ip-destination",
+			.type = PROG_ARG_IP,
+			.ip_ptr = {
+			        .ptr = &prog->ip_destination,
+			},
+			.optional = true,
+		}, {
+			.short_opt = "-2",
+			.long_opt = "--l2",
+			.type = PROG_ARG_BOOL,
+			.boolean_ptr = {
+				.ptr = &prog->l2,
+			},
+			.optional = true,
+		}, {
+			.short_opt = "-4",
+			.long_opt = "--l4",
+			.type = PROG_ARG_BOOL,
+			.boolean_ptr = {
+				.ptr = &prog->l4,
+			},
+			.optional = true,
+		}, {
+			.short_opt = "-P",
+			.long_opt = "--data-port",
+			.type = PROG_ARG_LONG,
+			.long_ptr = {
+				.ptr = &prog->data_port,
+			},
+			.optional = true,
 		},
 	};
 	int rc;
@@ -1051,14 +1135,6 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 			argc - rc, argv[rc]);
 		prog_usage("isochron-send", args, ARRAY_SIZE(args));
 		return -1;
-	}
-
-	if (prog->vid == -1) {
-		prog->do_vlan = false;
-		prog->l2_header_len = sizeof(struct ethhdr);
-	} else {
-		prog->do_vlan = true;
-		prog->l2_header_len = sizeof(struct vlan_ethhdr);
 	}
 
 	if (prog->txtime && prog->taprio) {
@@ -1127,6 +1203,32 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 
 	if (!prog->stats_port)
 		prog->stats_port = ISOCHRON_STATS_PORT;
+
+	if (prog->l2 && prog->l4) {
+		fprintf(stderr, "Choose transport as either L2 or L4!\n");
+		return -EINVAL;
+	}
+
+	if (!prog->l2 && !prog->l4)
+		prog->l2 = true;
+
+	if (prog->l2 && is_zero_ether_addr(prog->dest_mac)) {
+		fprintf(stderr, "Please specify destination MAC address\n");
+		return -EINVAL;
+	}
+
+	if (prog->l2) {
+		if (prog->vid == -1) {
+			prog->do_vlan = false;
+			prog->l2_header_len = sizeof(struct ethhdr);
+		} else {
+			prog->do_vlan = true;
+			prog->l2_header_len = sizeof(struct vlan_ethhdr);
+		}
+	}
+
+	if (!prog->data_port)
+		prog->data_port = ISOCHRON_DATA_PORT;
 
 	/* Convert negative logic from cmdline to positive */
 	prog->do_ts = !prog->do_ts;

isochron/common.c
@@ -407,7 +407,7 @@ int sk_timestamping_init(int fd, const char *if_name, int on)
 	return 0;
 }
 
-int sk_receive(int fd, void *buf, int buflen, struct timestamp *tstamp,
+int sk_receive(int fd, void *buf, int buflen, struct isochron_timestamp *tstamp,
 	       int flags, int timeout)
 {
 	struct iovec iov = { buf, buflen };

isochron/common.h
@@ -175,7 +175,8 @@ void isochron_rcv_log_print(struct isochron_log *log);
 void isochron_send_log_print(struct isochron_log *log);
 void isochron_log_remove(struct isochron_log *log, void *p, int len);
 
-#define ISOCHRON_STATS_PORT	5000
+#define ISOCHRON_STATS_PORT	5000 /* TCP */
+#define ISOCHRON_DATA_PORT	6000 /* UDP */
 
 #define VLAN_PRIO_MASK		0xe000 /* Priority Code Point */
 #define VLAN_PRIO_SHIFT		13
@@ -247,7 +248,7 @@ int prog_parse_np_args(int argc, char **argv,
 void prog_usage(char *prog_name, struct prog_arg *prog_args,
 		int prog_args_size);
 
-struct timestamp {
+struct isochron_timestamp {
 	struct timespec		hw;
 	struct timespec		sw;
 };
@@ -293,7 +294,7 @@ struct isochron_stats {
 
 int mac_addr_from_string(__u8 *to, char *from);
 int sk_timestamping_init(int fd, const char *if_name, int on);
-int sk_receive(int fd, void *buf, int buflen, struct timestamp *tstamp,
+int sk_receive(int fd, void *buf, int buflen, struct isochron_timestamp *tstamp,
 	       int flags, int timeout);
 __s64 timespec_to_ns(const struct timespec *ts);
 struct timespec ns_to_timespec(__s64 ns);
@@ -315,6 +316,32 @@ static inline __u64 ether_addr_to_u64(const unsigned char *addr)
 		u = u << 8 | addr[i];
 
 	return u;
+}
+
+/**
+ * ether_addr_copy - Copy an Ethernet address
+ * @dst: Pointer to a six-byte array Ethernet address destination
+ * @src: Pointer to a six-byte array Ethernet address source
+ *
+ * Please note: dst & src must both be aligned to u16.
+ */
+static inline void ether_addr_copy(__u8 *dst, const __u8 *src)
+{
+	*(__u32 *)dst = *(const __u32 *)src;
+	*(__u16 *)(dst + 4) = *(const __u16 *)(src + 4);
+}
+
+/**
+ * is_zero_ether_addr - Determine if give Ethernet address is all zeros.
+ * @addr: Pointer to a six-byte array containing the Ethernet address
+ *
+ * Return true if the address is all zeroes.
+ *
+ * Please note: addr must be aligned to u16.
+ */
+static inline bool is_zero_ether_addr(const __u8 *addr)
+{
+	return ((*(const __u32 *)addr) | (*(const __u16 *)(addr + 4))) == 0;
 }
 
 int trace_mark_open();

isochron/rcv.c
@@ -40,16 +40,16 @@ struct prog_data {
 	bool sched_rr;
 	long sched_priority;
 	long utc_tai_offset;
+	bool l2;
+	bool l4;
+	long data_port;
 };
 
 int signal_received;
 
 static int app_loop(void *app_data, char *rcvbuf, size_t len,
-		    const struct timestamp *tstamp)
+		    const struct isochron_timestamp *tstamp)
 {
-	/* Header structures */
-	struct ethhdr *eth_hdr = (struct ethhdr *)rcvbuf;
-	struct isochron_header *hdr = (struct isochron_header *)(eth_hdr + 1);
 	struct isochron_rcv_pkt_data rcv_pkt = {0};
 	struct prog_data *prog = app_data;
 	struct timespec now_ts;
@@ -59,14 +59,27 @@ static int app_loop(void *app_data, char *rcvbuf, size_t len,
 	clock_gettime(prog->clkid, &now_ts);
 	now = timespec_to_ns(&now_ts);
 	rcv_pkt.arrival = now;
-	rcv_pkt.tx_time = __be64_to_cpu(hdr->tx_time);
-	rcv_pkt.etype = ntohs(eth_hdr->h_proto);
-	memcpy(rcv_pkt.smac, eth_hdr->h_source, ETH_ALEN);
-	memcpy(rcv_pkt.dmac, eth_hdr->h_dest, ETH_ALEN);
-	rcv_pkt.seqid = __be32_to_cpu(hdr->seqid);
-	rcv_pkt.hwts = timespec_to_ns(&tstamp->hw);
-	rcv_pkt.swts = utc_to_tai(timespec_to_ns(&tstamp->sw),
-				  prog->utc_tai_offset);
+	if (prog->l2) {
+		struct ethhdr *eth_hdr = (struct ethhdr *)rcvbuf;
+		struct isochron_header *hdr = (struct isochron_header *)(eth_hdr + 1);
+
+		rcv_pkt.tx_time = __be64_to_cpu(hdr->tx_time);
+		rcv_pkt.etype = ntohs(eth_hdr->h_proto);
+		memcpy(rcv_pkt.smac, eth_hdr->h_source, ETH_ALEN);
+		memcpy(rcv_pkt.dmac, eth_hdr->h_dest, ETH_ALEN);
+		rcv_pkt.seqid = __be32_to_cpu(hdr->seqid);
+		rcv_pkt.hwts = timespec_to_ns(&tstamp->hw);
+		rcv_pkt.swts = utc_to_tai(timespec_to_ns(&tstamp->sw),
+					  prog->utc_tai_offset);
+	} else {
+		struct isochron_header *hdr = (struct isochron_header *)rcvbuf;
+
+		rcv_pkt.tx_time = __be64_to_cpu(hdr->tx_time);
+		rcv_pkt.seqid = __be32_to_cpu(hdr->seqid);
+		rcv_pkt.hwts = timespec_to_ns(&tstamp->hw);
+		rcv_pkt.swts = utc_to_tai(timespec_to_ns(&tstamp->sw),
+					  prog->utc_tai_offset);
+	}
 
 	if (rcv_pkt.seqid > prog->iterations) {
 		if (!prog->quiet)
@@ -130,7 +143,7 @@ static int multicast_listen(int fd, unsigned int if_index,
 
 static int server_loop(struct prog_data *prog, void *app_data)
 {
-	struct timestamp tstamp = {0};
+	struct isochron_timestamp tstamp = {0};
 	struct pollfd pfd[2] = {
 		[0] = {
 			.fd = prog->data_fd,
@@ -193,10 +206,12 @@ static int server_loop(struct prog_data *prog, void *app_data)
 				rc = -errno;
 				break;
 			}
-			if (ether_addr_to_u64(prog->dest_mac) &&
-			    ether_addr_to_u64(prog->dest_mac) !=
-			    ether_addr_to_u64(eth_hdr->h_dest))
-				continue;
+			if (prog->l2)
+				if (ether_addr_to_u64(prog->dest_mac) &&
+				    ether_addr_to_u64(prog->dest_mac) !=
+				    ether_addr_to_u64(eth_hdr->h_dest))
+					continue;
+
 			rc = app_loop(app_data, prog->rcvbuf, len, &tstamp);
 			if (rc < 0)
 				break;
@@ -279,6 +294,11 @@ static int prog_init(struct prog_data *prog)
 		.sin_addr.s_addr = htonl(INADDR_ANY),
 		.sin_port = htons(prog->stats_port),
 	};
+	struct sockaddr_in serv_data_addr = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = htonl(INADDR_ANY),
+		.sin_port = htons(prog->data_port),
+	};
 	struct sigaction sa;
 	int sockopt = 1;
 	int rc;
@@ -335,22 +355,30 @@ static int prog_init(struct prog_data *prog)
 		  sizeof(serv_addr));
 	if (rc < 0) {
 		perror("listener: bind");
+		close(prog->stats_listenfd);
 		return -errno;
 	}
 
 	rc = listen(prog->stats_listenfd, 1);
 	if (rc < 0) {
 		perror("listener: listen");
+		close(prog->stats_listenfd);
 		return -errno;
 	}
 
 	if (!prog->etype)
 		prog->etype = ETH_P_ISOCHRON;
 
-	/* Open PF_PACKET socket, listening for the specified EtherType */
-	prog->data_fd = socket(PF_PACKET, SOCK_RAW, htons(prog->etype));
+	if (prog->l2)
+		/* Open PF_PACKET socket, listening for the specified EtherType */
+		prog->data_fd = socket(PF_PACKET, SOCK_RAW, htons(prog->etype));
+	else
+		prog->data_fd = socket(AF_INET, SOCK_DGRAM,
+				       IPPROTO_UDP);
+
 	if (prog->data_fd < 0) {
 		perror("listener: data socket");
+		close(prog->stats_listenfd);
 		return -errno;
 	}
 
@@ -361,21 +389,42 @@ static int prog_init(struct prog_data *prog)
 			sizeof(int));
 	if (rc < 0) {
 		perror("setsockopt");
+		close(prog->stats_listenfd);
 		close(prog->data_fd);
 		return -errno;
 	}
-	/* Bind to device */
-	rc = setsockopt(prog->data_fd, SOL_SOCKET, SO_BINDTODEVICE,
-			prog->if_name, IFNAMSIZ - 1);
-	if (rc < 0) {
-		perror("SO_BINDTODEVICE");
-		close(prog->data_fd);
-		exit(EXIT_FAILURE);
+
+	if (prog->l2) {
+		/* Bind to device */
+		rc = setsockopt(prog->data_fd, SOL_SOCKET, SO_BINDTODEVICE,
+				prog->if_name, IFNAMSIZ - 1);
+		if (rc < 0) {
+			perror("SO_BINDTODEVICE");
+			close(prog->stats_listenfd);
+			close(prog->data_fd);
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		rc = bind(prog->data_fd, (struct sockaddr *)&serv_data_addr,
+			  sizeof(serv_data_addr));
+		if (rc < 0) {
+			perror("bind");
+			close(prog->stats_listenfd);
+			close(prog->data_fd);
+			return -errno;
+		}
 	}
 
-	if (ether_addr_to_u64(prog->dest_mac))
+	if (ether_addr_to_u64(prog->dest_mac)) {
 		rc = multicast_listen(prog->data_fd, prog->if_index,
 				      prog->dest_mac, true);
+		if (rc) {
+			perror("multicast_listen");
+			close(prog->stats_listenfd);
+			close(prog->data_fd);
+			return rc;
+		}
+	}
 
 	if (prog->do_ts)
 		return sk_timestamping_init(prog->data_fd, prog->if_name, 1);
@@ -474,6 +523,22 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 			        .ptr = &prog->utc_tai_offset,
 			},
 			.optional = true,
+		}, {
+			.short_opt = "-2",
+			.long_opt = "--l2",
+			.type = PROG_ARG_BOOL,
+			.boolean_ptr = {
+				.ptr = &prog->l2,
+			},
+			.optional = true,
+		}, {
+			.short_opt = "-4",
+			.long_opt = "--l4",
+			.type = PROG_ARG_BOOL,
+			.boolean_ptr = {
+				.ptr = &prog->l4,
+			},
+			.optional = true,
 		},
 	};
 	int rc;
@@ -502,6 +567,17 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 
 	if (!prog->stats_port)
 		prog->stats_port = ISOCHRON_STATS_PORT;
+
+	if (prog->l2 && prog->l4) {
+		fprintf(stderr, "Choose transport as either L2 or L4!\n");
+		return -EINVAL;
+	}
+
+	if (!prog->l2 && !prog->l4)
+		prog->l2 = true;
+
+	if (!prog->data_port)
+		prog->data_port = ISOCHRON_DATA_PORT;
 
 	/* Default to the old behavior, which was to allocate a 10 MiB
 	 * log buffer given a 56 byte size of struct isochron_rcv_pkt_data

commit 1e057d571351daa34ecf99ca40e25ccad07abc69

isochron: send: use IPv6 header definition from glibc
It looks like in old glibc versions (tested with gcc 5.4 and glibc 2.21),
the kernel headers and the libc headers were not compatible with one
another, resulting in a duplicate definition of struct in6_addr when
both were included in the same program:

"Re: Redefinition of struct in6_addr in <netinet/in.h> and <linux/in6.h>"
https://sourceware.org/legacy-ml/libc-alpha/2013-01/msg00694.html

This was fixed in later glibc versions, but with 2.21 it is still
broken, and isochron consequently fails to build with that redefinition
error.

To maintain compatibility with that libc version, we can simply drop the
kernel-provided linux/in6.h and use the IPv6 header from netinet/in.h.

Fixes: 004c09ddbab4 ("isochron: send UDP packets too")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/send.c
@@ -8,7 +8,6 @@
  * https://sourceforge.net/p/linuxptp/mailman/message/31998404/
  */
 #include <linux/if_packet.h>
-#include <linux/ipv6.h>
 #include <limits.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -21,6 +20,7 @@
 #include <net/if.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <netinet/ip6.h>
 #include <netinet/udp.h>
 #include <errno.h>
 #include <sys/mman.h>
@@ -513,7 +513,7 @@ static int prog_init(struct prog_data *prog)
 	if (prog->ip_destination.family == AF_INET)
 		prog->l4_header_len = sizeof(struct iphdr) + sizeof(struct udphdr);
 	else if (prog->ip_destination.family == AF_INET6)
-		prog->l4_header_len = sizeof(struct ipv6hdr) + sizeof(struct udphdr);
+		prog->l4_header_len = sizeof(struct ip6_hdr) + sizeof(struct udphdr);
 
 	if (prog->l4)
 		prog->tx_len -= sizeof(struct ethhdr) + prog->l4_header_len;