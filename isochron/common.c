NXP/isochron

commit c0c0924b435813d1eea5143ae3483298a78bf6d5

isochron: add support for txtime
The isochron program already has all the concepts in place for making
proper use of SO_TXTIME and tc-etf.

Because we want to do as little work as possible in the fast path, we
preallocate and prepopulate the control message passed to sendto(), and
we just fix up the one thing that changes per packet: the scheduled TX
time.

Loosely based upon:
tools/testing/selftests/net/so_txtime.c
tools/testing/selftests/net/txtimestamp.c

Testing:

tc qdisc add dev ${SEND_IFACE} handle 100: parent root mqprio num_tc 8 \
	queues 1@0 1@1 1@2 1@3 1@4 1@5 1@6 1@7 \
	map 0 1 2 3 4 5 6 7 \
	hw 1
tc qdisc replace dev ${SEND_IFACE} parent 100:6 etf \
	clockid CLOCK_TAI offload delta 50000000
taskset $((1 << 1)) ./isochron send \
	--interface ${SEND_IFACE} \
	--dmac ${RCV_MAC_ADDRESS} \
	--priority 5 \
	--base-time $BASE_TIME \
	--cycle-time 0.001000000 \
	--num-frames ${num_pkts} \
	--frame-size 64 \
	--vid 0 \
	--client 192.168.100.2 \
	--etype 0xdead \
	--sched-rr \
	--sched-priority 98 \
	--txtime \
	--utc-tai-offset 37

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -8,6 +8,7 @@
 #include <linux/net_tstamp.h>
 #include <netinet/ether.h>
 #include <linux/sockios.h>
+#include <linux/errqueue.h>
 #include <sys/syscall.h>
 #include <sys/ioctl.h>
 #include <arpa/inet.h>
@@ -439,14 +440,57 @@ int sk_receive(int fd, void *buf, int buflen, struct timestamp *tstamp,
 			}
 			ts = (struct timespec *) CMSG_DATA(cm);
 			tstamp->hw = ts[2];
-		}
-		if (level == SOL_SOCKET && type == SO_TIMESTAMPNS) {
+		} else if (level == SOL_SOCKET && type == SO_TIMESTAMPNS) {
 			if (cm->cmsg_len < sizeof(*ts)) {
 				fprintf(stderr, "short SO_TIMESTAMPNS message\n");
 				return -1;
 			}
 			ts = (struct timespec *) CMSG_DATA(cm);
 			tstamp->sw = ts[0];
+		} else if (level == SOL_PACKET && type == PACKET_TX_TIMESTAMP) {
+			struct sock_extended_err *sock_err;
+			char txtime_buf[TIMESPEC_BUFSIZ];
+			__u64 txtime;
+
+			sock_err = (struct sock_extended_err *)CMSG_DATA(cm);
+			if (!sock_err)
+				continue;
+
+			switch (sock_err->ee_origin) {
+			case SO_EE_ORIGIN_TIMESTAMPING:
+				/* Normal cmsg received for TX timestamping */
+				break;
+			case SO_EE_ORIGIN_TXTIME:
+				txtime = ((__u64)sock_err->ee_data << 32) +
+					 sock_err->ee_info;
+				ns_sprintf(txtime_buf, txtime);
+
+				switch (sock_err->ee_code) {
+				case SO_EE_CODE_TXTIME_INVALID_PARAM:
+					fprintf(stderr,
+						"packet with txtime %s dropped due to invalid params\n",
+						txtime_buf);
+					return -1;
+				case SO_EE_CODE_TXTIME_MISSED:
+					fprintf(stderr,
+						"packet with txtime %s dropped due to missed deadline\n",
+						txtime_buf);
+					return -1;
+				default:
+					return -1;
+				}
+				break;
+			default:
+				fprintf(stderr,
+					"unknown socket error %d, origin %d code %d: %s\n",
+					sock_err->ee_errno, sock_err->ee_origin,
+					sock_err->ee_code,
+					strerror(sock_err->ee_errno));
+				break;
+			}
+		} else {
+			fprintf(stderr, "unknown cmsg level %d type %d\n",
+				level, type);
 		}
 	}
 

isochron/common.h
@@ -32,6 +32,35 @@ static inline int sched_setattr(pid_t pid, const struct sched_attr *attr,
 	return syscall(SYS_sched_setattr, pid, attr, flags);
 }
 
+#ifndef SO_TXTIME
+#define SO_TXTIME		61
+#define SCM_TXTIME		SO_TXTIME
+
+#ifndef PACKET_TX_TIMESTAMP
+#define PACKET_TX_TIMESTAMP		16
+#endif
+
+struct sock_txtime {
+	clockid_t clockid;
+	uint16_t flags;
+};
+
+enum txtime_flags {
+	SOF_TXTIME_DEADLINE_MODE = (1 << 0),
+	SOF_TXTIME_REPORT_ERRORS = (1 << 1),
+
+	SOF_TXTIME_FLAGS_LAST = SOF_TXTIME_REPORT_ERRORS,
+	SOF_TXTIME_FLAGS_MASK = (SOF_TXTIME_FLAGS_LAST - 1) |
+				 SOF_TXTIME_FLAGS_LAST
+};
+#endif
+
+#ifndef SO_EE_ORIGIN_TXTIME
+#define SO_EE_ORIGIN_TXTIME		6
+#define SO_EE_CODE_TXTIME_INVALID_PARAM	1
+#define SO_EE_CODE_TXTIME_MISSED	2
+#endif
+
 struct isochron_header {
 	__s64			tx_time;
 	__s64			wakeup;

isochron/send.c
@@ -26,6 +26,7 @@
 #include <time.h>
 #include <math.h>
 #include "common.h"
+#include <linux/net_tstamp.h>
 
 #define BUF_SIZ		10000
 #define TIME_FMT_LEN	27 /* "[%s] " */
@@ -35,6 +36,10 @@ struct prog_data {
 	__u8 src_mac[ETH_ALEN];
 	char if_name[IFNAMSIZ];
 	char sendbuf[BUF_SIZ];
+	struct cmsghdr *cmsg;
+	struct iovec iov;
+	struct msghdr msg;
+	char msg_control[CMSG_SPACE(sizeof(__s64))];
 	char stats_srv_addr[INET6_ADDRSTRLEN];
 	struct sockaddr_ll socket_address;
 	struct isochron_log log;
@@ -59,6 +64,8 @@ struct prog_data {
 	char tracebuf[BUF_SIZ];
 	long port;
 	bool taprio;
+	bool txtime;
+	bool deadline;
 	bool do_vlan;
 	int l2_header_len;
 	bool sched_fifo;
@@ -152,12 +159,15 @@ static int do_work(struct prog_data *prog, int iteration, __s64 scheduled)
 	hdr->wakeup = __cpu_to_be64(now);
 	hdr->seqid = __cpu_to_be32(iteration);
 
+	if (prog->txtime)
+		*((__u64 *)CMSG_DATA(prog->cmsg)) = (__u64)(scheduled);
+
 	/* Send packet */
-	rc = sendto(prog->data_fd, prog->sendbuf, prog->tx_len, 0,
-		    (const struct sockaddr *)&prog->socket_address,
-		    sizeof(struct sockaddr_ll));
+	rc = sendmsg(prog->data_fd, &prog->msg, 0);
 	if (rc < 0) {
-		perror("send\n");
+		perror("sendmsg");
+		sk_receive(prog->data_fd, err_pkt, BUF_SIZ, NULL,
+			   MSG_ERRQUEUE, 0);
 		return rc;
 	}
 
@@ -342,6 +352,23 @@ static int prog_init(struct prog_data *prog)
 		goto out_close_data_fd;
 	}
 
+	if (prog->txtime) {
+		static struct sock_txtime sk_txtime = {
+			.clockid = CLOCK_TAI,
+			.flags = SOF_TXTIME_REPORT_ERRORS,
+		};
+
+		if (prog->deadline)
+			sk_txtime.flags |= SOF_TXTIME_DEADLINE_MODE;
+
+		rc = setsockopt(prog->data_fd, SOL_SOCKET, SO_TXTIME,
+				&sk_txtime, sizeof(sk_txtime));
+		if (rc) {
+			perror("setsockopt");
+			goto out_close_data_fd;
+		}
+	}
+
 	/* Get the index of the interface to send on */
 	memset(&if_idx, 0, sizeof(struct ifreq));
 	strncpy(if_idx.ifr_name, prog->if_name, IFNAMSIZ - 1);
@@ -453,6 +480,25 @@ static int prog_init(struct prog_data *prog)
 		prog->sendbuf[i++] = 0xad;
 		prog->sendbuf[i++] = 0xbe;
 		prog->sendbuf[i++] = 0xef;
+	}
+
+	prog->iov.iov_base = prog->sendbuf;
+	prog->iov.iov_len = prog->tx_len;
+
+	memset(&prog->msg, 0, sizeof(prog->msg));
+	prog->msg.msg_name = (struct sockaddr *)&prog->socket_address;
+	prog->msg.msg_namelen = sizeof(struct sockaddr_ll);
+	prog->msg.msg_iov = &prog->iov;
+	prog->msg.msg_iovlen = 1;
+
+	if (prog->txtime) {
+		prog->msg.msg_control = prog->msg_control;
+		prog->msg.msg_controllen = sizeof(prog->msg_control);
+
+		prog->cmsg = CMSG_FIRSTHDR(&prog->msg);
+		prog->cmsg->cmsg_level = SOL_SOCKET;
+		prog->cmsg->cmsg_type = SCM_TXTIME;
+		prog->cmsg->cmsg_len = CMSG_LEN(sizeof(__u64));
 	}
 
 	/* Drain potentially old packets from the isochron receiver */
@@ -585,7 +631,8 @@ static void isochron_process_stat(struct prog_data *prog,
 
 	entry->seqid = send_pkt->seqid;
 	entry->wakeup_to_hw_ts = send_pkt->hwts - send_pkt->wakeup;
-	/* When IEEE 802.1Qbv (tc-taprio offload) is enabled, we know that the
+	entry->hw_rx_deadline_delta = rcv_pkt->hwts - rcv_pkt->tx_time;
+	/* When tc-taprio or tc-etf offload is enabled, we know that the
 	 * MAC TX timestamp will be larger than the gate event, because the
 	 * application's schedule should be the same as the NIC's schedule.
 	 * The NIC will buffer that packet until the gate opens, something
@@ -600,10 +647,10 @@ static void isochron_process_stat(struct prog_data *prog,
 	 * budget, i.e. "how much we could still reduce the cycle time without
 	 * losing deadlines".
 	 */
-	if (prog->taprio)
-		entry->hw_rx_deadline_delta = rcv_pkt->hwts - rcv_pkt->tx_time;
+	if (prog->taprio || prog->txtime)
+		entry->latency_budget = send_pkt->hwts - send_pkt->tx_time;
 	else
-		entry->latency_budget = send_pkt->tx_time - rcv_pkt->hwts;
+		entry->latency_budget = send_pkt->tx_time - send_pkt->hwts;
 	entry->path_delay = rcv_pkt->hwts - send_pkt->hwts;
 	entry->wakeup_latency = send_pkt->wakeup - (send_pkt->tx_time -
 						    prog->advance_time);
@@ -716,17 +763,20 @@ static void isochron_print_stats(struct prog_data *prog,
 				path_delay), "Path delay");
 	isochron_print_one_stat(&stats, offsetof(struct isochron_stat_entry,
 				wakeup_to_hw_ts), "Wakeup to HW TX timestamp");
-	if (prog->taprio)
+	isochron_print_one_stat(&stats, offsetof(struct isochron_stat_entry,
+				hw_rx_deadline_delta), "HW RX deadline delta (TX time to HW RX timestamp)");
+	if (prog->taprio || prog->txtime)
 		isochron_print_one_stat(&stats, offsetof(struct isochron_stat_entry,
-					hw_rx_deadline_delta), "HW RX deadline delta");
+					latency_budget), "MAC latency (TX time to HW TX timestamp)");
 	else
 		isochron_print_one_stat(&stats, offsetof(struct isochron_stat_entry,
-					latency_budget), "Latency budget");
+					latency_budget), "Application latency budget (HW TX timestamp to TX time)");
+
 	isochron_print_one_stat(&stats, offsetof(struct isochron_stat_entry,
 				wakeup_latency), "Wakeup latency");
 	isochron_print_one_stat(&stats, offsetof(struct isochron_stat_entry,
 				arrival_latency), "Arrival latency (HW RX timestamp to application)");
-	if (!prog->taprio)
+	if (!prog->taprio && !prog->txtime)
 		printf("HW TX deadline misses: %d (%.3lf%%)\n",
 		       stats.hw_tx_deadline_misses,
 		       100.0f * stats.hw_tx_deadline_misses / stats.frame_count);
@@ -936,6 +986,22 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 			},
 			.optional = true,
 		}, {
+			.short_opt = "-x",
+			.long_opt = "--txtime",
+			.type = PROG_ARG_BOOL,
+			.boolean_ptr = {
+			        .ptr = &prog->txtime,
+			},
+			.optional = true,
+		}, {
+			.short_opt = "-D",
+			.long_opt = "--deadline",
+			.type = PROG_ARG_BOOL,
+			.boolean_ptr = {
+			        .ptr = &prog->deadline,
+			},
+			.optional = true,
+		}, {
 			.short_opt = "-H",
 			.long_opt = "--sched-priority",
 			.type = PROG_ARG_LONG,
@@ -994,6 +1060,17 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 	} else {
 		prog->do_vlan = true;
 		prog->l2_header_len = sizeof(struct vlan_ethhdr);
+	}
+
+	if (prog->txtime && prog->taprio) {
+		fprintf(stderr,
+			"Cannot enable txtime and taprio mode at the same time\n");
+		return -EINVAL;
+	}
+
+	if (prog->deadline && !prog->txtime) {
+		fprintf(stderr, "Deadline mode supported only with txtime\n");
+		return -EINVAL;
 	}
 
 	/* No point in leaving this one's default to zero, if we know that

commit f013146b8451f8b504a8598d5d220eb80430a8c6

isochron: fix sendmsg error path
When sendmsg fails with SO_TXTIME, we will call sk_receive on the data
socket's error queue in order to retrieve a relevant kernel message
about our misconfiguration.

All is well, except the kernel also provides us timestamps in the cmsg,
but we aren't prepared to handle them: we've passed a NULL pointer for
the timestamp structure.

So guard against accessing a NULL timestamp pointer in sk_receive.

Fixes: c0c0924b4358 ("isochron: add support for txtime")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -462,14 +462,16 @@ int sk_receive(int fd, void *buf, int buflen, struct isochron_timestamp *tstamp,
 				return -1;
 			}
 			ts = (struct timespec *) CMSG_DATA(cm);
-			tstamp->hw = ts[2];
+			if (tstamp)
+				tstamp->hw = ts[2];
 		} else if (level == SOL_SOCKET && type == SO_TIMESTAMPNS) {
 			if (cm->cmsg_len < sizeof(*ts)) {
 				fprintf(stderr, "short SO_TIMESTAMPNS message\n");
 				return -1;
 			}
 			ts = (struct timespec *) CMSG_DATA(cm);
-			tstamp->sw = ts[0];
+			if (tstamp)
+				tstamp->sw = ts[0];
 		} else if (level == SOL_PACKET && type == PACKET_TX_TIMESTAMP) {
 			struct sock_extended_err *sock_err;
 			char txtime_buf[TIMESPEC_BUFSIZ];