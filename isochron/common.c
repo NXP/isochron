NXP/isochron

commit e03fa12cbcef250665341a6c9abfa49437d8f0de

isochron: transmit the log buffer size when collecting statistics
This is a more wide-ranging change, because we need to be more careful
about the size of the log buffer. Currently it can happen that the
static allocation is not enough, for large tests. So we need to make the
buffer size configurable, depending on the size of the test.

The first 4 bytes of data transmitted on the stats fd are now the size
of the buffer (before, that was implicit based on when the server would
close the TCP connection).

We are also adding an argument to the receiver, which says how many
packets to expect per test. This indicates how large the buffer needs to
be.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -26,8 +26,6 @@
 #include <stdarg.h>
 #include "common.h"
 
-#define LOGBUF_SIZ	(10 * 1024 * 1024) /* 10 MiB */
-
 int mac_addr_from_string(__u8 *to, char *from)
 {
 	unsigned long byte;
@@ -449,9 +447,9 @@ int sk_receive(int fd, void *buf, int buflen, struct timestamp *tstamp,
 	return len;
 }
 
-int isochron_log_init(struct isochron_log *log)
+int isochron_log_init(struct isochron_log *log, size_t size)
 {
-	log->buf = calloc(sizeof(char), LOGBUF_SIZ);
+	log->buf = calloc(sizeof(char), size);
 	if (!log->buf)
 		return -ENOMEM;
 
@@ -475,6 +473,13 @@ int isochron_log_xmit(struct isochron_log *log, int fd)
 	char *pos;
 	int rc;
 
+	rc = write(fd, &log->buf_len, sizeof(log->buf_len));
+	if (rc < 0) {
+		fprintf(stderr, "buf_len write returned %d: %s\n",
+			errno, strerror(errno));
+		return -errno;
+	}
+
 	while ((rc = write(fd, p, cnt)) > 0) {
 		cnt -= rc;
 		p += rc;
@@ -491,22 +496,47 @@ int isochron_log_xmit(struct isochron_log *log, int fd)
 
 int isochron_log_recv(struct isochron_log *log, int fd)
 {
-	char *p = log->buf;
-	char *pos;
+	char *pos, *p;
+	int buf_len;
 	int rc;
 
-	while ((rc = read(fd, p, LOGBUF_SIZ)) > 0) {
+	rc = read(fd, &buf_len, sizeof(buf_len));
+	if (rc < 0) {
+		fprintf(stderr, "could not read buffer length: %d: %s\n",
+			-rc, strerror(-rc));
+		return rc;
+	}
+
+	if (buf_len <= 0) {
+		fprintf(stderr, "invalid buffer length: %d\n", buf_len);
+		return -ERANGE;
+	}
+
+	rc = isochron_log_init(log, buf_len);
+	if (rc)
+		return rc;
+
+	log->buf_len = buf_len;
+	p = log->buf;
+
+	while ((rc = read(fd, p, buf_len)) > 0 && buf_len) {
 		p += rc;
-		log->buf_len += rc;
+		buf_len -= rc;
 	}
 
 	if (rc < 0) {
 		fprintf(stderr, "read returned %d: %s\n",
 			errno, strerror(errno));
-		rc = -errno;
+		return -errno;
 	}
 
-	return rc;
+	if (buf_len) {
+		fprintf(stderr, "%d unread bytes from receive buffer\n",
+			buf_len);
+		return -EIO;
+	}
+
+	return 0;
 }
 
 void isochron_log_teardown(struct isochron_log *log)

isochron/common.h
@@ -113,7 +113,7 @@ struct isochron_log {
 	char *buf;
 };
 
-int isochron_log_init(struct isochron_log *log);
+int isochron_log_init(struct isochron_log *log, size_t size);
 void isochron_log_data(struct isochron_log *log, void *data, int len);
 int isochron_log_xmit(struct isochron_log *log, int fd);
 int isochron_log_recv(struct isochron_log *log, int fd);

isochron/rcv.c
@@ -35,6 +35,7 @@ struct prog_data {
 	bool quiet;
 	long etype;
 	long port;
+	long iterations;
 };
 
 int signal_received;
@@ -61,6 +62,12 @@ static int app_loop(void *app_data, char *rcvbuf, size_t len,
 	rcv_pkt.seqid = __be32_to_cpu(hdr->seqid);
 	rcv_pkt.hwts = timespec_to_ns(&tstamp->hw);
 	rcv_pkt.swts = timespec_to_ns(&tstamp->sw);
+
+	if (rcv_pkt.seqid > prog->iterations) {
+		if (!prog->quiet)
+			printf("Discarding seqid %d\n", rcv_pkt.seqid);
+		return -1;
+	}
 
 	isochron_log_data(&prog->log, &rcv_pkt, sizeof(rcv_pkt));
 
@@ -199,7 +206,8 @@ static int server_loop(struct prog_data *prog, void *app_data)
 
 			isochron_log_xmit(&prog->log, stats_fd);
 			isochron_log_teardown(&prog->log);
-			rc = isochron_log_init(&prog->log);
+			rc = isochron_log_init(&prog->log, prog->iterations *
+					       sizeof(struct isochron_rcv_pkt_data));
 			if (rc < 0)
 				break;
 
@@ -235,7 +243,8 @@ static int prog_init(struct prog_data *prog)
 	int sockopt = 1;
 	int rc;
 
-	rc = isochron_log_init(&prog->log);
+	rc = isochron_log_init(&prog->log, prog->iterations *
+			       sizeof(struct isochron_rcv_pkt_data));
 	if (rc < 0)
 		return rc;
 
@@ -385,6 +394,14 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 				.ptr = &prog->port,
 			},
 			.optional = true,
+		}, {
+			.short_opt = "-n",
+			.long_opt = "--num-frames",
+			.type = PROG_ARG_LONG,
+			.long_ptr = {
+				.ptr = &prog->iterations,
+			},
+			.optional = true,
 		},
 	};
 	int rc;
@@ -405,6 +422,12 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 
 	if (!prog->port)
 		prog->port = ISOCHRON_STATS_PORT;
+
+	/* Default to the old behavior, which was to allocate a 10 MiB
+	 * log buffer given a 56 byte size of struct isochron_rcv_pkt_data
+	 */
+	if (!prog->iterations)
+		prog->iterations = 187245;
 
 	return 0;
 }

isochron/send.c
@@ -377,7 +377,8 @@ static int prog_init(struct prog_data *prog)
 	ns_sprintf(now_buf, utc_to_tai(now));
 	fprintf(stderr, "%10s: %s\n", "Now", now_buf);
 
-	rc = isochron_log_init(&prog->log);
+	rc = isochron_log_init(&prog->log, prog->iterations *
+			       sizeof(struct isochron_send_pkt_data));
 	if (rc < 0)
 		return rc;
 
@@ -665,7 +666,6 @@ static int prog_teardown(struct prog_data *prog)
 
 		printf("Collecting receiver stats\n");
 
-		isochron_log_init(&rcv_log);
 		rc = prog_collect_rcv_stats(prog, &rcv_log);
 		if (rc)
 			return rc;

commit 4597534419d009d8a9a7ccda543cae0f47b4ea7b

isochron: avoid memory leaks on errors in isochron_log_recv
Now that the log buffer is configurable in size, it is also dynamically
allocated by isochron_log_init(). Therefore, we must pair that with
isochron_log_teardown() on error paths.

Fixes: e03fa12cbcef ("isochron: transmit the log buffer size when collecting statistics")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -533,6 +533,11 @@ int isochron_log_init(struct isochron_log *log, size_t size)
 	return 0;
 }
 
+void isochron_log_teardown(struct isochron_log *log)
+{
+	free(log->buf);
+}
+
 void isochron_log_data(struct isochron_log *log, void *data, int len)
 {
 	char *p = log->buf + log->buf_len;
@@ -602,21 +607,18 @@ int isochron_log_recv(struct isochron_log *log, int fd)
 	if (rc < 0) {
 		fprintf(stderr, "read returned %d: %s\n",
 			errno, strerror(errno));
+		isochron_log_teardown(log);
 		return -errno;
 	}
 
 	if (buf_len) {
 		fprintf(stderr, "%d unread bytes from receive buffer\n",
 			buf_len);
+		isochron_log_teardown(log);
 		return -EIO;
 	}
 
 	return 0;
-}
-
-void isochron_log_teardown(struct isochron_log *log)
-{
-	free(log->buf);
 }
 
 void isochron_rcv_log_print(struct isochron_log *log)