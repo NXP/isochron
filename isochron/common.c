NXP/isochron

commit 44b125cbe47dff33adb6adeb2e86a11b12ac1139

isochron: make sk_timestamping_init take a bool argument
Stop treating boolean values as integers and use the dedicated data type.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -360,7 +360,7 @@ static int hwts_init(int fd, const char *if_name, int rx_filter, int tx_type)
 	return 0;
 }
 
-int sk_timestamping_init(int fd, const char *if_name, int on)
+int sk_timestamping_init(int fd, const char *if_name, bool on)
 {
 	int rc, filter, flags, tx_type;
 

isochron/common.h
@@ -299,7 +299,7 @@ struct isochron_stats {
 };
 
 int mac_addr_from_string(__u8 *to, char *from);
-int sk_timestamping_init(int fd, const char *if_name, int on);
+int sk_timestamping_init(int fd, const char *if_name, bool on);
 int sk_receive(int fd, void *buf, int buflen, struct isochron_timestamp *tstamp,
 	       int flags, int timeout);
 __s64 timespec_to_ns(const struct timespec *ts);

isochron/rcv.c
@@ -423,7 +423,7 @@ static int prog_init(struct prog_data *prog)
 	}
 
 	if (prog->do_ts)
-		return sk_timestamping_init(prog->data_fd, prog->if_name, 1);
+		return sk_timestamping_init(prog->data_fd, prog->if_name, true);
 
 	return 0;
 }

isochron/send.c
@@ -551,7 +551,7 @@ static int prog_init(struct prog_data *prog)
 	}
 
 	if (prog->do_ts) {
-		rc = sk_timestamping_init(prog->data_fd, prog->if_name, 1);
+		rc = sk_timestamping_init(prog->data_fd, prog->if_name, true);
 		if (rc < 0)
 			goto out_munlock;
 	}

commit a3576ce44b4927870d07be710a10db79a78f146b

isochron: repair sk_timestamping_init breakage
The "int on" was actually passed to setsockopt() which did expect an
int-sized argument. So enabling timestamping was broken.

Fixes: 44b125cbe47d ("isochron: make sk_timestamping_init take a bool argument")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -387,6 +387,7 @@ static int hwts_init(int fd, const char *if_name, int rx_filter, int tx_type)
 int sk_timestamping_init(int fd, const char *if_name, bool on)
 {
 	int rc, filter, flags, tx_type;
+	int enabled = on ? 1 : 0;
 
 	flags = SOF_TIMESTAMPING_TX_HARDWARE |
 		SOF_TIMESTAMPING_RX_HARDWARE |
@@ -412,7 +413,8 @@ int sk_timestamping_init(int fd, const char *if_name, bool on)
 		return -1;
 	}
 
-	rc = setsockopt(fd, SOL_SOCKET, SO_TIMESTAMPNS, &on, sizeof(on));
+	rc = setsockopt(fd, SOL_SOCKET, SO_TIMESTAMPNS, &enabled,
+			sizeof(enabled));
 	if (rc < 0) {
 		fprintf(stderr, "ioctl SO_TIMESTAMPNS failed: %s\n",
 			strerror(errno));