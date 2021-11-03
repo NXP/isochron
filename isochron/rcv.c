NXP/isochron

commit f725db8df63ee35e4d6bb106e8cef75585a8fcc9

isochron: add a secondary stats socket between receiver and sender
This is used to transmit the log buffer at the moment. In a future patch
the log buffer will be parsed in order to calculate statistics directly
from the C program, instead of relying on an external time_test.py.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/rcv.c
@@ -17,6 +17,7 @@
 #include <signal.h>
 #include <errno.h>
 #include <time.h>
+#include <poll.h>
 #include "common.h"
 
 #define BUF_SIZ		1522
@@ -28,6 +29,7 @@ struct prog_data {
 	__u8 rcvbuf[BUF_SIZ];
 	struct rtprint rt;
 	clockid_t clkid;
+	int stats_listenfd;
 	int data_fd;
 	bool do_ts;
 };
@@ -125,42 +127,93 @@ static int multicast_listen(int fd, unsigned int if_index,
 
 static int server_loop(struct prog_data *prog, void *app_data)
 {
-	struct ethhdr *eth_hdr = (struct ethhdr *)prog->rcvbuf;
 	struct timestamp tstamp = {0};
+	struct pollfd pfd[2] = {
+		[0] = {
+			.fd = prog->data_fd,
+			.events = POLLIN | POLLERR | POLLPRI,
+		},
+		[1] = {
+			.fd = prog->stats_listenfd,
+			.events = POLLIN | POLLERR | POLLPRI,
+		},
+	};
 	ssize_t len;
 	int rc = 0;
+	int cnt;
 
 	do {
-		len = sk_receive(prog->data_fd, prog->rcvbuf, BUF_SIZ, &tstamp,
-				 0, TXTSTAMP_TIMEOUT_MS);
-		/* Suppress "Interrupted system call" message */
-		if (len < 0 && errno != EINTR) {
-			fprintf(stderr, "recvfrom returned %d: %s\n",
-				errno, strerror(errno));
-			rc = -errno;
+		cnt = poll(pfd, ARRAY_SIZE(pfd), -1);
+		if (cnt < 0) {
+			if (errno == EINTR) {
+				break;
+			} else {
+				fprintf(stderr, "poll returned %d: %s\n",
+					errno, strerror(errno));
+				rc = -errno;
+				break;
+			}
+		} else if (!cnt) {
 			break;
 		}
-		if (ether_addr_to_u64(prog->dest_mac) &&
-		    ether_addr_to_u64(prog->dest_mac) !=
-		    ether_addr_to_u64(eth_hdr->h_dest))
-			continue;
-		rc = app_loop(app_data, prog->rcvbuf, len, &tstamp);
-		if (rc < 0)
-			break;
+
+		if (pfd[0].revents & (POLLIN | POLLERR | POLLPRI)) {
+			struct ethhdr *eth_hdr = (struct ethhdr *)prog->rcvbuf;
+
+			len = sk_receive(prog->data_fd, prog->rcvbuf,
+					 BUF_SIZ, &tstamp, 0,
+					 TXTSTAMP_TIMEOUT_MS);
+			/* Suppress "Interrupted system call" message */
+			if (len < 0 && errno != EINTR) {
+				fprintf(stderr, "recvfrom returned %d: %s\n",
+					errno, strerror(errno));
+				rc = -errno;
+				break;
+			}
+			if (ether_addr_to_u64(prog->dest_mac) &&
+			    ether_addr_to_u64(prog->dest_mac) !=
+			    ether_addr_to_u64(eth_hdr->h_dest))
+				continue;
+			rc = app_loop(app_data, prog->rcvbuf, len, &tstamp);
+			if (rc < 0)
+				break;
+		}
+
+		if (pfd[1].revents & (POLLIN | POLLERR | POLLPRI)) {
+			char client_addr[INET6_ADDRSTRLEN];
+			struct sockaddr_in addr;
+			socklen_t addr_len;
+			int stats_fd;
+
+			addr_len = sizeof(struct sockaddr_in);
+			rc = accept(prog->stats_listenfd,
+				    (struct sockaddr *)&addr, &addr_len);
+			if (rc < 0) {
+				if (errno != EINTR)
+					fprintf(stderr, "accept returned %d: %s\n",
+						errno, strerror(errno));
+				return -errno;
+			}
+
+			if (!inet_ntop(addr.sin_family, &addr.sin_addr.s_addr,
+				       client_addr, addr_len)) {
+				fprintf(stderr, "inet_pton returned %d: %s\n",
+					errno, strerror(errno));
+				return -errno;
+			}
+
+			printf("Accepted connection from %s\n", client_addr);
+
+			stats_fd = rc;
+
+			rtflush(&prog->rt, stats_fd);
+			rtflush(&prog->rt, 0);
+
+			close(stats_fd);
+		}
 		if (signal_received)
 			break;
 	} while (1);
-
-	/* Avoid the nanosecond portion of last output line
-	 * from getting truncated when process is killed
-	 */
-	fflush(stdout);
-
-	close(prog->data_fd);
-
-	if (ether_addr_to_u64(prog->dest_mac))
-		rc = multicast_listen(prog->data_fd, prog->if_index,
-				      prog->dest_mac, false);
 
 	return rc;
 }
@@ -179,6 +232,11 @@ void sig_handler(int signo)
 
 static int prog_init(struct prog_data *prog)
 {
+	struct sockaddr_in serv_addr = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = htonl(INADDR_ANY),
+		.sin_port = htons(ISOCHRON_STATS_PORT),
+	};
 	struct sigaction sa;
 	int sockopt = 1;
 	int rc;
@@ -213,10 +271,40 @@ static int prog_init(struct prog_data *prog)
 		return -errno;
 	}
 
+	prog->stats_listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (prog->stats_listenfd < 0) {
+		perror("listener: stats socket");
+		return -errno;
+	}
+
+	/* Allow the socket to be reused, in case the connection
+	 * is closed prematurely
+	 */
+	rc = setsockopt(prog->stats_listenfd, SOL_SOCKET, SO_REUSEADDR, &sockopt,
+			sizeof(int));
+	if (rc < 0) {
+		perror("setsockopt");
+		close(prog->stats_listenfd);
+		return -errno;
+	}
+
+	rc = bind(prog->stats_listenfd, (struct sockaddr*)&serv_addr,
+		  sizeof(serv_addr));
+	if (rc < 0) {
+		perror("listener: bind");
+		return -errno;
+	}
+
+	rc = listen(prog->stats_listenfd, 1);
+	if (rc < 0) {
+		perror("listener: listen");
+		return -errno;
+	}
+
 	/* Open PF_PACKET socket, listening for EtherType ETH_P_TSN */
 	prog->data_fd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_TSN));
 	if (prog->data_fd < 0) {
-		perror("listener: socket");
+		perror("listener: data socket");
 		return -errno;
 	}
 
@@ -299,8 +387,14 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 
 static int prog_teardown(struct prog_data *prog)
 {
-	rtflush(&prog->rt);
+	rtflush(&prog->rt, STDOUT_FILENO);
 	rtprint_teardown(&prog->rt);
+	close(prog->stats_listenfd);
+	close(prog->data_fd);
+
+	if (ether_addr_to_u64(prog->dest_mac))
+		multicast_listen(prog->data_fd, prog->if_index,
+				 prog->dest_mac, false);
 
 	return 0;
 }

isochron/common.c
@@ -473,14 +473,36 @@ int rtprintf(struct rtprint *rt, char *fmt, ...)
 	return rc;
 }
 
-void rtflush(struct rtprint *rt)
+void rtflush(struct rtprint *rt, int fd)
 {
 	int rc, i = 0;
 
 	while (i < rt->log_buf_len && rt->log_buf[i]) {
-		rc = printf("%s", rt->log_buf + i);
+		rc = dprintf(fd, "%s", rt->log_buf + i);
 		i += (rc + 1);
 	}
+}
+
+int rtprint_rcv(struct rtprint *rt, int fd)
+{
+	char *p = rt->log_buf;
+	char *pos;
+	int rc;
+
+	while ((rc = read(fd, p, LOGBUF_SIZ)) > 0) {
+		p += rc;
+		*p = '\0';
+		p++;
+		rt->log_buf_len += rc + 1;
+	}
+
+	if (rc < 0) {
+		fprintf(stderr, "read returned %d: %s\n",
+			errno, strerror(errno));
+		rc = -errno;
+	}
+
+	return rc;
 }
 
 void rtprint_teardown(struct rtprint *rt)

isochron/common.h
@@ -88,8 +88,11 @@ struct rtprint {
 
 int rtprint_init(struct rtprint *rt);
 int rtprintf(struct rtprint *rt, char *fmt, ...);
-void rtflush(struct rtprint *rt);
+int rtprint_rcv(struct rtprint *rt, int fd);
+void rtflush(struct rtprint *rt, int fd);
 void rtprint_teardown(struct rtprint *rt);
+
+#define ISOCHRON_STATS_PORT	5000
 
 #define VLAN_PRIO_MASK		0xe000 /* Priority Code Point */
 #define VLAN_PRIO_SHIFT		13

isochron/send.c
@@ -29,6 +29,7 @@ struct prog_data {
 	__u8 src_mac[ETH_ALEN];
 	char if_name[IFNAMSIZ];
 	char sendbuf[BUF_SIZ];
+	char stats_srv_addr[INET6_ADDRSTRLEN];
 	struct sockaddr_ll socket_address;
 	struct rtprint rt;
 	long timestamped;
@@ -334,8 +335,11 @@ static int prog_init(struct prog_data *prog)
 		return rc;
 	}
 
-	if (prog->do_ts)
-		return sk_timestamping_init(prog->data_fd, prog->if_name, 1);
+	if (prog->do_ts) {
+		rc = sk_timestamping_init(prog->data_fd, prog->if_name, 1);
+		if (rc < 0)
+			return rc;
+	}
 
 	/* Packet data */
 	while (i < prog->tx_len) {
@@ -348,12 +352,64 @@ static int prog_init(struct prog_data *prog)
 	return 0;
 }
 
-static int prog_teardown(struct prog_data *prog)
+static int prog_collect_rcv_stats(struct prog_data *prog)
 {
-	rtflush(&prog->rt);
-	rtprint_teardown(&prog->rt);
+	struct sockaddr_in serv_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(ISOCHRON_STATS_PORT),
+	};
+	struct rtprint rcv_rt;
+	int stats_fd;
+	int rc;
+
+	rc = inet_pton(serv_addr.sin_family, prog->stats_srv_addr,
+		       &serv_addr.sin_addr);
+	if (rc <= 0) {
+		if (rc == 0)
+			fprintf(stderr, "%s not in presentation format",
+				prog->stats_srv_addr);
+		else
+			fprintf(stderr, "inet_pton returned %d: %s\n",
+				errno, strerror(errno));
+		return -EAFNOSUPPORT;
+	}
+
+	stats_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (stats_fd < 0) {
+		fprintf(stderr, "socket returned %d: %s\n",
+			errno, strerror(errno));
+		return -errno;
+	}
+
+	rc = connect(stats_fd, (struct sockaddr *)&serv_addr,
+		     sizeof(serv_addr));
+	if (rc < 0) {
+		fprintf(stderr, "connect returned %d: %s\n",
+			errno, strerror(errno));
+		return -errno;
+	}
+
+	printf("Collecting receiver stats\n");
+	rtprint_init(&rcv_rt);
+	rtprint_rcv(&rcv_rt, stats_fd);
+	rtflush(&rcv_rt, STDOUT_FILENO);
 
 	return 0;
+}
+
+static int prog_teardown(struct prog_data *prog)
+{
+	int rc;
+
+	rtflush(&prog->rt, STDOUT_FILENO);
+
+	rc = prog_collect_rcv_stats(prog);
+	if (rc)
+		return rc;
+
+	rtprint_teardown(&prog->rt);
+
+	return rc;
 }
 
 static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
@@ -453,6 +509,14 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 				.ptr = &prog->vid,
 			},
 			.optional = true,
+		}, {
+			.short_opt = "-C",
+			.long_opt = "--client",
+			.type = PROG_ARG_STRING,
+			.string = {
+				.buf = prog->stats_srv_addr,
+				.size = INET6_ADDRSTRLEN,
+			},
 		},
 	};
 	int rc;

commit 6aea3bce6fdb80435782e3a7bb49b41b3b1f4754

isochron: rcv: don't disable multicast listening after closing the data_fd
./isochron rcv --interface swp2 --dmac 01:80:c2:00:00:0e
^Csetsockopt PACKET_MR_MULTICAST failed: Bad file descriptor
setsockopt PACKET_MR_ALLMULTI failed: Bad file descriptor
setsockopt PACKET_MR_PROMISC failed: Bad file descriptor
all socket options failed

Fixes: f725db8df63e ("isochron: add a secondary stats socket between receiver and sender")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/rcv.c
@@ -639,12 +639,13 @@ static int prog_teardown(struct prog_data *prog)
 	if (!prog->quiet)
 		isochron_rcv_log_print(&prog->log);
 	isochron_log_teardown(&prog->log);
-	close(prog->stats_listenfd);
-	close(prog->data_fd);
 
 	if (ether_addr_to_u64(prog->dest_mac))
 		multicast_listen(prog->data_fd, prog->if_index,
 				 prog->dest_mac, false);
+
+	close(prog->stats_listenfd);
+	close(prog->data_fd);
 
 	return 0;
 }