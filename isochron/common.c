NXP/isochron

commit 505df8ed8c3399655d8ee017bb308c58b16e3294

isochron: transmit the log buffer using a reply TLV as well
Right now, we create a GET TLV from the sender, but the receiver still
sends the log buffer bluntly.

Make the sender expect a RESPONSE TLV from the receiver, and make the
receiver create that TLV.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -761,6 +761,39 @@ void isochron_log_remove(struct isochron_log *log, void *p, int len)
 	log->buf_len -= len;
 }
 
+int isochron_send_tlv(int fd, enum isochron_management_action action,
+		      enum isochron_management_id mid, size_t size)
+{
+	struct isochron_management_message *msg;
+	unsigned char buf[BUFSIZ];
+	struct isochron_tlv *tlv;
+	ssize_t len;
+
+	msg = (struct isochron_management_message *)buf;
+	msg->version = ISOCHRON_MANAGEMENT_VERSION;
+	msg->action = action;
+	msg->payload_length = __cpu_to_be32(sizeof(*tlv) + size);
+
+	tlv = (struct isochron_tlv *)(msg + 1);
+	tlv->tlv_type = htons(ISOCHRON_TLV_MANAGEMENT);
+	tlv->management_id = htons(mid);
+	tlv->length_field = __cpu_to_be32(size);
+
+	len = write_exact(fd, buf, sizeof(*msg) + sizeof(*tlv));
+	if (len < 0)
+		return len;
+	if (len == 0)
+		return -ECONNRESET;
+	return 0;
+}
+
+size_t isochron_log_buf_tlv_size(struct isochron_log *log)
+{
+	return sizeof(__be32) + /* log_version */
+	       sizeof(__be32) + /* buf_len */
+	       log->buf_len;
+}
+
 static const char * const trace_marker_paths[] = {
 	"/sys/kernel/debug/tracing/trace_marker",
 	"/debug/tracing/trace_marker",

isochron/common.h
@@ -207,6 +207,11 @@ struct isochron_tlv {
 	__be32		length_field;
 } __attribute((packed));
 
+size_t isochron_log_buf_tlv_size(struct isochron_log *log);
+
+int isochron_send_tlv(int fd, enum isochron_management_action action,
+		      enum isochron_management_id mid, size_t size);
+
 #define ISOCHRON_STATS_PORT	5000 /* TCP */
 #define ISOCHRON_DATA_PORT	6000 /* UDP */
 #define ISOCHRON_LOG_VERSION	3

isochron/rcv.c
@@ -228,11 +228,19 @@ static void isochron_tlv_next(struct isochron_tlv **tlv, size_t *len)
 static int isochron_parse_one_tlv(struct prog_data *prog,
 				  struct isochron_tlv *tlv)
 {
+	int rc;
+
 	if (ntohs(tlv->tlv_type) != ISOCHRON_TLV_MANAGEMENT)
 		return 0;
 
 	switch (ntohs(tlv->management_id)) {
 	case ISOCHRON_MID_LOG:
+		rc = isochron_send_tlv(prog->stats_fd, ISOCHRON_RESPONSE,
+				       ISOCHRON_MID_LOG,
+				       isochron_log_buf_tlv_size(&prog->log));
+		if (rc)
+			return 0;
+
 		isochron_log_xmit(&prog->log, prog->stats_fd);
 		isochron_log_teardown(&prog->log);
 		return isochron_log_init(&prog->log, prog->iterations *

isochron/send.c
@@ -181,9 +181,8 @@ static int wait_for_rcv_last_pkt(struct prog_data *prog)
 static int prog_collect_rcv_stats(struct prog_data *prog,
 				  struct isochron_log *rcv_log)
 {
-	struct isochron_management_message *msg;
-	unsigned char buf[BUFSIZ];
-	struct isochron_tlv *tlv;
+	struct isochron_management_message msg;
+	struct isochron_tlv tlv;
 	ssize_t len;
 	int rc;
 
@@ -191,19 +190,25 @@ static int prog_collect_rcv_stats(struct prog_data *prog,
 	if (rc)
 		return rc;
 
-	msg = (struct isochron_management_message *)buf;
-	msg->version = ISOCHRON_MANAGEMENT_VERSION;
-	msg->action = ISOCHRON_GET;
-	msg->payload_length = __cpu_to_be32(sizeof(*tlv));
+	rc = isochron_send_tlv(prog->stats_fd, ISOCHRON_GET, ISOCHRON_MID_LOG, 0);
+	if (rc)
+		return rc;
 
-	tlv = (struct isochron_tlv *)(msg + 1);
-	tlv->tlv_type = ISOCHRON_TLV_MANAGEMENT;
-	tlv->management_id = ISOCHRON_MID_LOG;
-	tlv->length_field = 0;
-
-	len = write_exact(prog->stats_fd, buf, sizeof(*msg) + sizeof(*tlv));
+	len = recv_exact(prog->stats_fd, &msg, sizeof(msg), 0);
 	if (len <= 0)
-		return len;
+		return len ? len : -ECONNRESET;
+
+	len = recv_exact(prog->stats_fd, &tlv, sizeof(tlv), 0);
+	if (len <= 0)
+		return len ? len : -ECONNRESET;
+
+	if (msg.version != ISOCHRON_MANAGEMENT_VERSION ||
+	    msg.action != ISOCHRON_RESPONSE ||
+	    ntohs(tlv.tlv_type) != ISOCHRON_TLV_MANAGEMENT ||
+	    ntohs(tlv.management_id) != ISOCHRON_MID_LOG) {
+		fprintf(stderr, "Unexpected reply from isochron receiver\n");
+		return -EBADMSG;
+	}
 
 	return isochron_log_recv(rcv_log, prog->stats_fd);
 }

commit 2264af4012e2208f05c36209fa3e856ef768d2db

isochron: don't send uninitialized mem in isochron_send_tlv()
We are not populating all fields of struct isochron_management_message,
for example we are not populating the reserved field.

So memset the buffer to zero before populating the fields we care about.

Fixes: 505df8ed8c33 ("isochron: transmit the log buffer using a reply TLV as well")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -779,6 +779,8 @@ int isochron_send_tlv(int fd, enum isochron_management_action action,
 	struct isochron_tlv *tlv;
 	ssize_t len;
 
+	memset(buf, 0, sizeof(*msg) + sizeof(*tlv));
+
 	msg = (struct isochron_management_message *)buf;
 	msg->version = ISOCHRON_MANAGEMENT_VERSION;
 	msg->action = action;