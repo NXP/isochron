NXP/isochron

commit 51fbbe4885102dd59b005dd45158071fd540a8f2

isochron: send: add initial sysmon support
sysmon is a more-or-less standalone translation module which deals with
supervising phc2sys. It does not communicate with phc2sys, instead it
receives the name of an Ethernet interface as argument, finds that
interface's PTP clock, and monitors that PTP clock's offset to
CLOCK_REALTIME (yes, we compare a UTC time base with a TAI time base,
and it is up to the caller of translating UTC to TAI).

Naturally, most of sysmon's code is shamelessly stolen from phc2sys.

Sadly, this new code is not free of warnings, and this is due to the
ethtool ioctl kernel headers:

/opt/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/sysroot/usr/include/linux/ethtool.h:1575:35:
warning: comparison of integer expressions of different signedness: ‘__u32’ {aka ‘unsigned int’} and ‘int’ [-Wsign-compare]
 1575 |  return speed <= INT_MAX || speed == SPEED_UNKNOWN;
      |                                   ^~

So we'll have to live with it, it seems, just make Wsign-compare a
non-fatal warning.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/send.c
@@ -31,6 +31,7 @@
 #include <math.h>
 #include "common.h"
 #include "ptpmon.h"
+#include "sysmon.h"
 #include <linux/net_tstamp.h>
 
 #define BUF_SIZ		10000
@@ -50,6 +51,7 @@ struct prog_data {
 	char uds_remote[UNIX_PATH_MAX];
 	char sendbuf[BUF_SIZ];
 	struct ptpmon *ptpmon;
+	struct sysmon *sysmon;
 	LIST_HEAD(port_head, port) ports;
 	struct cmsghdr *cmsg;
 	struct iovec iov;
@@ -101,6 +103,7 @@ struct prog_data {
 	long domain_number;
 	long transport_specific;
 	long sync_threshold;
+	long num_readings;
 };
 
 static int signal_received;
@@ -577,10 +580,14 @@ static int prog_update_port_list(struct prog_data *prog)
 
 static int prog_sync_done(struct prog_data *prog)
 {
+	bool local_ptpmon_sync_done, local_sysmon_sync_done;
 	bool all_masters = true, have_slaves = false;
+	__s64 sysmon_offset, sysmon_delay;
+	char now_buf[TIMESPEC_BUFSIZ];
 	struct current_ds current_ds;
-	__s64 master_offset;
+	__s64 ptpmon_offset;
 	struct port *port;
+	__u64 sysmon_ts;
 	int rc;
 
 	LIST_FOREACH(port, &prog->ports, list) {
@@ -604,10 +611,23 @@ static int prog_sync_done(struct prog_data *prog)
 		return rc;
 	}
 
-	master_offset = master_offset_from_current_ds(&current_ds);
-	printf("offset %10lld to master\n", master_offset);
+	ptpmon_offset = master_offset_from_current_ds(&current_ds);
 
-	return llabs(master_offset) <= prog->sync_threshold ? 1 : 0;
+	rc = sysmon_get_offset(prog->sysmon, &sysmon_offset, &sysmon_ts,
+			       &sysmon_delay);
+	if (rc)
+		return rc;
+
+	sysmon_offset += NSEC_PER_SEC * prog->utc_tai_offset;
+
+	ns_sprintf(now_buf, sysmon_ts);
+	printf("isochron[%s]: ptpmon offset %10lld sysmon offset %10lld delay %10lld\n",
+	       now_buf, ptpmon_offset, sysmon_offset, sysmon_delay);
+
+	local_ptpmon_sync_done = !!(llabs(ptpmon_offset) <= prog->sync_threshold);
+	local_sysmon_sync_done = !!(llabs(sysmon_offset) <= prog->sync_threshold);
+
+	return local_ptpmon_sync_done && local_sysmon_sync_done;
 }
 
 static int prog_check_sync(struct prog_data *prog)
@@ -701,6 +721,26 @@ static void prog_teardown_ptpmon(struct prog_data *prog)
 	ptpmon_close(prog->ptpmon);
 	ptpmon_destroy(prog->ptpmon);
 	prog->ptpmon = NULL;
+}
+
+static int prog_init_sysmon(struct prog_data *prog)
+{
+	if (prog->omit_sync)
+		return 0;
+
+	prog->sysmon = sysmon_create(prog->if_name, prog->num_readings);
+	if (!prog->sysmon)
+		return -ENOMEM;
+
+	return 0;
+}
+
+static void prog_teardown_sysmon(struct prog_data *prog)
+{
+	if (!prog->sysmon)
+		return;
+
+	sysmon_destroy(prog->sysmon);
 }
 
 static int prog_init(struct prog_data *prog)
@@ -904,6 +944,10 @@ static int prog_init(struct prog_data *prog)
 	if (rc)
 		goto out_munlock;
 
+	rc = prog_init_sysmon(prog);
+	if (rc)
+		goto out_teardown_ptpmon;
+
 	/* Drain potentially old packets from the isochron receiver */
 	if (prog->stats_srv.family) {
 		struct isochron_log rcv_log;
@@ -914,6 +958,9 @@ static int prog_init(struct prog_data *prog)
 	}
 
 	return rc;
+
+out_teardown_ptpmon:
+	prog_teardown_ptpmon(prog);
 out_munlock:
 	munlockall();
 out_log_teardown:
@@ -1157,6 +1204,7 @@ static int prog_teardown(struct prog_data *prog)
 			isochron_send_log_print(&prog->log);
 	}
 
+	prog_teardown_sysmon(prog);
 	prog_teardown_ptpmon(prog);
 
 	munlockall();
@@ -1459,6 +1507,14 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 				.ptr = &prog->sync_threshold,
 			},
 			.optional = true,
+		}, {
+			.short_opt = "-R",
+			.long_opt = "--num-readings",
+			.type = PROG_ARG_LONG,
+			.long_ptr = {
+				.ptr = &prog->num_readings,
+			},
+			.optional = true,
 		},
 	};
 	int rc;
@@ -1589,6 +1645,9 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 			"cannot take timestamps if running indefinitely\n");
 		return -EINVAL;
 	}
+
+	if (!prog->num_readings)
+		prog->num_readings = 5;
 
 	if (prog->utc_tai_offset == -1) {
 		/* If we're using the ptpmon, we'll get the UTC offset

Makefile
@@ -1,6 +1,6 @@
 VERSION := $(shell ./setlocalversion)
 EXTRA_CFLAGS := $(shell ./toolchain_deps.sh "$(CC)" "$(CFLAGS)")
 CFLAGS := $(CFLAGS) -DVERSION=\"${VERSION}\" $(EXTRA_CFLAGS)
-CFLAGS += -Wall -Wextra -Werror
+CFLAGS += -Wall -Wextra -Werror -Wno-error=sign-compare
 
 include isochron/Makefile

isochron/Makefile
@@ -8,7 +8,8 @@ src := \
 	rcv.o \
 	ptpmon.o \
 	send.o \
-	stats.o
+	stats.o \
+	sysmon.o
 
 objs := $(addprefix isochron/, $(src))
 

isochron/missing.h
@@ -1,0 +1,72 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (C) 2011 Richard Cochran <richardcochran@gmail.com>
+ * Copyright 2021 NXP
+ */
+/* This file contains code snippets from:
+ * - The Linux kernel
+ * - The linuxptp project
+ */
+#ifndef _MISSING_H
+#define _MISSING_H
+
+#ifndef PTP_MAX_SAMPLES
+#define PTP_MAX_SAMPLES 25 /* Maximum allowed offset measurement samples. */
+#endif /* PTP_MAX_SAMPLES */
+
+#ifndef PTP_SYS_OFFSET
+
+#define PTP_SYS_OFFSET     _IOW(PTP_CLK_MAGIC, 5, struct ptp_sys_offset)
+
+struct ptp_sys_offset {
+	unsigned int n_samples; /* Desired number of measurements. */
+	unsigned int rsv[3];    /* Reserved for future use. */
+	/*
+	 * Array of interleaved system/phc time stamps. The kernel
+	 * will provide 2*n_samples + 1 time stamps, with the last
+	 * one as a system time stamp.
+	 */
+	struct ptp_clock_time ts[2 * PTP_MAX_SAMPLES + 1];
+};
+
+#endif /* PTP_SYS_OFFSET */
+
+#ifndef PTP_SYS_OFFSET_PRECISE
+
+#define PTP_SYS_OFFSET_PRECISE \
+	_IOWR(PTP_CLK_MAGIC, 8, struct ptp_sys_offset_precise)
+
+struct ptp_sys_offset_precise {
+	struct ptp_clock_time device;
+	struct ptp_clock_time sys_realtime;
+	struct ptp_clock_time sys_monoraw;
+	unsigned int rsv[4];    /* Reserved for future use. */
+};
+
+#endif /* PTP_SYS_OFFSET_PRECISE */
+
+#ifndef PTP_SYS_OFFSET_EXTENDED
+
+#define PTP_SYS_OFFSET_EXTENDED \
+	_IOWR(PTP_CLK_MAGIC, 9, struct ptp_sys_offset_extended)
+
+struct ptp_sys_offset_extended {
+	unsigned int n_samples; /* Desired number of measurements. */
+	unsigned int rsv[3];    /* Reserved for future use. */
+	/*
+	 * Array of [system, phc, system] time stamps. The kernel will provide
+	 * 3*n_samples time stamps.
+	 */
+	struct ptp_clock_time ts[PTP_MAX_SAMPLES][3];
+};
+
+#endif /* PTP_SYS_OFFSET_EXTENDED */
+
+#define CLOCKFD 3
+#define FD_TO_CLOCKID(fd)	((clockid_t) ((((unsigned int) ~fd) << 3) | CLOCKFD))
+#define CLOCKID_TO_FD(clk)	((unsigned int) ~((clk) >> 3))
+
+#ifndef CLOCK_INVALID
+#define CLOCK_INVALID -1
+#endif
+
+#endif

isochron/sysmon.c
@@ -1,0 +1,373 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (C) 2012 Richard Cochran <richardcochran@gmail.com>
+ * Copyright 2021 NXP
+ */
+/* This file contains code snippets from the linuxptp project
+ */
+#define _GNU_SOURCE	/* for asprintf() */
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <linux/ethtool.h>
+#include <linux/if.h>
+#include <linux/ptp_clock.h>
+#include <linux/sockios.h>
+#include <linux/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "missing.h"
+
+#define NSEC_PER_SEC 1000000000LL
+
+enum sysoff_method {
+	SYSOFF_RUN_TIME_MISSING = -1,
+	SYSOFF_PRECISE,
+	SYSOFF_EXTENDED,
+	SYSOFF_BASIC,
+	SYSOFF_LAST,
+};
+
+/**
+ * Contains timestamping information returned by the GET_TS_INFO ioctl.
+ * @valid:            set to non-zero when the info struct contains valid data.
+ * @phc_index:        index of the PHC device.
+ * @so_timestamping:  supported time stamping modes.
+ * @tx_types:         driver level transmit options for the HWTSTAMP ioctl.
+ * @rx_filters:       driver level receive options for the HWTSTAMP ioctl.
+ */
+struct sk_ts_info {
+	int valid;
+	int phc_index;
+	unsigned int so_timestamping;
+	unsigned int tx_types;
+	unsigned int rx_filters;
+};
+
+struct sysmon {
+	clockid_t clkid;
+	char *name;
+	int phc_index;
+	int num_readings;
+	enum sysoff_method sysoff_method;
+};
+
+static __s64 pct_to_ns(struct ptp_clock_time *t)
+{
+	return t->sec * NSEC_PER_SEC + t->nsec;
+}
+
+static int sk_get_ts_info(const char *name, struct sk_ts_info *sk_info)
+{
+	struct ethtool_ts_info info;
+	struct ifreq ifr;
+	int fd, err;
+
+	memset(sk_info, 0, sizeof(struct sk_ts_info));
+
+	memset(&ifr, 0, sizeof(ifr));
+	memset(&info, 0, sizeof(info));
+	info.cmd = ETHTOOL_GET_TS_INFO;
+	strncpy(ifr.ifr_name, name, IFNAMSIZ - 1);
+	ifr.ifr_data = (char *) &info;
+
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd < 0) {
+		fprintf(stderr, "socket failed: %m\n");
+		return -errno;
+	}
+
+	err = ioctl(fd, SIOCETHTOOL, &ifr);
+	if (err < 0) {
+		fprintf(stderr, "ioctl SIOCETHTOOL failed: %m\n");
+		close(fd);
+		return -errno;
+	}
+
+	close(fd);
+
+	/* copy the necessary data to sk_info */
+	sk_info->valid = 1;
+	sk_info->phc_index = info.phc_index;
+	sk_info->so_timestamping = info.so_timestamping;
+	sk_info->tx_types = info.tx_types;
+	sk_info->rx_filters = info.rx_filters;
+
+	return 0;
+}
+
+static clockid_t phc_open(const char *iface, int *phc_index)
+{
+	struct sk_ts_info ts_info;
+	char phc_device[19];
+	struct timespec ts;
+	clockid_t clkid;
+	int fd;
+
+	/* check if device is a valid ethernet device */
+	if (sk_get_ts_info(iface, &ts_info) || !ts_info.valid) {
+		fprintf(stderr, "unknown clock %s: %m\n", iface);
+		return CLOCK_INVALID;
+	}
+
+	if (ts_info.phc_index < 0) {
+		fprintf(stderr, "interface %s does not have a PHC\n", iface);
+		return CLOCK_INVALID;
+	}
+
+	snprintf(phc_device, sizeof(phc_device), "/dev/ptp%d", ts_info.phc_index);
+
+	fd = open(phc_device, O_RDWR);
+	if (fd < 0) {
+		fprintf(stderr, "cannot open %s: %m\n", phc_device);
+		return CLOCK_INVALID;
+	}
+
+	clkid = FD_TO_CLOCKID(fd);
+	/* check if clkid is valid */
+	if (clock_gettime(clkid, &ts)) {
+		close(fd);
+		return CLOCK_INVALID;
+	}
+
+	*phc_index = ts_info.phc_index;
+	return clkid;
+}
+
+static void phc_close(clockid_t clkid)
+{
+	close(CLOCKID_TO_FD(clkid));
+}
+
+static int sysoff_precise(int fd, __s64 *result, __u64 *ts)
+{
+	struct ptp_sys_offset_precise pso;
+	memset(&pso, 0, sizeof(pso));
+	if (ioctl(fd, PTP_SYS_OFFSET_PRECISE, &pso)) {
+		printf("ioctl PTP_SYS_OFFSET_PRECISE: %m\n");
+		return SYSOFF_RUN_TIME_MISSING;
+	}
+	*result = pct_to_ns(&pso.sys_realtime) - pct_to_ns(&pso.device);
+	*ts = pct_to_ns(&pso.sys_realtime);
+	return SYSOFF_PRECISE;
+}
+
+static __s64 sysoff_estimate(struct ptp_clock_time *pct, int extended,
+			     int n_samples, __u64 *ts, __s64 *delay)
+{
+	__s64 shortest_interval, best_timestamp, best_offset;
+	__s64 interval, timestamp, offset;
+	__s64 t1, t2, tp;
+	int i = 0;
+
+	if (extended) {
+		t1 = pct_to_ns(&pct[3*i]);
+		tp = pct_to_ns(&pct[3*i+1]);
+		t2 = pct_to_ns(&pct[3*i+2]);
+	} else {
+		t1 = pct_to_ns(&pct[2*i]);
+		tp = pct_to_ns(&pct[2*i+1]);
+		t2 = pct_to_ns(&pct[2*i+2]);
+	}
+	shortest_interval = t2 - t1;
+	best_timestamp = (t2 + t1) / 2;
+	best_offset = best_timestamp - tp;
+
+	for (i = 1; i < n_samples; i++) {
+		if (extended) {
+			t1 = pct_to_ns(&pct[3*i]);
+			tp = pct_to_ns(&pct[3*i+1]);
+			t2 = pct_to_ns(&pct[3*i+2]);
+		} else {
+			t1 = pct_to_ns(&pct[2*i]);
+			tp = pct_to_ns(&pct[2*i+1]);
+			t2 = pct_to_ns(&pct[2*i+2]);
+		}
+		interval = t2 - t1;
+		timestamp = (t2 + t1) / 2;
+		offset = timestamp - tp;
+		if (interval < shortest_interval) {
+			shortest_interval = interval;
+			best_timestamp = timestamp;
+			best_offset = offset;
+		}
+	}
+	*ts = best_timestamp;
+	*delay = shortest_interval;
+	return best_offset;
+}
+
+static int sysoff_extended(int fd, int n_samples, __s64 *result, __u64 *ts,
+			   __s64 *delay)
+{
+	struct ptp_sys_offset_extended pso;
+	memset(&pso, 0, sizeof(pso));
+	pso.n_samples = n_samples;
+	if (ioctl(fd, PTP_SYS_OFFSET_EXTENDED, &pso)) {
+		printf("ioctl PTP_SYS_OFFSET_EXTENDED: %m\n");
+		return SYSOFF_RUN_TIME_MISSING;
+	}
+	*result = sysoff_estimate(&pso.ts[0][0], 1, n_samples, ts, delay);
+	return SYSOFF_EXTENDED;
+}
+
+static int sysoff_basic(int fd, int n_samples, __s64 *result, __u64 *ts,
+			__s64 *delay)
+{
+	struct ptp_sys_offset pso;
+	memset(&pso, 0, sizeof(pso));
+	pso.n_samples = n_samples;
+	if (ioctl(fd, PTP_SYS_OFFSET, &pso)) {
+		perror("ioctl PTP_SYS_OFFSET");
+		return SYSOFF_RUN_TIME_MISSING;
+	}
+	*result = sysoff_estimate(pso.ts, 0, n_samples, ts, delay);
+	return SYSOFF_BASIC;
+}
+
+/**
+ * Measure the offset between a PHC and the system time.
+ * @param fd         An open file descriptor to a PHC device.
+ * @param method     A non-negative SYSOFF_ value returned by sysoff_probe().
+ * @param n_samples  The number of consecutive readings to make.
+ * @param result     The estimated offset in nanoseconds.
+ * @param ts         The system time corresponding to the 'result'.
+ * @param delay      The delay in reading of the clock in nanoseconds.
+ * @return  One of the SYSOFF_ enumeration values.
+ */
+static int sysoff_measure(int fd, enum sysoff_method method, int n_samples,
+			  __s64 *result, __u64 *ts, __s64 *delay)
+{
+	switch (method) {
+	case SYSOFF_PRECISE:
+		*delay = 0;
+		return sysoff_precise(fd, result, ts);
+	case SYSOFF_EXTENDED:
+		return sysoff_extended(fd, n_samples, result, ts, delay);
+	case SYSOFF_BASIC:
+		return sysoff_basic(fd, n_samples, result, ts, delay);
+	default:
+		return SYSOFF_RUN_TIME_MISSING;
+	}
+}
+
+/**
+ * Check to see if a PTP_SYS_OFFSET ioctl is supported.
+ * @param fd  An open file descriptor to a PHC device.
+ * @return  One of the SYSOFF_ enumeration values.
+ */
+static enum sysoff_method sysoff_probe(int fd, int n_samples)
+{
+	__s64 junk, delay;
+	__u64 ts;
+	int i;
+
+	if (n_samples > PTP_MAX_SAMPLES) {
+		fprintf(stderr, "warning: %d exceeds kernel max readings %d\n",
+			n_samples, PTP_MAX_SAMPLES);
+		fprintf(stderr, "falling back to clock_gettime method\n");
+		return SYSOFF_RUN_TIME_MISSING;
+	}
+
+	for (i = 0; i < SYSOFF_LAST; i++) {
+		if (sysoff_measure(fd, i, n_samples, &junk, &ts, &delay) < 0)
+			continue;
+		return i;
+	}
+
+	return SYSOFF_RUN_TIME_MISSING;
+}
+
+static int read_phc(clockid_t clkid, clockid_t sysclk, int readings,
+		    __s64 *offset, __u64 *ts, __s64 *delay)
+{
+	__s64 interval, best_interval = INT64_MAX;
+	struct timespec t_dst1, t_dst2, t_src;
+	int i;
+
+	/* Pick the quickest clkid reading. */
+	for (i = 0; i < readings; i++) {
+		if (clock_gettime(sysclk, &t_dst1) ||
+		    clock_gettime(clkid, &t_src) ||
+		    clock_gettime(sysclk, &t_dst2)) {
+			fprintf(stderr, "failed to read clock: %m\n");
+			return 0;
+		}
+
+		interval = (t_dst2.tv_sec - t_dst1.tv_sec) * NSEC_PER_SEC +
+			    t_dst2.tv_nsec - t_dst1.tv_nsec;
+
+		if (best_interval > interval) {
+			best_interval = interval;
+			*offset = (t_dst1.tv_sec - t_src.tv_sec) * NSEC_PER_SEC +
+				   t_dst1.tv_nsec - t_src.tv_nsec + interval / 2;
+			*ts = t_dst2.tv_sec * NSEC_PER_SEC + t_dst2.tv_nsec;
+		}
+	}
+	*delay = best_interval;
+
+	return 1;
+}
+
+int sysmon_get_offset(struct sysmon *sysmon, __s64 *offset, __u64 *ts,
+		      __s64 *delay)
+{
+	if (sysmon->sysoff_method != SYSOFF_RUN_TIME_MISSING) {
+		if (sysoff_measure(CLOCKID_TO_FD(sysmon->clkid),
+				   sysmon->sysoff_method, sysmon->num_readings,
+				   offset, ts, delay) < 0)
+			return -1;
+	} else {
+		if (!read_phc(sysmon->clkid, CLOCK_REALTIME,
+			      sysmon->num_readings, offset, ts, delay))
+			return -1;
+	}
+
+	return 0;
+}
+
+struct sysmon *sysmon_create(const char *iface, int num_readings)
+{
+	struct sysmon *sysmon;
+	int phc_index = -1;
+	clockid_t clkid;
+	int err;
+
+	sysmon = calloc(1, sizeof(*sysmon));
+	if (!sysmon)
+		return NULL;
+
+	clkid = phc_open(iface, &phc_index);
+	if (clkid == CLOCK_INVALID) {
+		free(sysmon);
+		return NULL;
+	}
+
+	sysmon->clkid = clkid;
+	sysmon->phc_index = phc_index;
+	sysmon->num_readings = num_readings;
+	sysmon->sysoff_method = sysoff_probe(CLOCKID_TO_FD(clkid),
+					     num_readings);
+
+	err = asprintf(&sysmon->name, "/dev/ptp%d", phc_index);
+	if (err < 0) {
+		phc_close(clkid);
+		free(sysmon);
+		return NULL;
+	}
+
+	return sysmon;
+}
+
+void sysmon_destroy(struct sysmon *sysmon)
+{
+	phc_close(sysmon->clkid);
+	free(sysmon->name);
+	free(sysmon);
+}

isochron/sysmon.h
@@ -1,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright 2021 NXP */
+#ifndef _SYSMON_H
+#define _SYSMON_H
+
+#include <linux/types.h>
+
+struct sysmon;
+
+struct sysmon *sysmon_create(const char *iface, int num_readings);
+void sysmon_destroy(struct sysmon *sysmon);
+int sysmon_get_offset(struct sysmon *sysmon, __s64 *offset, __u64 *ts,
+		      __s64 *delay);
+
+#endif

commit 7e4ff31cf0185084ee726eb7582ea3ddb5ea9984

isochron: send: translate sysmon offset to TAI
We are printing the current system time while monitoring the sync
status, but that is in UTC, whereas every other isochron timestamp is in
TAI. Translate that to TAI as well.

Fixes: 51fbbe488510 ("isochron: send: add initial sysmon support")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/send.c
@@ -652,6 +652,7 @@ static bool prog_sync_done(struct prog_data *prog)
 		return false;
 
 	sysmon_offset += NSEC_PER_SEC * prog->utc_tai_offset;
+	sysmon_ts += NSEC_PER_SEC * prog->utc_tai_offset;
 
 	local_ptpmon_sync_done = !!(llabs(ptpmon_offset) <= prog->sync_threshold);
 	local_sysmon_sync_done = !!(llabs(sysmon_offset) <= prog->sync_threshold);