NXP/isochron

commit 42952229c6031ee6edd414041c0bcd2a342e871e

isochron: look up the port state by interface name in ptpmon
Currently we use the MID_PORT_DATA_SET to query ptp4l port states, but
this is sub-optimal because we don't really know which port that is.

One case where that might not be ok is when ptp4l runs on an entirely
different port than the one on which we run isochron, and that port even
has a different PHC.

Unfortunately the standard PORT_DATA_SET doesn't give us something based
on which we can figure out whether this port is that port. IEEE 1588-2008
clause "7.5.2.2.2 IEEE EUI-64 clockIdentity values" doesn't say that the
clockIdentity should be derived in one way or another (ptp4l does seem
to derive it from the MAC address of the first port in the BC).

Enter the non-portable managementID PORT_PROPERTIES_NP which is defined
and implemented by ptp4l. This does provide the string corresponding to
the interface name, but is a bit hard to get. Its data type is a PTPText,
which is formatted Pascal-style, with the first octet denoting the
number of bytes in the string to follow.

I had expected that such a variable-length data type to only be present
at the end of a data set structure, on the premise that it would shift
all other following structure fields to unpredictable offsets, making it
impossible to map a variable-length C structure over the data set.

After opening the standard just now, I can see that the premise is
correct, but still that didn't stop the gods from putting PTPText fields
in the middle of data sets and structures, like:

| 5.3.10 FaultRecord
| The FaultRecord type is used to construct fault logs.
|
| struct FaultRecord
| {
| 	UInteger16 faultRecordLength;
| 	Timestamp faultTime;
| 	Enumeration8 severityCode;
| 	PTPText faultName;
| 	PTPText faultValue;
| 	PTPText faultDescription;
| };

Seriously?! Why not just make them fixed size if you're goint to do that?

Anyway, thanks to Jiri Benc for not doing that for PORT_PROPERTIES_NP in

isochron/common.c
@@ -847,3 +847,47 @@ int get_utc_tai_offset()
 	adjtimex(&tx);
 	return tx.tai;
 }
+
+int ptpmon_query_port_state_by_name(struct ptpmon *ptpmon, const char *iface,
+				    enum port_state *port_state)
+{
+	struct default_ds default_ds;
+	int portnum;
+	int rc;
+
+	rc = ptpmon_query_clock_mid(ptpmon, MID_DEFAULT_DATA_SET,
+				    &default_ds, sizeof(default_ds));
+	if (rc) {
+		fprintf(stderr, "Failed to query DEFAULT_DATA_SET: %d (%s)\n",
+			rc, strerror(-rc));
+		return rc;
+	}
+
+	for (portnum = 1; portnum <= ntohs(default_ds.number_ports); portnum++) {
+		struct port_properties_np port_properties_np;
+		struct port_identity portid;
+
+		portid_set(&portid, &default_ds.clock_identity, portnum);
+
+		rc = ptpmon_query_port_mid_extra(ptpmon, &portid,
+						 MID_PORT_PROPERTIES_NP,
+						 &port_properties_np,
+						 sizeof(port_properties_np),
+						 MAX_IFACE_LEN);
+		if (rc) {
+			fprintf(stderr,
+				"Failed to query PORT_PROPERTIES_NP: %d (%s)\n",
+				rc, strerror(-rc));
+			return rc;
+		}
+
+		if (strcmp(port_properties_np.iface, iface))
+			continue;
+
+		*port_state = port_properties_np.port_state;
+
+		return 0;
+	}
+
+	return -ENODEV;
+}

isochron/common.h
@@ -443,4 +443,7 @@ master_offset_from_current_ds(const struct current_ds *current_ds)
 	return (__s64 )(__be64_to_cpu(current_ds->offset_from_master)) >> 16;
 }
 
+int ptpmon_query_port_state_by_name(struct ptpmon *ptpmon, const char *iface,
+				    enum port_state *port_state);
+
 #endif

isochron/ptpmon.c
@@ -429,6 +429,42 @@ static int ptpmon_copy_data_set(void *priv, struct ptp_tlv *tlv,
 	return 0;
 }
 
+static __u8 *ptp_management_tlv_extra_len_field(__u8 *tlv_data, size_t dest_len)
+{
+	return tlv_data + dest_len - 1;
+}
+
+static int ptpmon_copy_variable_len_data_set(void *priv, struct ptp_tlv *tlv,
+					     const void *tlv_data)
+{
+	struct ptpmon_tlv_parse_priv *parse = priv;
+	size_t extra_len, expected_len;
+
+	if (ntohs(tlv->management_id) != parse->mid) {
+		fprintf(stderr, "unknown management id 0x%x, expected 0x%x\n",
+			ntohs(tlv->management_id), parse->mid);
+		return -EINVAL;
+	}
+
+	extra_len = *ptp_management_tlv_extra_len_field((__u8 *)tlv_data,
+							parse->dest_len);
+	expected_len = parse->dest_len + 2 + extra_len;
+	/* PTP messages are padded to even lengths */
+	if (expected_len & 1)
+		expected_len++;
+
+	if (ntohs(tlv->length_field) != expected_len) {
+		fprintf(stderr,
+			"unexpected TLV length %d for management id %d, expected %zu\n",
+			ntohs(tlv->length_field), parse->mid, expected_len);
+		return -EINVAL;
+	}
+
+	memcpy(parse->dest, tlv_data, parse->dest_len + extra_len);
+
+	return 0;
+}
+
 static int ptp_management_message_for_each_tlv(struct ptp_message *msg,
 					       ptpmon_tlv_cb_t cb, void *priv)
 {
@@ -479,6 +515,59 @@ static int ptp_message_parse_reply(struct ptp_message *msg, ptpmon_tlv_cb_t cb,
 	}
 
 	return ptp_management_message_for_each_tlv(msg, cb, priv);
+}
+
+static void ptp_management_message_update_extra_len(struct ptp_message *msg,
+						    size_t dest_len,
+						    size_t extra_len)
+{
+	struct ptp_tlv *tlv = ptp_management_suffix(msg);
+	void *tlv_data = ptp_management_tlv_data(tlv);
+
+	*ptp_management_tlv_extra_len_field(tlv_data, dest_len) = extra_len;
+}
+
+int ptpmon_query_port_mid_extra(struct ptpmon *ptpmon,
+				const struct port_identity *target_port_identity,
+				enum ptp_management_id mid,
+				void *dest, size_t dest_len, size_t extra_len)
+{
+	struct ptpmon_tlv_parse_priv parse = {
+		.mid = mid,
+		.dest = dest,
+		.dest_len = dest_len,
+	};
+	struct ptp_message msg;
+	int err;
+
+	ptp_message_clear(&msg);
+
+	ptpmon_message_init(ptpmon, &msg, GET, target_port_identity);
+
+	if (!ptp_message_add_management_tlv(&msg, mid, dest_len + extra_len))
+		return -ERANGE;
+
+	ptp_management_message_update_extra_len(&msg, dest_len, extra_len);
+
+	err = ptpmon_send(ptpmon, &msg);
+	if (err)
+		return err;
+
+	err = ptpmon_recv(ptpmon, &msg);
+	if (err)
+		return err;
+
+	return ptp_message_parse_reply(&msg, ptpmon_copy_variable_len_data_set,
+				       &parse);
+}
+
+int ptpmon_query_clock_mid_extra(struct ptpmon *ptpmon,
+				 enum ptp_management_id mid,
+				 void *dest, size_t dest_len,
+				 size_t extra_len)
+{
+	return ptpmon_query_port_mid_extra(ptpmon, &target_all_ports, mid,
+					   dest, dest_len, extra_len);
 }
 
 int ptpmon_query_port_mid(struct ptpmon *ptpmon,

isochron/ptpmon.h
@@ -9,6 +9,8 @@
 
 #include <stdbool.h>
 
+#define MAX_IFACE_LEN					255
+
 enum ptp_management_id {
 	/* Clock management ID values */
 	MID_USER_DESCRIPTION				= 0x0002,
@@ -142,6 +144,15 @@ struct parent_data_set {
 	struct clock_identity	grandmaster_identity;
 } __attribute((packed));
 
+/* MID_PORT_PROPERTIES_NP */
+struct port_properties_np {
+	struct port_identity	port_identity;
+	__u8			port_state;
+	__u8			timestamping;
+	__u8			iface_len;
+	char			iface[0]; /* up to MAX_IFACE_LEN */
+} __attribute((packed));
+
 /** Defines the state of a port. */
 enum port_state {
 	PS_INITIALIZING = 1,
@@ -210,5 +221,13 @@ int ptpmon_query_port_mid(struct ptpmon *ptpmon,
 			  void *dest, size_t dest_len);
 int ptpmon_query_clock_mid(struct ptpmon *ptpmon, enum ptp_management_id mid,
 			   void *dest, size_t dest_len);
+int ptpmon_query_port_mid_extra(struct ptpmon *ptpmon,
+				const struct port_identity *target_port_identity,
+				enum ptp_management_id mid,
+				void *dest, size_t dest_len, size_t extra_len);
+int ptpmon_query_clock_mid_extra(struct ptpmon *ptpmon,
+				 enum ptp_management_id mid,
+				 void *dest, size_t dest_len,
+				 size_t extra_len);
 
 #endif

isochron/rcv.c
@@ -295,50 +295,22 @@ static int prog_forward_utc_offset(struct prog_data *prog)
 static int prog_forward_port_state(struct prog_data *prog)
 {
 	struct isochron_port_state state;
-	struct default_ds default_ds;
-	int portnum;
+	enum port_state port_state;
 	int rc;
 
-	rc = ptpmon_query_clock_mid(prog->ptpmon, MID_DEFAULT_DATA_SET,
-				    &default_ds, sizeof(default_ds));
-	if (rc) {
-		fprintf(stderr, "Failed to query DEFAULT_DATA_SET: %d (%s)\n",
-			rc, strerror(-rc));
-		fprintf(stderr,
-			"Please make sure PTP daemon is running, or re-run using --omit-sync\n");
-		return rc;
-	}
+	rc = ptpmon_query_port_state_by_name(prog->ptpmon, prog->if_name,
+					     &port_state);
+	if (rc)
+		return 0;
 
-	/* FIXME: this always returns the port state of the first port,
-	 * not necessarily our port.
-	 */
-	for (portnum = 1; portnum <= ntohs(default_ds.number_ports); portnum++) {
-		struct port_identity portid;
-		struct port_ds port_ds;
+	state.state = port_state;
 
-		portid_set(&portid, &default_ds.clock_identity, portnum);
+	rc = isochron_send_tlv(prog->stats_fd, ISOCHRON_RESPONSE,
+			       ISOCHRON_MID_PORT_STATE, sizeof(state));
+	if (rc)
+		return 0;
 
-		rc = ptpmon_query_port_mid(prog->ptpmon, &portid,
-					   MID_PORT_DATA_SET,
-					   &port_ds, sizeof(port_ds));
-		if (rc) {
-			fprintf(stderr,
-				"Failed to query PORT_DATA_SET: %d (%s)\n",
-				rc, strerror(-rc));
-			return rc;
-		}
-
-		state.state = port_ds.port_state;
-
-		rc = isochron_send_tlv(prog->stats_fd, ISOCHRON_RESPONSE,
-				       ISOCHRON_MID_PORT_STATE,
-				       sizeof(state));
-		if (rc)
-			return 0;
-
-		write_exact(prog->stats_fd, &state, sizeof(state));
-		break;
-	}
+	write_exact(prog->stats_fd, &state, sizeof(state));
 
 	return 0;
 }

isochron/send.c
@@ -52,7 +52,8 @@ struct prog_data {
 	char sendbuf[BUF_SIZ];
 	struct ptpmon *ptpmon;
 	struct sysmon *sysmon;
-	LIST_HEAD(port_head, port) ports;
+	enum port_state last_local_port_state;
+	enum port_state last_remote_port_state;
 	struct cmsghdr *cmsg;
 	struct iovec iov;
 	struct msghdr msg;
@@ -637,152 +638,57 @@ static void sig_handler(int signo)
 	}
 }
 
-static struct port *port_get(struct prog_data *prog,
-			     const struct port_identity *portid)
-{
-	struct port *port;
-
-	LIST_FOREACH(port, &prog->ports, list)
-		if (portid_eq(&port->portid, portid))
-			return port;
-
-	return NULL;
-}
-
-static struct port *port_add(struct prog_data *prog,
-			     const struct port_identity *portid)
-{
-	char portid_buf[PORTID_BUFSIZE];
-	struct port *port;
-
-	port = port_get(prog, portid);
-	if (port)
-		return port;
-
-	port = calloc(1, sizeof(*port));
-	if (!port)
-		return NULL;
-
-	portid_to_string(portid, portid_buf);
-	printf("new port %s\n", portid_buf);
-	memcpy(&port->portid, portid, sizeof(*portid));
-	LIST_INSERT_HEAD(&prog->ports, port, list);
-	return port;
-}
-
-static void port_del(struct port *port)
-{
-	LIST_REMOVE(port, list);
-	free(port);
-}
-
-static void port_set_state(struct port *port, enum port_state state)
-{
-	char portid_buf[PORTID_BUFSIZE];
-
-	if (port->state == state)
-		return;
-
-	portid_to_string(&port->portid, portid_buf);
-	printf("port %s changed state to %s\n", portid_buf,
-		port_state_to_string(state));
-	port->state = state;
-}
-
-static int prog_update_port_list(struct prog_data *prog)
-{
-	char portid_buf[PORTID_BUFSIZE];
-	struct default_ds default_ds;
-	struct port *port, *tmp;
-	int portnum;
-	int rc;
-
-	rc = ptpmon_query_clock_mid(prog->ptpmon, MID_DEFAULT_DATA_SET,
-				    &default_ds, sizeof(default_ds));
-	if (rc) {
-		fprintf(stderr, "Failed to query DEFAULT_DATA_SET: %d (%s)\n",
-			rc, strerror(-rc));
-		fprintf(stderr,
-			"Please make sure PTP daemon is running, or re-run using --omit-sync\n");
-		return rc;
-	}
-
-	LIST_FOREACH(port, &prog->ports, list)
-		port->active = false;
-
-	for (portnum = 1; portnum <= ntohs(default_ds.number_ports); portnum++) {
-		struct port_identity portid;
-		struct port_ds port_ds;
-
-		portid_set(&portid, &default_ds.clock_identity, portnum);
-
-		rc = ptpmon_query_port_mid(prog->ptpmon, &portid,
-					   MID_PORT_DATA_SET,
-					   &port_ds, sizeof(port_ds));
-		if (rc) {
-			fprintf(stderr,
-				"Failed to query PORT_DATA_SET: %d (%s)\n",
-				rc, strerror(-rc));
-			return rc;
-		}
-
-		port = port_add(prog, &portid);
-		if (!port)
-			return -ENOMEM;
-
-		port->active = true;
-		port_set_state(port, port_ds.port_state);
-	}
-
-	LIST_FOREACH_SAFE(port, &prog->ports, list, tmp) {
-		if (!port->active) {
-			portid_to_string(&port->portid, portid_buf);
-			printf("Pruning inactive port %s\n", portid_buf);
-			port_del(port);
-		}
-	}
-
-	return 0;
-}
-
 static bool prog_sync_done(struct prog_data *prog)
 {
+	bool local_port_transient_state, remote_port_transient_state;
 	bool remote_ptpmon_sync_done, remote_sysmon_sync_done;
 	bool local_ptpmon_sync_done, local_sysmon_sync_done;
-	bool all_masters = true, have_slaves = false;
+	enum port_state local_port_state, remote_port_state;
 	__s64 rcv_sysmon_offset, rcv_ptpmon_offset;
 	struct clock_identity rcv_gm_clkid;
 	__s64 sysmon_offset, sysmon_delay;
-	enum port_state rcv_port_state;
 	char now_buf[TIMESPEC_BUFSIZ];
 	struct current_ds current_ds;
 	__s64 ptpmon_offset;
 	int rcv_utc_offset;
-	struct port *port;
 	__u64 sysmon_ts;
 	int rc;
 
 	rc = prog_collect_receiver_sync_stats(prog, &rcv_sysmon_offset,
 					      &rcv_ptpmon_offset,
-					      &rcv_utc_offset, &rcv_port_state,
+					      &rcv_utc_offset,
+					      &remote_port_state,
 					      &rcv_gm_clkid);
 	if (rc)
 		return false;
 
 	rcv_sysmon_offset += NSEC_PER_SEC * rcv_utc_offset;
 
-	LIST_FOREACH(port, &prog->ports, list) {
-		if (port->state != PS_MASTER)
-			all_masters = false;
-		if (port->state == PS_SLAVE)
-			have_slaves = true;
+	if (remote_port_state != prog->last_remote_port_state) {
+		printf("Remote port changed state to %s\n",
+		       port_state_to_string(remote_port_state));
+		prog->last_remote_port_state = remote_port_state;
 	}
 
-	if (all_masters)
-		return true;
-
-	if (!have_slaves)
+	rc = ptpmon_query_port_state_by_name(prog->ptpmon, prog->if_name,
+					     &local_port_state);
+	if (rc) {
+		fprintf(stderr,
+			"Failed to query port state for %s (%s), waiting for ptp4l\n",
+			prog->if_name, strerror(-rc));
 		return false;
+	}
+
+	if (local_port_state != prog->last_local_port_state) {
+		printf("Local port changed state to %s\n",
+		       port_state_to_string(local_port_state));
+		prog->last_local_port_state = local_port_state;
+	}
+
+	local_port_transient_state = local_port_state != PS_MASTER &&
+				     local_port_state != PS_SLAVE;
+	remote_port_transient_state = remote_port_state != PS_MASTER &&
+				      remote_port_state != PS_SLAVE;
 
 	rc = ptpmon_query_clock_mid(prog->ptpmon, MID_CURRENT_DATA_SET,
 				    &current_ds, sizeof(current_ds));
@@ -812,24 +718,19 @@ static bool prog_sync_done(struct prog_data *prog)
 	remote_ptpmon_sync_done = !!(llabs(rcv_ptpmon_offset) <= prog->sync_threshold);
 	remote_sysmon_sync_done = !!(llabs(rcv_sysmon_offset) <= prog->sync_threshold);
 
-	return local_ptpmon_sync_done && local_sysmon_sync_done &&
+	return !local_port_transient_state && !remote_port_transient_state &&
+	       local_ptpmon_sync_done && local_sysmon_sync_done &&
 	       remote_ptpmon_sync_done && remote_sysmon_sync_done;
 }
 
 static int prog_check_sync(struct prog_data *prog)
 {
-	int rc;
-
 	if (!prog->ptpmon)
 		return 0;
 
 	while (1) {
 		if (signal_received)
 			return -EINTR;
-
-		rc = prog_update_port_list(prog);
-		if (rc)
-			continue;
 
 		if (prog_sync_done(prog))
 			break;

commit 7962cf5bbb86c9708ca86353355d1685d3c755c8

isochron: fix buffer overrun in ptpmon_query_port_state_by_name
There is a design bug in ptpmon_query_port_state_by_name() when it calls
ptpmon_query_port_mid_extra(): since there is an extra size required for
the interface name, we should have an on-stack buffer that holds at
least sizeof(struct port_properties_np) + MAX_IFACE_LEN bytes. However,
we currently only declare an on-stack variable of sizeof(struct
port_properties_np) bytes, so the interface name is placed in what
appears to be padding memory.

For some reason this wasn't caught with plain 4-letter interface names,
but with a 6-letter interface name, it's broken:

ptpmon_query_port_state_by_name: skipping over iface name "enp7s0������", looking for "enp7s0"
Failed to read ptpmon port state: No such device

Fix this by allocating a buffer of the needed size.

Issue: https://github.com/vladimiroltean/tsn-scripts/issues/8
Fixes: 42952229c603 ("isochron: look up the port state by interface name in ptpmon")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -387,25 +387,27 @@ int ptpmon_query_port_state_by_name(struct ptpmon *ptpmon, const char *iface,
 	num_ports = __be16_to_cpu(default_ds.number_ports);
 
 	for (portnum = 1; portnum <= num_ports; portnum++) {
-		struct port_properties_np port_properties_np;
+		__u8 buf[sizeof(struct port_properties_np) + MAX_IFACE_LEN] = {0};
+		struct port_properties_np *port_properties_np;
 		struct port_identity portid;
 
 		portid_set(&portid, &default_ds.clock_identity, portnum);
 
 		rc = ptpmon_query_port_mid_extra(ptpmon, &portid,
-						 MID_PORT_PROPERTIES_NP,
-						 &port_properties_np,
-						 sizeof(port_properties_np),
+						 MID_PORT_PROPERTIES_NP, buf,
+						 sizeof(struct port_properties_np),
 						 MAX_IFACE_LEN);
 		if (rc) {
 			pr_err(rc, "Failed to query PORT_PROPERTIES_NP: %m\n");
 			return rc;
 		}
 
-		if (strcmp(port_properties_np.iface, iface))
+		port_properties_np = (struct port_properties_np *)buf;
+
+		if (strcmp(port_properties_np->iface, iface))
 			continue;
 
-		*port_state = port_properties_np.port_state;
+		*port_state = port_properties_np->port_state;
 
 		return 0;
 	}