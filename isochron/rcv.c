NXP/isochron

commit 80bf9994b28aef4c3c9a6fdc5e505b613e6b788f

isochron: allow the interface to not be the same as the ptp4l interface
As explained here:
https://github.com/vladimiroltean/tsn-scripts/issues/8#issuecomment-999432505

there exist network interfaces which have VLAN filtering forced on
(cannot be disabled). To work on these, the user must create a VLAN
interface on top of the real interface, and use isochron on that.
In this case, isochron will see VLAN-untagged traffic (basically don't
specify --vid on the sender) and the VLAN will be inserted and stripped
by the kernel.

The problem is that currently, isochron expects that ptp4l runs on the
same interface as isochron, otherwise port state monitoring won't work.
So this practically means that ptp4l should be run in the same VLAN as
isochron, which is undesirable.

To lift that limitation, we should make the ptpmon port state
comparisons more flexible. Practically speaking, we should attempt to
resolve the real device behind a potential VLAN device, and allow
isochron and ptp4l to be on separate VLAN devices as long as the real
device is the same.

We do the communication with the kernel using the rtnetlink socket, and
we need to link with libmnl for that.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/rcv.c
@@ -37,6 +37,7 @@ struct prog_data {
 	clockid_t clkid;
 	struct ptpmon *ptpmon;
 	struct sysmon *sysmon;
+	struct mnl_socket *rtnl;
 	int stats_listenfd;
 	int stats_fd;
 	int data_fd;
@@ -395,7 +396,7 @@ static int prog_forward_port_state(struct prog_data *prog)
 	int rc;
 
 	rc = ptpmon_query_port_state_by_name(prog->ptpmon, prog->if_name,
-					     &port_state);
+					     prog->rtnl, &port_state);
 	if (rc) {
 		pr_err(rc, "Failed to read ptpmon port state: %m\n");
 		isochron_send_empty_tlv(prog->stats_fd, ISOCHRON_MID_PORT_STATE);
@@ -972,6 +973,34 @@ static void prog_teardown_data_timeout_fd(struct prog_data *prog)
 	close(prog->data_timeout_fd);
 }
 
+static int prog_rtnl_open(struct prog_data *prog)
+{
+	struct mnl_socket *nl;
+
+	nl = mnl_socket_open(NETLINK_ROUTE);
+	if (!nl) {
+		perror("mnl_socket_open");
+		return -errno;
+	}
+
+	if (mnl_socket_bind(nl, 0, MNL_SOCKET_AUTOPID) < 0) {
+		perror("mnl_socket_bind");
+		return -errno;
+	}
+
+	prog->rtnl = nl;
+
+	return 0;
+}
+
+static void prog_rtnl_close(struct prog_data *prog)
+{
+	struct mnl_socket *nl = prog->rtnl;
+
+	prog->rtnl = NULL;
+	mnl_socket_close(nl);
+}
+
 static int prog_init(struct prog_data *prog)
 {
 	int rc;
@@ -980,9 +1009,13 @@ static int prog_init(struct prog_data *prog)
 	if (rc)
 		return rc;
 
-	rc = prog_init_ptpmon(prog);
+	rc = prog_rtnl_open(prog);
 	if (rc)
 		return rc;
+
+	rc = prog_init_ptpmon(prog);
+	if (rc)
+		goto out_close_rtnl;
 
 	rc = prog_init_sysmon(prog);
 	if (rc)
@@ -1019,6 +1052,8 @@ out_teardown_sysmon:
 	prog_teardown_sysmon(prog);
 out_teardown_ptpmon:
 	prog_teardown_ptpmon(prog);
+out_close_rtnl:
+	prog_rtnl_close(prog);
 	return rc;
 }
 
@@ -1234,6 +1269,7 @@ static void prog_teardown(struct prog_data *prog)
 	prog_teardown_stats_listenfd(prog);
 	prog_teardown_sysmon(prog);
 	prog_teardown_ptpmon(prog);
+	prog_rtnl_close(prog);
 }
 
 int isochron_rcv_main(int argc, char *argv[])

isochron/send.c
@@ -51,6 +51,7 @@ struct prog_data {
 	__u8 sendbuf[BUF_SIZ];
 	struct ptpmon *ptpmon;
 	struct sysmon *sysmon;
+	struct mnl_socket *rtnl;
 	enum port_state last_local_port_state;
 	enum port_state last_remote_port_state;
 	struct cmsghdr *cmsg;
@@ -591,7 +592,7 @@ static bool prog_sync_ok(struct prog_data *prog)
 	}
 
 	rc = ptpmon_query_port_state_by_name(prog->ptpmon, prog->if_name,
-					     &local_port_state);
+					     prog->rtnl, &local_port_state);
 	if (rc) {
 		pr_err(rc, "ptpmon failed to query port state: %m\n");
 		return false;
@@ -1266,6 +1267,34 @@ static void prog_teardown_trace_mark(struct prog_data *prog)
 	trace_mark_close(prog->trace_mark_fd);
 }
 
+static int prog_rtnl_open(struct prog_data *prog)
+{
+	struct mnl_socket *nl;
+
+	nl = mnl_socket_open(NETLINK_ROUTE);
+	if (!nl) {
+		perror("mnl_socket_open");
+		return -errno;
+	}
+
+	if (mnl_socket_bind(nl, 0, MNL_SOCKET_AUTOPID) < 0) {
+		perror("mnl_socket_bind");
+		return -errno;
+	}
+
+	prog->rtnl = nl;
+
+	return 0;
+}
+
+static void prog_rtnl_close(struct prog_data *prog)
+{
+	struct mnl_socket *nl = prog->rtnl;
+
+	prog->rtnl = NULL;
+	mnl_socket_close(nl);
+}
+
 static int prog_init(struct prog_data *prog)
 {
 	int rc;
@@ -1276,9 +1305,13 @@ static int prog_init(struct prog_data *prog)
 
 	prog->clkid = CLOCK_TAI;
 
-	rc = prog_init_stats_socket(prog);
+	rc = prog_rtnl_open(prog);
 	if (rc)
 		goto out;
+
+	rc = prog_init_stats_socket(prog);
+	if (rc)
+		goto out_close_rtnl;
 
 	rc = prog_query_dest_mac(prog);
 	if (rc)
@@ -1336,6 +1369,8 @@ out_close_data_fd:
 	prog_teardown_data_fd(prog);
 out_stats_socket_teardown:
 	prog_teardown_stats_socket(prog);
+out_close_rtnl:
+	prog_rtnl_close(prog);
 out:
 	return rc;
 }
@@ -1350,6 +1385,7 @@ static void prog_teardown(struct prog_data *prog)
 	prog_teardown_trace_mark(prog);
 	prog_teardown_data_fd(prog);
 	prog_teardown_stats_socket(prog);
+	prog_rtnl_close(prog);
 }
 
 static int prog_parse_args(int argc, char **argv, struct prog_data *prog)

isochron/Makefile
@@ -2,6 +2,7 @@ prefix ?= /usr/local
 exec_prefix ?= ${prefix}
 bindir ?= ${exec_prefix}/bin
 etcdir ?= /etc
+PKG_CONFIG ?= pkg-config
 
 src := \
 	argparser.o \
@@ -12,6 +13,7 @@ src := \
 	ptpmon.o \
 	rcv.o \
 	report.o \
+	rtnl.o \
 	send.o \
 	sysmon.o
 
@@ -28,6 +30,9 @@ get_man_section = $(lastword $(subst ., ,$1))
 get_manpage_destination = $(join $(DESTDIR)${mandir}/man, \
                           $(join $(call get_man_section,$1)/, \
                           $(subst docs/man/,,$1)))
+
+CFLAGS  += $(shell ${PKG_CONFIG} --cflags libmnl)
+LDFLAGS += $(shell ${PKG_CONFIG} --libs libmnl)
 
 TARGET := isochron/isochron
 

isochron/common.c
@@ -28,6 +28,7 @@
 /* For va_start and va_end */
 #include <stdarg.h>
 #include "common.h"
+#include "rtnl.h"
 
 void pr_err(int rc, const char *fmt, ...)
 {
@@ -371,11 +372,17 @@ void isochron_fixup_kernel_utc_offset(int ptp_utc_offset)
 }
 
 int ptpmon_query_port_state_by_name(struct ptpmon *ptpmon, const char *iface,
+				    struct mnl_socket *rtnl,
 				    enum port_state *port_state)
 {
 	struct default_ds default_ds;
+	char real_ifname[IFNAMSIZ];
 	int portnum, num_ports;
 	int rc;
+
+	rc = vlan_resolve_real_dev(rtnl, iface, real_ifname);
+	if (rc)
+		return rc;
 
 	rc = ptpmon_query_clock_mid(ptpmon, MID_DEFAULT_DATA_SET,
 				    &default_ds, sizeof(default_ds));
@@ -389,6 +396,7 @@ int ptpmon_query_port_state_by_name(struct ptpmon *ptpmon, const char *iface,
 	for (portnum = 1; portnum <= num_ports; portnum++) {
 		__u8 buf[sizeof(struct port_properties_np) + MAX_IFACE_LEN] = {0};
 		struct port_properties_np *port_properties_np;
+		char real_port_ifname[IFNAMSIZ];
 		struct port_identity portid;
 
 		portid_set(&portid, &default_ds.clock_identity, portnum);
@@ -404,7 +412,12 @@ int ptpmon_query_port_state_by_name(struct ptpmon *ptpmon, const char *iface,
 
 		port_properties_np = (struct port_properties_np *)buf;
 
-		if (strcmp(port_properties_np->iface, iface))
+		rc = vlan_resolve_real_dev(rtnl, port_properties_np->iface,
+					   real_port_ifname);
+		if (rc)
+			return rc;
+
+		if (strcmp(real_port_ifname, real_ifname))
 			continue;
 
 		*port_state = port_properties_np->port_state;

isochron/common.h
@@ -9,6 +9,7 @@
 #define _COMMON_H
 
 #include <arpa/inet.h>
+#include <libmnl/libmnl.h>
 #include <linux/types.h>
 #include <netinet/ether.h>
 #include <sched.h>
@@ -249,6 +250,7 @@ master_offset_from_current_ds(const struct current_ds *current_ds)
 }
 
 int ptpmon_query_port_state_by_name(struct ptpmon *ptpmon, const char *iface,
+				    struct mnl_socket *rtnl,
 				    enum port_state *port_state);
 
 int isochron_handle_signals(void (*handler)(int signo));

isochron/rtnl.c
@@ -1,0 +1,257 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright 2022 NXP */
+#include <linux/if_link.h>
+#include <linux/rtnetlink.h>
+#include <errno.h>
+#include <string.h>
+#include "common.h"
+
+struct vlan_info {
+	const char *kind;
+	__u32 link_ifindex;
+};
+
+struct ifname_info {
+	const char *ifname;
+};
+
+struct ifindex_info {
+	int ifindex;
+};
+
+static int rtnl_parse_vlan_linkinfo(const struct nlattr *attr, void *data)
+{
+	__u16 type = mnl_attr_get_type(attr);
+	struct vlan_info *v = data;
+
+	/* skip unsupported attributes */
+	if (mnl_attr_type_valid(attr, IFLA_INFO_MAX) < 0)
+		return MNL_CB_OK;
+
+	switch (type) {
+	case IFLA_INFO_KIND:
+		if (mnl_attr_validate(attr, MNL_TYPE_STRING) < 0) {
+			perror("mnl_attr_validate");
+			return MNL_CB_ERROR;
+		}
+		v->kind = mnl_attr_get_str(attr);
+		break;
+	}
+
+	return MNL_CB_OK;
+}
+
+static int rtnl_parse_vlan_attr(const struct nlattr *attr, void *data)
+{
+	struct vlan_info *v = data;
+
+	/* skip unsupported attributes */
+	if (mnl_attr_type_valid(attr, IFLA_MAX) < 0)
+		return MNL_CB_OK;
+
+	switch (mnl_attr_get_type(attr)) {
+	case IFLA_LINK:
+		if (mnl_attr_validate(attr, MNL_TYPE_U32) < 0) {
+			perror("mnl_attr_validate");
+			return MNL_CB_ERROR;
+		}
+
+		v->link_ifindex = mnl_attr_get_u32(attr);
+		break;
+	case IFLA_LINKINFO:
+		mnl_attr_parse_nested(attr, rtnl_parse_vlan_linkinfo, v);
+
+		break;
+	}
+
+	return MNL_CB_OK;
+}
+
+static int rtnl_parse_vlan_nlh(const struct nlmsghdr *nlh, void *data)
+{
+	struct ifinfomsg *ifm = mnl_nlmsg_get_payload(nlh);
+	struct vlan_info *v = data;
+
+	mnl_attr_parse(nlh, sizeof(*ifm), rtnl_parse_vlan_attr, v);
+
+	return MNL_CB_STOP;
+}
+
+static int rtnl_parse_ifname_attr(const struct nlattr *attr, void *data)
+{
+	struct ifname_info *i = data;
+
+	/* skip unsupported attributes */
+	if (mnl_attr_type_valid(attr, IFLA_MAX) < 0)
+		return MNL_CB_OK;
+
+	switch (mnl_attr_get_type(attr)) {
+	case IFLA_IFNAME:
+		if (mnl_attr_validate(attr, MNL_TYPE_STRING) < 0) {
+			perror("mnl_attr_validate");
+			return MNL_CB_ERROR;
+		}
+
+		i->ifname = mnl_attr_get_str(attr);
+		break;
+	}
+
+	return MNL_CB_OK;
+}
+
+static int rtnl_parse_ifname_nlh(const struct nlmsghdr *nlh, void *data)
+{
+	struct ifinfomsg *ifm = mnl_nlmsg_get_payload(nlh);
+	struct ifname_info *i = data;
+
+	mnl_attr_parse(nlh, sizeof(*ifm), rtnl_parse_ifname_attr, i);
+
+	return MNL_CB_STOP;
+}
+
+static int rtnl_parse_ifindex_nlh(const struct nlmsghdr *nlh, void *data)
+{
+	struct ifinfomsg *ifm = mnl_nlmsg_get_payload(nlh);
+	struct ifindex_info *i = data;
+
+	i->ifindex = ifm->ifi_index;
+
+	return MNL_CB_STOP;
+}
+
+static int rtnl_getlink_by_ifname(struct mnl_socket *rtnl, const char *ifname,
+				  mnl_cb_t cb, void *data)
+{
+	char buf[MNL_SOCKET_BUFFER_SIZE];
+	struct ifinfomsg *ifm;
+	struct nlmsghdr *nlh;
+	__u32 seq, portid;
+	int rc;
+
+	portid = mnl_socket_get_portid(rtnl);
+
+	nlh = mnl_nlmsg_put_header(buf);
+	nlh->nlmsg_type = RTM_GETLINK;
+
+	nlh->nlmsg_flags = NLM_F_REQUEST;
+	nlh->nlmsg_seq = seq = time(NULL);
+
+	ifm = mnl_nlmsg_put_extra_header(nlh, sizeof(*ifm));
+	ifm->ifi_family = AF_UNSPEC;
+	ifm->ifi_change = 0;
+	ifm->ifi_flags = 0;
+
+	mnl_attr_put_str(nlh, IFLA_IFNAME, ifname);
+
+	if (mnl_socket_sendto(rtnl, nlh, nlh->nlmsg_len) < 0) {
+		perror("mnl_socket_sendto");
+		return -errno;
+	}
+
+	rc = mnl_socket_recvfrom(rtnl, buf, sizeof(buf));
+	if (rc < 0) {
+		perror("mnl_socket_recvfrom");
+		return -errno;
+	}
+
+	rc = mnl_cb_run(buf, rc, seq, portid, cb, data);
+	if (rc < 0) {
+		perror("mnl_cb_run");
+		return -errno;
+	}
+
+	return 0;
+}
+
+static int rtnl_getlink_by_ifindex(struct mnl_socket *rtnl, int ifindex,
+				   mnl_cb_t cb, void *data)
+{
+	char buf[MNL_SOCKET_BUFFER_SIZE];
+	struct ifinfomsg *ifm;
+	struct nlmsghdr *nlh;
+	__u32 seq, portid;
+	int rc;
+
+	portid = mnl_socket_get_portid(rtnl);
+
+	nlh = mnl_nlmsg_put_header(buf);
+	nlh->nlmsg_type = RTM_GETLINK;
+
+	nlh->nlmsg_flags = NLM_F_REQUEST;
+	nlh->nlmsg_seq = seq = time(NULL);
+
+	ifm = mnl_nlmsg_put_extra_header(nlh, sizeof(*ifm));
+	ifm->ifi_family = AF_UNSPEC;
+	ifm->ifi_index = ifindex;
+	ifm->ifi_change = 0;
+	ifm->ifi_flags = 0;
+
+	if (mnl_socket_sendto(rtnl, nlh, nlh->nlmsg_len) < 0) {
+		perror("mnl_socket_sendto");
+		return -errno;
+	}
+
+	rc = mnl_socket_recvfrom(rtnl, buf, sizeof(buf));
+	if (rc < 0) {
+		perror("mnl_socket_recvfrom");
+		return -errno;
+	}
+
+	rc = mnl_cb_run(buf, rc, seq, portid, cb, data);
+	if (rc < 0) {
+		perror("mnl_cb_run");
+		return -errno;
+	}
+
+	return 0;
+}
+
+static int rtnl_fill_vlan_info(struct mnl_socket *rtnl, int ifindex,
+			       struct vlan_info *v)
+{
+	return rtnl_getlink_by_ifindex(rtnl, ifindex, rtnl_parse_vlan_nlh, v);
+}
+
+static int rtnl_fill_ifindex_name(struct mnl_socket *rtnl, int ifindex,
+				  struct ifname_info *i)
+{
+	return rtnl_getlink_by_ifindex(rtnl, ifindex, rtnl_parse_ifname_nlh, i);
+}
+
+static int rtnl_fill_ifname_ifindex(struct mnl_socket *rtnl, const char *ifname,
+				    struct ifindex_info *i)
+{
+	return rtnl_getlink_by_ifname(rtnl, ifname, rtnl_parse_ifindex_nlh, i);
+}
+
+int vlan_resolve_real_dev(struct mnl_socket *rtnl, const char *vlan_ifname,
+			  char *real_ifname)
+{
+	struct ifindex_info ifindex = {};
+	struct ifname_info ifname = {};
+	struct vlan_info v = {};
+	int rc;
+
+	rc = rtnl_fill_ifname_ifindex(rtnl, vlan_ifname, &ifindex);
+	if (rc)
+		return rc;
+
+	do {
+		rc = rtnl_fill_vlan_info(rtnl, ifindex.ifindex, &v);
+		if (rc)
+			return rc;
+
+		if (!v.kind || strcmp(v.kind, "vlan"))
+			break;
+
+		ifindex.ifindex = v.link_ifindex;
+	} while (true);
+
+	rc = rtnl_fill_ifindex_name(rtnl, ifindex.ifindex, &ifname);
+	if (rc)
+		return rc;
+
+	strcpy(real_ifname, ifname.ifname);
+
+	return 0;
+}

isochron/rtnl.h
@@ -1,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright 2022 NXP */
+#ifndef _RTNL_H
+#define _RTNL_H
+
+int vlan_resolve_real_dev(struct mnl_socket *rtnl, const char *vlan_ifname,
+			  char *real_ifname);
+
+#endif

commit aa21e6dbbfc8391ec1c24095fcda88c67dc95eee

isochron: call mnl_socket_close on error path of prog_rtnl_open
This was copied from the libmnl examples, but there we have an
exit(EXIT_FAILURE), so I suppose they don't care about error handling as
much. Anyway it's a bug, so fix it.

Fixes: 80bf9994b28a ("isochron: allow the interface to not be the same as the ptp4l interface")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/rcv.c
@@ -985,6 +985,7 @@ static int prog_rtnl_open(struct prog_data *prog)
 
 	if (mnl_socket_bind(nl, 0, MNL_SOCKET_AUTOPID) < 0) {
 		perror("mnl_socket_bind");
+		mnl_socket_close(nl);
 		return -errno;
 	}
 

isochron/send.c
@@ -1279,6 +1279,7 @@ static int prog_rtnl_open(struct prog_data *prog)
 
 	if (mnl_socket_bind(nl, 0, MNL_SOCKET_AUTOPID) < 0) {
 		perror("mnl_socket_bind");
+		mnl_socket_close(nl);
 		return -errno;
 	}
 