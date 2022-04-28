NXP/isochron

commit bccad2d20ee473e0b82faf726bf6e278d821fbd5

isochron: send: fix subtly invalid software TX timestamps
We calculate the time it takes to send a packet based on the delta
between the application wakeup time and the software TX timestamp, but
even with perfect sync and a ridiculously large cycle time, one can see
that isochron apparently thinks we've exceeded the alloted time,
although we didn't.

Sending one packet takes on average 90.229% of the cycle time (min 0.873% max 100.200%)
Waking up takes on average 0.152% of the cycle time (min 0.144% max 0.182%)

The Linux TX timestamping UAPI is garbage and my understanding of it
was even worse.

Documentation/networking/timestamping.rst says:

| Note that if the SO_TIMESTAMP or SO_TIMESTAMPNS option is enabled
| together with SO_TIMESTAMPING using SOF_TIMESTAMPING_SOFTWARE, a false
| software timestamp will be generated in the recvmsg() call and passed
| in ts[0] when a real software timestamp is missing. This happens also
| on hardware transmit timestamps.

which is basically what is happening here. We aren't seeing a software
timestamp filled in by the driver, but instead a fake one created by the
kernel to make us happy and shut up. Even though one can see that the
software TX timestamp is larger than the hardware TX timestamp, which is
absurd.

So if we want reliable SOF_TIMESTAMPING_OPT_TX_SWHW behavior (both
software and hardware timestamps for the same skb), we should drop
support for retrieving software timestamps through SO_TIMESTAMPNS and go
all the way with SO_TIMESTAMPING.

Except that SO_TIMESTAMPING has other crap of its own:

| SOF_TIMESTAMPING_OPT_TX_SWHW:
|   Request both hardware and software timestamps for outgoing packets
|   when SOF_TIMESTAMPING_TX_HARDWARE and SOF_TIMESTAMPING_TX_SOFTWARE
|   are enabled at the same time. If both timestamps are generated,
|   two separate messages will be looped to the socket's error queue,
|   each containing just one timestamp.

And we aren't yet prepared to deal with multiple sk_receive() calls for

isochron/common.c
@@ -433,12 +433,15 @@ static int hwts_init(int fd, const char *if_name, int rx_filter, int tx_type)
 int sk_timestamping_init(int fd, const char *if_name, bool on)
 {
 	int rc, filter, flags, tx_type;
-	int enabled = on ? 1 : 0;
 
 	flags = SOF_TIMESTAMPING_TX_HARDWARE |
 		SOF_TIMESTAMPING_RX_HARDWARE |
+		SOF_TIMESTAMPING_TX_SOFTWARE |
+		SOF_TIMESTAMPING_RX_SOFTWARE |
+		SOF_TIMESTAMPING_SOFTWARE |
 		SOF_TIMESTAMPING_RAW_HARDWARE |
-		SOF_TIMESTAMPING_OPT_TX_SWHW;
+		SOF_TIMESTAMPING_OPT_TX_SWHW |
+		SOF_TIMESTAMPING_OPT_ID;
 
 	filter = HWTSTAMP_FILTER_ALL;
 
@@ -457,14 +460,6 @@ int sk_timestamping_init(int fd, const char *if_name, bool on)
 		fprintf(stderr, "ioctl SO_TIMESTAMPING failed: %s\n",
 			strerror(errno));
 		return -1;
-	}
-
-	rc = setsockopt(fd, SOL_SOCKET, SO_TIMESTAMPNS, &enabled,
-			sizeof(enabled));
-	if (rc < 0) {
-		fprintf(stderr, "ioctl SO_TIMESTAMPNS failed: %s\n",
-			strerror(errno));
-		return rc;
 	}
 
 	flags = 1;
@@ -502,8 +497,7 @@ int sk_receive(int fd, void *buf, int buflen, struct isochron_timestamp *tstamp,
 
 		rc = poll(&pfd, 1, timeout);
 		if (rc == 0) {
-			/* Timed out waiting for TX timestamp */
-			return -EAGAIN;
+			return 0;
 		} else if (rc < 0) {
 			fprintf(stderr, "poll for tx timestamp failed: %s\n",
 				strerror(rc));
@@ -526,22 +520,20 @@ int sk_receive(int fd, void *buf, int buflen, struct isochron_timestamp *tstamp,
 		int level = cm->cmsg_level;
 		int type  = cm->cmsg_type;
 
-		if (level == SOL_SOCKET && type == SO_TIMESTAMPING) {
+		if (level == SOL_SOCKET && type == SCM_TIMESTAMPING) {
+			struct scm_timestamping *tss;
+
 			if (cm->cmsg_len < sizeof(*ts) * 3) {
 				fprintf(stderr, "short SO_TIMESTAMPING message\n");
 				return -1;
 			}
-			ts = (struct timespec *) CMSG_DATA(cm);
-			if (tstamp)
-				tstamp->hw = ts[2];
-		} else if (level == SOL_SOCKET && type == SO_TIMESTAMPNS) {
-			if (cm->cmsg_len < sizeof(*ts)) {
-				fprintf(stderr, "short SO_TIMESTAMPNS message\n");
-				return -1;
+
+			tss = (struct scm_timestamping *)CMSG_DATA(cm);
+
+			if (tstamp) {
+				tstamp->sw = tss->ts[0];
+				tstamp->hw = tss->ts[2];
 			}
-			ts = (struct timespec *) CMSG_DATA(cm);
-			if (tstamp)
-				tstamp->sw = ts[0];
 		} else if (level == SOL_PACKET && type == PACKET_TX_TIMESTAMP) {
 			struct sock_extended_err *sock_err;
 			char txtime_buf[TIMESPEC_BUFSIZ];
@@ -553,7 +545,8 @@ int sk_receive(int fd, void *buf, int buflen, struct isochron_timestamp *tstamp,
 
 			switch (sock_err->ee_origin) {
 			case SO_EE_ORIGIN_TIMESTAMPING:
-				/* Normal cmsg received for TX timestamping */
+				if (tstamp)
+					tstamp->tskey = sock_err->ee_data;
 				break;
 			case SO_EE_ORIGIN_TXTIME:
 				txtime = ((__u64)sock_err->ee_data << 32) +
@@ -622,6 +615,16 @@ void isochron_log_data(struct isochron_log *log, void *data, int len)
 
 	memcpy(p, data, len);
 	log->buf_len += len;
+}
+
+/* Get a reference to an existing log entry */
+void *isochron_log_get_entry(struct isochron_log *log, size_t entry_size,
+			     int index)
+{
+	if (index * entry_size > log->buf_len)
+		return NULL;
+
+	return log->buf + entry_size * index;
 }
 
 int isochron_log_xmit(struct isochron_log *log, int fd)

isochron/common.h
@@ -139,6 +139,8 @@ struct isochron_log {
 
 int isochron_log_init(struct isochron_log *log, size_t size);
 void isochron_log_data(struct isochron_log *log, void *data, int len);
+void *isochron_log_get_entry(struct isochron_log *log, size_t entry_size,
+			     int index);
 int isochron_log_xmit(struct isochron_log *log, int fd);
 int isochron_log_recv(struct isochron_log *log, int fd);
 void isochron_log_teardown(struct isochron_log *log);
@@ -306,8 +308,9 @@ void prog_usage(const char *prog_name, struct prog_arg *prog_args,
 		int prog_args_size);
 
 struct isochron_timestamp {
-	struct timespec		hw;
-	struct timespec		sw;
+	struct timespec hw;
+	struct timespec sw;
+	__u32 tskey;
 };
 
 struct isochron_send_pkt_data {

isochron/send.c
@@ -43,7 +43,7 @@ struct prog_data {
 	unsigned char src_mac[ETH_ALEN];
 	char if_name[IFNAMSIZ];
 	char uds_remote[UNIX_PATH_MAX];
-	char sendbuf[BUF_SIZ];
+	__u8 sendbuf[BUF_SIZ];
 	struct ptpmon *ptpmon;
 	struct sysmon *sysmon;
 	enum port_state last_local_port_state;
@@ -500,30 +500,8 @@ static void prog_teardown_stats_socket(struct prog_data *prog)
 	close(prog->stats_fd);
 }
 
-static void process_txtstamp(struct prog_data *prog, const __u8 *buf,
-			     struct isochron_timestamp *tstamp)
-{
-	struct isochron_send_pkt_data send_pkt = {0};
-	struct isochron_header *hdr;
-
-	if (prog->l2)
-		hdr = (struct isochron_header *)(buf + prog->l2_header_len);
-	else
-		hdr = (struct isochron_header *)(buf + prog->l4_header_len);
-
-	send_pkt.tx_time = hdr->tx_time;
-	send_pkt.wakeup = hdr->wakeup;
-	send_pkt.seqid = hdr->seqid;
-	send_pkt.hwts = __cpu_to_be64(timespec_to_ns(&tstamp->hw));
-	send_pkt.swts = __cpu_to_be64(utc_to_tai(timespec_to_ns(&tstamp->sw),
-						 prog->utc_tai_offset));
-
-	isochron_log_data(&prog->log, &send_pkt, sizeof(send_pkt));
-
-	prog->timestamped++;
-}
-
-static void log_no_tstamp(struct prog_data *prog, const char *buf)
+/* Timestamps will come later */
+static void prog_log_packet_no_tstamp(struct prog_data *prog, const __u8 *buf)
 {
 	struct isochron_send_pkt_data send_pkt = {0};
 	struct isochron_header *hdr;
@@ -535,18 +513,79 @@ static void log_no_tstamp(struct prog_data *prog, const char *buf)
 	if (prog->l2)
 		hdr = (struct isochron_header *)(buf + prog->l2_header_len);
 	else
-		hdr = (struct isochron_header *)buf;
+		hdr = (struct isochron_header *)(buf + prog->l4_header_len);
 
 	send_pkt.tx_time = hdr->tx_time;
 	send_pkt.wakeup = hdr->wakeup;
 	send_pkt.seqid = hdr->seqid;
+	send_pkt.swts = 0;
+	send_pkt.hwts = 0;
 
 	isochron_log_data(&prog->log, &send_pkt, sizeof(send_pkt));
 }
 
+static bool
+isochron_pkt_fully_timestamped(struct isochron_send_pkt_data *send_pkt)
+{
+	return send_pkt->hwts && send_pkt->swts;
+}
+
+/* Propagates the return code from sk_receive, i.e. the number of bytes
+ * read from the socket (if we could successfully read a timestamp),
+ * or a negative error code.
+ */
+static int prog_poll_txtstamps(struct prog_data *prog, int timeout)
+{
+	struct isochron_send_pkt_data *send_pkt;
+	struct isochron_timestamp tstamp = {};
+	__be64 hwts, swts, swts_utc;
+	__u8 err_pkt[BUF_SIZ];
+	int rc;
+
+	rc = sk_receive(prog->data_fd, err_pkt, BUF_SIZ, &tstamp, MSG_ERRQUEUE,
+			timeout);
+	if (rc <= 0)
+		return rc;
+
+	/* Since we log the packets in the same order as the kernel keeps
+	 * track of timestamps using SOF_TIMESTAMPING_OPT_ID on the data
+	 * socket, finding packets by timestamp key and patching their
+	 * timestamp is a cheap hack to avoid a linear lookup.
+	 */
+	send_pkt = isochron_log_get_entry(&prog->log, sizeof(*send_pkt),
+					  tstamp.tskey);
+	if (!send_pkt) {
+		fprintf(stderr,
+			"received timestamp for unknown key %u\n",
+			tstamp.tskey);
+		return -EINVAL;
+	}
+
+	if (isochron_pkt_fully_timestamped(send_pkt)) {
+		fprintf(stderr,
+			"received duplicate timestamp for packet key %u already fully timestamped\n",
+			tstamp.tskey);
+		return -EINVAL;
+	}
+
+	swts = __cpu_to_be64(utc_to_tai(timespec_to_ns(&tstamp.sw),
+					prog->utc_tai_offset));
+	swts_utc = __cpu_to_be64(timespec_to_ns(&tstamp.sw));
+	hwts = __cpu_to_be64(timespec_to_ns(&tstamp.hw));
+
+	if (swts_utc)
+		send_pkt->swts = swts;
+	if (hwts)
+		send_pkt->hwts = hwts;
+
+	if (isochron_pkt_fully_timestamped(send_pkt))
+		prog->timestamped++;
+
+	return rc;
+}
+
 static int do_work(struct prog_data *prog, int iteration, __s64 scheduled)
 {
-	struct isochron_timestamp tstamp = {0};
 	struct isochron_header *hdr;
 	struct timespec now_ts;
 	__u8 err_pkt[BUF_SIZ];
@@ -583,38 +622,30 @@ static int do_work(struct prog_data *prog, int iteration, __s64 scheduled)
 
 	trace(prog, "send seqid %d end\n", iteration);
 
-	if (prog->do_ts) {
-		rc = sk_receive(prog->data_fd, err_pkt, BUF_SIZ, &tstamp,
-				MSG_ERRQUEUE, 0);
-		if (rc == -EAGAIN)
-			return 0;
-		if (rc < 0)
-			return rc;
+	prog_log_packet_no_tstamp(prog, prog->sendbuf);
 
-		/* If a timestamp becomes available, process it now
-		 * (don't wait for later)
-		 */
-		process_txtstamp(prog, err_pkt, &tstamp);
-	} else {
-		log_no_tstamp(prog, prog->sendbuf);
+	if (prog->do_ts) {
+		do {
+			/* Break on errors and on no immediate availability
+			 * of timestamps
+			 */
+			rc = prog_poll_txtstamps(prog, 0);
+		} while (rc > 0);
 	}
 
-	return 0;
+	return rc;
 }
 
 static int wait_for_txtimestamps(struct prog_data *prog)
 {
-	unsigned char err_pkt[BUF_SIZ];
-	struct isochron_timestamp tstamp;
 	int rc;
 
 	if (!prog->do_ts)
 		return 0;
 
 	while (prog->timestamped < prog->sent) {
-		rc = sk_receive(prog->data_fd, err_pkt, BUF_SIZ, &tstamp,
-				MSG_ERRQUEUE, TXTSTAMP_TIMEOUT_MS);
-		if (rc < 0) {
+		rc = prog_poll_txtstamps(prog, TXTSTAMP_TIMEOUT_MS);
+		if (rc <= 0) {
 			fprintf(stderr,
 				"Timed out waiting for TX timestamp: %d (%s)\n",
 				rc, strerror(-rc));
@@ -622,11 +653,6 @@ static int wait_for_txtimestamps(struct prog_data *prog)
 				prog->sent - prog->timestamped);
 			return rc;
 		}
-
-		/* If a timestamp becomes available, process it now
-		 * (don't wait for later)
-		 */
-		process_txtstamp(prog, err_pkt, &tstamp);
 	}
 
 	return 0;

commit c9030922c130c9ec35a7accd452bac7213067dbe

isochron: receive tskey, tstype and txtime for IPv4/IPv6 sockets too
Currently we only process control messages for SOL_PACKET (L2), but we
should do so for SOL_IP and SOL_IPV6 as well, otherwise we cannot
retrieve datagram timestamps, and what we see are some weird messages

 unknown cmsg level 0 type 11

Fixes: bccad2d20ee4 ("isochron: send: fix subtly invalid software TX timestamps")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/common.c
@@ -348,7 +348,9 @@ int sk_receive(int fd, void *buf, int buflen, struct isochron_timestamp *tstamp,
 				tstamp->sw = tss->ts[0];
 				tstamp->hw = tss->ts[2];
 			}
-		} else if (level == SOL_PACKET && type == PACKET_TX_TIMESTAMP) {
+		} else if ((level == SOL_PACKET && type == PACKET_TX_TIMESTAMP) ||
+			   (level == SOL_IP && type == IP_RECVERR) ||
+			   (level == SOL_IPV6 && type == IPV6_RECVERR)) {
 			struct sock_extended_err *sock_err;
 			char txtime_buf[TIMESPEC_BUFSIZ];
 			__u64 txtime;