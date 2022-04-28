NXP/isochron

commit d6f8d09b2fcef099ed2f86a1650d4e29de58de3a

isochron: report: save and view logs from the filesystem
The isochron sender is becoming less and less focused, it is gathering
more data and trying to print it in a way which may or may not be
meaningful to the end user, like for example the metric statistics that
we extract and print are pretty much arbitrary.

Furthermore, right now we don't even print all the data we gather, for
example we omit printing the software timestamps per packet even though
we calculate statistics based on them. This is because printing
everything we have would fill even a very wide line of text, which is
unpleasant for a user to say the least.

So it looks like it may be just simpler for the sender to log what it
has collected to the filesystem, then leave the task for a different
program to interpret that data. That different program can receive input
from the user as to what data to filter for and how to print it. That is
the "isochron record" sub-program.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/log.c
@@ -1,17 +1,50 @@
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright 2019-2021 NXP */
 #include <errno.h>
+#include <fcntl.h>
 #include <limits.h>
 #include <math.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "common.h"
 #include "endian.h"
 #include "log.h"
 
 #define ISOCHRON_LOG_VERSION	4
+
+#define FILEMODE (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP) /*0660*/
+
+#define BIT(nr)			(1UL << (nr))
+
+#define ISOCHRON_FLAG_OMIT_SYNC		BIT(0)
+#define ISOCHRON_FLAG_DO_TS		BIT(1)
+#define ISOCHRON_FLAG_TAPRIO		BIT(2)
+#define ISOCHRON_FLAG_TXTIME		BIT(3)
+#define ISOCHRON_FLAG_DEADLINE		BIT(4)
+
+struct isochron_log_file_header {
+	char		magic[8];
+	__be32		version;
+	__be32		packet_count;
+	__be16		frame_size;
+	__be16		flags;
+	__be32		reserved;
+	__be64		base_time;
+	__be64		advance_time;
+	__be64		shift_time;
+	__be64		cycle_time;
+	__be64		window_size;
+	__be64		send_log_start;
+	__be64		rcv_log_start;
+	__be32		send_log_size;
+	__be32		rcv_log_size;
+	__be64		reserved2;
+} __attribute((packed));
 
 struct isochron_packet_metrics {
 	LIST_ENTRY(isochron_packet_metrics) list;
@@ -532,4 +565,171 @@ int isochron_log_init(struct isochron_log *log, size_t size)
 void isochron_log_teardown(struct isochron_log *log)
 {
 	free(log->buf);
+}
+
+int isochron_log_load(const char *file, struct isochron_log *send_log,
+		      struct isochron_log *rcv_log, long *packet_count,
+		      long *frame_size, bool *omit_sync, bool *do_ts,
+		      bool *taprio, bool *txtime, bool *deadline,
+		      __s64 *base_time, __s64 *advance_time, __s64 *shift_time,
+		      __s64 *cycle_time, __s64 *window_size)
+{
+	struct isochron_log_file_header header;
+	size_t len;
+	int fd, rc;
+	int flags;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0) {
+		perror("open");
+		rc = fd;
+		goto out;
+	}
+
+	len = read_exact(fd, &header, sizeof(header));
+	if (len <= 0) {
+		perror("isochron log header write");
+		rc = len;
+		goto out_close;
+	}
+
+	if (strcmp(header.magic, "ISOCHRON")) {
+		fprintf(stderr, "Unrecognized file format\n");
+		rc = -EINVAL;
+		goto out_close;
+	}
+
+	flags = __be16_to_cpu(header.flags);
+	*omit_sync = !!(flags & ISOCHRON_FLAG_OMIT_SYNC);
+	*do_ts = !!(flags & ISOCHRON_FLAG_DO_TS);
+	*taprio = !!(flags & ISOCHRON_FLAG_TAPRIO);
+	*txtime = !!(flags & ISOCHRON_FLAG_TXTIME);
+	*deadline = !!(flags & ISOCHRON_FLAG_DEADLINE);
+
+	*packet_count = __be32_to_cpu(header.packet_count);
+	*frame_size = __be16_to_cpu(header.frame_size);
+	*base_time = (__s64 )__be64_to_cpu(header.base_time);
+	*advance_time = (__s64 )__be64_to_cpu(header.advance_time);
+	*shift_time = (__s64 )__be64_to_cpu(header.shift_time);
+	*cycle_time = (__s64 )__be64_to_cpu(header.cycle_time);
+	*window_size = (__s64 )__be64_to_cpu(header.window_size);
+
+	if (lseek(fd, __be64_to_cpu(header.send_log_start), SEEK_SET) < 0) {
+		perror("isochron send log lseek");
+		rc = -errno;
+		goto out_close;
+	}
+
+	rc = isochron_log_init(send_log, __be32_to_cpu(header.send_log_size));
+	if (rc) {
+		fprintf(stderr, "failed to allocate memory for send log\n");
+		goto out_close;
+	}
+
+	len = read_exact(fd, send_log->buf, send_log->size);
+	if (len <= 0) {
+		perror("isochron send log read");
+		rc = len;
+		goto out_send_log_teardown;
+	}
+
+	if (lseek(fd, __be64_to_cpu(header.rcv_log_start), SEEK_SET) < 0) {
+		perror("isochron rcv log lseek");
+		rc = -errno;
+		goto out_send_log_teardown;
+	}
+
+	rc = isochron_log_init(rcv_log, __be32_to_cpu(header.rcv_log_size));
+	if (rc) {
+		fprintf(stderr, "failed to allocate memory for rcv log\n");
+		goto out_send_log_teardown;
+	}
+
+	len = read_exact(fd, rcv_log->buf, rcv_log->size);
+	if (len <= 0) {
+		perror("isochron rcv log read");
+		rc = len;
+		goto out_rcv_log_teardown;
+	}
+
+	close(fd);
+
+	return 0;
+
+out_rcv_log_teardown:
+	isochron_log_teardown(rcv_log);
+out_send_log_teardown:
+	isochron_log_teardown(send_log);
+out_close:
+	close(fd);
+out:
+	return rc;
+}
+
+int isochron_log_save(const char *file, const struct isochron_log *send_log,
+		      const struct isochron_log *rcv_log, long packet_count,
+		      long frame_size, bool omit_sync, bool do_ts, bool taprio,
+		      bool txtime, bool deadline, __s64 base_time,
+		      __s64 advance_time, __s64 shift_time, __s64 cycle_time,
+		      __s64 window_size)
+{
+	struct isochron_log_file_header header = {
+		.version	= __cpu_to_be32(ISOCHRON_LOG_VERSION),
+		.packet_count	= __cpu_to_be32(packet_count),
+		.frame_size	= __cpu_to_be16(frame_size),
+		.base_time	= __cpu_to_be64(base_time),
+		.advance_time	= __cpu_to_be64(advance_time),
+		.shift_time	= __cpu_to_be64(shift_time),
+		.cycle_time	= __cpu_to_be64(cycle_time),
+		.window_size	= __cpu_to_be64(window_size),
+	};
+	int flags = 0;
+	size_t len;
+	int fd;
+
+	if (omit_sync)
+		flags |= ISOCHRON_FLAG_OMIT_SYNC;
+	if (do_ts)
+		flags |= ISOCHRON_FLAG_DO_TS;
+	if (taprio)
+		flags |= ISOCHRON_FLAG_TAPRIO;
+	if (txtime)
+		flags |= ISOCHRON_FLAG_TXTIME;
+	if (deadline)
+		flags |= ISOCHRON_FLAG_DEADLINE;
+
+	strcpy(header.magic, "ISOCHRON");
+	header.flags = __cpu_to_be16(flags);
+	header.send_log_start = __cpu_to_be64(sizeof(header));
+	header.send_log_size = __cpu_to_be32(send_log->size);
+	header.rcv_log_start = __cpu_to_be64(sizeof(header) + send_log->size);
+	header.rcv_log_size = __cpu_to_be32(rcv_log->size);
+
+	fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, FILEMODE);
+	if (fd < 0) {
+		perror("open");
+		return fd;
+	}
+
+	len = write_exact(fd, &header, sizeof(header));
+	if (len <= 0) {
+		perror("isochron log header write");
+		return len;
+	}
+
+	len = write_exact(fd, send_log->buf, send_log->size);
+	if (len <= 0) {
+		perror("isochron send log write");
+		return len;
+	}
+
+	len = write_exact(fd, rcv_log->buf, rcv_log->size);
+	if (len <= 0) {
+		perror("isochron rcv log write");
+		return len;
+	}
+
+	close(fd);
+
+	return 0;
 }

isochron/Makefile
@@ -5,9 +5,10 @@ bindir ?= ${exec_prefix}/bin
 src := \
 	isochron.o \
 	common.o \
-	rcv.o \
 	log.o \
 	ptpmon.o \
+	rcv.o \
+	report.o \
 	send.o \
 	stats.o \
 	sysmon.o

isochron/isochron.c
@@ -11,6 +11,7 @@ enum isochron_func {
 	ISOCHRON_SEND,
 	ISOCHRON_RCV,
 	ISOCHRON_STATS,
+	ISOCHRON_REPORT,
 };
 
 static void isochron_usage(void)
@@ -19,8 +20,11 @@ static void isochron_usage(void)
 	fprintf(stderr, "isochron send ...\n");
 	fprintf(stderr, "isochron rcv ...\n");
 	fprintf(stderr, "isochron stats ...\n");
+	fprintf(stderr, "isochron report ...\n");
 	fprintf(stderr, "Run \"isochron send --help\" or ");
-	fprintf(stderr, "\"isochron rcv --help\" or \"isochron stats --help\" for more details.\n");
+	fprintf(stderr, "\"isochron rcv --help\" or ");
+	fprintf(stderr, "\"isochron stats --help\" or ");
+	fprintf(stderr, "\"isochron report --help\" for more details.\n");
 }
 
 static int isochron_parse_args(int *argc, char ***argv,
@@ -42,6 +46,12 @@ static int isochron_parse_args(int *argc, char ***argv,
 		return 0;
 	} else if (strcmp(prog_name, "isochron-rcv") == 0) {
 		*func = ISOCHRON_RCV;
+		return 0;
+	} else if (strcmp(prog_name, "isochron-stats") == 0) {
+		*func = ISOCHRON_STATS;
+		return 0;
+	} else if (strcmp(prog_name, "isochron-report") == 0) {
+		*func = ISOCHRON_REPORT;
 		return 0;
 	}
 
@@ -68,9 +78,13 @@ static int isochron_parse_args(int *argc, char ***argv,
 	} else if (strcmp(prog_func, "stats") == 0) {
 		*func = ISOCHRON_STATS;
 		return 0;
+	} else if (strcmp(prog_func, "report") == 0) {
+		*func = ISOCHRON_REPORT;
+		return 0;
 	}
 
-	fprintf(stderr, "%s: unknown function %s, expected send or rcv\n",
+	fprintf(stderr,
+		"%s: unknown function %s, expected \"send\", \"rcv\", \"stats\" or \"report\"\n",
 		prog_name, prog_func);
 
 	return -EINVAL;
@@ -94,6 +108,9 @@ int main(int argc, char *argv[])
 		break;
 	case ISOCHRON_STATS:
 		rc = isochron_stats_main(argc, argv);
+		break;
+	case ISOCHRON_REPORT:
+		rc = isochron_report_main(argc, argv);
 		break;
 	default:
 		isochron_usage();

isochron/isochron.h
@@ -9,5 +9,6 @@
 int isochron_send_main(int argc, char *argv[]);
 int isochron_rcv_main(int argc, char *argv[]);
 int isochron_stats_main(int argc, char *argv[]);
+int isochron_report_main(int argc, char *argv[]);
 
 #endif

isochron/log.h
@@ -53,4 +53,18 @@ void isochron_print_stats(struct isochron_log *send_log,
 
 size_t isochron_log_buf_tlv_size(struct isochron_log *log);
 
+int isochron_log_load(const char *file, struct isochron_log *send_log,
+		      struct isochron_log *rcv_log, long *packet_count,
+		      long *frame_size, bool *omit_sync, bool *do_ts,
+		      bool *taprio, bool *txtime, bool *deadline,
+		      __s64 *base_time, __s64 *advance_time, __s64 *shift_time,
+		      __s64 *cycle_time, __s64 *window_size);
+
+int isochron_log_save(const char *file, const struct isochron_log *send_log,
+		      const struct isochron_log *rcv_log, long packet_count,
+		      long frame_size, bool omit_sync, bool do_ts, bool taprio,
+		      bool txtime, bool deadline, __s64 base_time,
+		      __s64 advance_time, __s64 shift_time, __s64 cycle_time,
+		      __s64 window_size);
+
 #endif

isochron/report.c
@@ -1,0 +1,109 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright 2021 NXP */
+#include <linux/limits.h>
+#include <stdio.h>
+#include <string.h>
+#include "common.h"
+#include "isochron.h"
+#include "log.h"
+
+struct prog_data {
+	struct isochron_log send_log;
+	struct isochron_log rcv_log;
+	long packet_count;
+	long frame_size;
+	bool omit_sync;
+	bool do_ts;
+	bool txtime;
+	bool taprio;
+	bool deadline;
+	__s64 base_time;
+	__s64 advance_time;
+	__s64 shift_time;
+	__s64 cycle_time;
+	__s64 window_size;
+	bool quiet;
+	char input_file[PATH_MAX];
+};
+
+static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
+{
+	bool help = false;
+	struct prog_arg args[] = {
+		{
+			.short_opt = "-h",
+			.long_opt = "--help",
+			.type = PROG_ARG_HELP,
+			.help_ptr = {
+			        .ptr = &help,
+			},
+			.optional = true,
+		}, {
+			.short_opt = "-F",
+			.long_opt = "--input-file",
+			.type = PROG_ARG_STRING,
+			.string = {
+				.buf = prog->input_file,
+				.size = PATH_MAX - 1,
+			},
+		}, {
+			.short_opt = "-q",
+			.long_opt = "--quiet",
+			.type = PROG_ARG_BOOL,
+			.boolean_ptr = {
+			        .ptr = &prog->quiet,
+			},
+			.optional = true,
+		},
+	};
+	int rc;
+
+	rc = prog_parse_np_args(argc, argv, args, ARRAY_SIZE(args));
+
+	/* Non-positional arguments left unconsumed */
+	if (rc < 0) {
+		fprintf(stderr, "Parsing returned %d: %s\n",
+			-rc, strerror(-rc));
+		return rc;
+	} else if (rc < argc) {
+		fprintf(stderr, "%d unconsumed arguments. First: %s\n",
+			argc - rc, argv[rc]);
+		prog_usage("isochron-report", args, ARRAY_SIZE(args));
+		return -1;
+	}
+
+	if (help) {
+		prog_usage("isochron-report", args, ARRAY_SIZE(args));
+		return -1;
+	}
+
+	return 0;
+}
+
+int isochron_report_main(int argc, char *argv[])
+{
+	struct prog_data prog = {0};
+	int rc;
+
+	rc = prog_parse_args(argc, argv, &prog);
+	if (rc)
+		return rc;
+
+	rc = isochron_log_load(prog.input_file, &prog.send_log, &prog.rcv_log,
+			       &prog.packet_count, &prog.frame_size,
+			       &prog.omit_sync, &prog.do_ts, &prog.taprio,
+			       &prog.txtime, &prog.deadline, &prog.base_time,
+			       &prog.advance_time, &prog.shift_time,
+			       &prog.cycle_time, &prog.window_size);
+	if (rc)
+		return rc;
+
+	isochron_print_stats(&prog.send_log, &prog.rcv_log, prog.omit_sync,
+			     prog.quiet, prog.taprio, prog.txtime,
+			     prog.cycle_time, prog.advance_time);
+
+	isochron_log_teardown(&prog.send_log);
+	isochron_log_teardown(&prog.rcv_log);
+
+	return 0;
+}

isochron/send.c
@@ -11,6 +11,7 @@
 #include <time.h>
 #include <linux/errqueue.h>
 #include <linux/if_packet.h>
+#include <linux/limits.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -100,6 +101,7 @@ struct prog_data {
 	long transport_specific;
 	long sync_threshold;
 	long num_readings;
+	char output_file[PATH_MAX];
 };
 
 static int signal_received;
@@ -1409,9 +1411,9 @@ out:
 	return rc;
 }
 
-static void prog_print_packet_log(struct prog_data *prog)
+static int prog_print_packet_log(struct prog_data *prog)
 {
-	int rc;
+	int rc = 0;
 
 	if (prog->stats_srv.family) {
 		char diff_buf[TIMESPEC_BUFSIZ];
@@ -1425,6 +1427,7 @@ static void prog_print_packet_log(struct prog_data *prog)
 		if (rc) {
 			fprintf(stderr, "Failed to collect receiver stats: %s\n",
 				strerror(-rc));
+			return rc;
 		} else {
 			clock_gettime(CLOCK_MONOTONIC, &t1);
 
@@ -1441,12 +1444,31 @@ static void prog_print_packet_log(struct prog_data *prog)
 
 			printf("Processing the stats took %s seconds\n", diff_buf);
 
+			if (strlen(prog->output_file)) {
+				rc = isochron_log_save(prog->output_file,
+						       &prog->log, &rcv_log,
+						       prog->iterations,
+						       prog->tx_len,
+						       prog->omit_sync,
+						       prog->do_ts,
+						       prog->taprio,
+						       prog->txtime,
+						       prog->deadline,
+						       prog->base_time,
+						       prog->advance_time,
+						       prog->shift_time,
+						       prog->cycle_time,
+						       prog->window_size);
+			}
+
 			isochron_log_teardown(&rcv_log);
 		}
 	} else {
 		if (!prog->quiet)
 			isochron_send_log_print(&prog->log);
 	}
+
+	return rc;
 }
 
 static void prog_teardown(struct prog_data *prog)
@@ -1758,6 +1780,15 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 				.ptr = &prog->num_readings,
 			},
 			.optional = true,
+		}, {
+			.short_opt = "-F",
+			.long_opt = "--output-file",
+			.type = PROG_ARG_STRING,
+			.string = {
+				.buf = prog->output_file,
+				.size = PATH_MAX - 1,
+			},
+			.optional = true,
 		},
 	};
 	int rc;
@@ -1845,6 +1876,12 @@ static int prog_parse_args(int argc, char **argv, struct prog_data *prog)
 	if (prog->tx_len > BUF_SIZ) {
 		fprintf(stderr,
 			"Frame size cannot exceed %d octets\n", BUF_SIZ);
+		return -EINVAL;
+	}
+
+	if (strlen(prog->output_file) && !prog->stats_srv.family) {
+		fprintf(stderr,
+			"--client is mandatory when --output-file is used\n");
 		return -EINVAL;
 	}
 
@@ -1944,7 +1981,7 @@ int isochron_send_main(int argc, char *argv[])
 	if (rc < 0)
 		goto out;
 
-	prog_print_packet_log(&prog);
+	rc = prog_print_packet_log(&prog);
 
 out:
 	prog_teardown(&prog);

commit cfb068fb58ace308419ce06964317b5d41a6f519

isochron: fix error message in isochron_log_load()
When read_exact() fails (such as what happens when opening an empty
file), we print an error message that contains the "write" verb instead
of "read", which is confusing. Fix this.

Fixes: d6f8d09b2fce ("isochron: report: save and view logs from the filesystem")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/log.c
@@ -1060,7 +1060,7 @@ int isochron_log_load(const char *file, struct isochron_log *send_log,
 
 	len = read_exact(fd, &header, sizeof(header));
 	if (len <= 0) {
-		perror("isochron log header write");
+		perror("isochron log header read");
 		rc = len;
 		goto out_close;
 	}