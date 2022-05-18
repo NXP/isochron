NXP/isochron

commit 215cb6b33c8a3ebbd084779d5f7c9ef46f501aab

Makefile: properly allow EXTRA_CFLAGS to be specified through command line
Currently our Makefile clobbers the EXTRA_CFLAGS variable, so if we
specify it from the command line like EXTRA_CFLAGS="-g", the output from
the toolchain_deps.sh script would be lost.

Fix that by always appending to the CFLAGS variable, and keeping
EXTRA_CFLAGS only for extra flags coming from the user.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

Makefile
@@ -1,7 +1,7 @@
 VERSION := $(shell ./setlocalversion)
-EXTRA_CFLAGS := $(shell ./toolchain_deps.sh "$(CC)" "$(CFLAGS)")
-CFLAGS := $(CFLAGS) -DVERSION=\"${VERSION}\" $(EXTRA_CFLAGS)
+CFLAGS := -DVERSION=\"${VERSION}\" $(EXTRA_CFLAGS)
 CFLAGS += -Wall -Wextra -Werror -Wno-error=sign-compare
+CFLAGS += $(shell ./toolchain_deps.sh "$(CC)" "$(CFLAGS)")
 CHECK := sparse
 CHECKFLAGS := -D__linux__ -Dlinux -D__STDC__ -Dunix -D__unix__ \
 	      -Wbitwise -Wno-return-void -Wno-unknown-attribute $(CF)

commit 31b47598c80422a07ceb3d39ed9edfc95ca8077a

Makefile: allow CFLAGS to be what is actually supplied by the user
Rename the internally used CFLAGS to MY_CFLAGS and actually inherit what
the user provides. Otherwise, we clobber the CFLAGS supplied by the
build system, and this may lead to issues when proper compilation
depends on that (different include path for cross-compilation sysroot,
etc).

Fixes: 215cb6b33c8a ("Makefile: properly allow EXTRA_CFLAGS to be specified through command line")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

Makefile
@@ -1,7 +1,7 @@
 VERSION := $(shell ./setlocalversion)
-CFLAGS := -DVERSION=\"${VERSION}\" $(EXTRA_CFLAGS)
-CFLAGS += -Wall -Wextra -Werror -Wno-error=sign-compare
-CFLAGS += $(shell ./toolchain_deps.sh "$(CC)" "$(CFLAGS)")
+MY_CFLAGS := -DVERSION=\"${VERSION}\" $(CFLAGS)
+MY_CFLAGS += -Wall -Wextra -Werror -Wno-error=sign-compare
+MY_CFLAGS += $(shell ./toolchain_deps.sh "$(CC)" "$(MY_CFLAGS)")
 CHECK := sparse
 CHECKFLAGS := -D__linux__ -Dlinux -D__STDC__ -Dunix -D__unix__ \
 	      -Wbitwise -Wno-return-void -Wno-unknown-attribute $(CF)

isochron/Makefile
@@ -44,7 +44,7 @@ ifeq (, $(LIBMNL_CFLAGS)$(LIBMNL_LDFLAGS))
 $(error pkg-config could not find libmnl)
 endif
 
-CFLAGS  += $(LIBMNL_CFLAGS)
+MY_CFLAGS += $(LIBMNL_CFLAGS)
 LDFLAGS += $(LIBMNL_LDFLAGS)
 
 TARGET := isochron/isochron
@@ -72,9 +72,9 @@ isochron/isochron: $(objs)
 	$(CC) $^ -o $@ $(LDFLAGS) -lm -pthread
 
 %.o: %.c
-	$(CC) $(CFLAGS) -MMD -c $< -o $@
+	$(CC) $(MY_CFLAGS) -MMD -c $< -o $@
 ifeq ($(C),1)
-	$(CHECK) $(CHECKFLAGS) $(CFLAGS) $<
+	$(CHECK) $(CHECKFLAGS) $(MY_CFLAGS) $<
 endif
 
 clean: