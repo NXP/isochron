NXP/isochron

commit 22f254469d4a0aa8bbdf9c0947ffa0412b5b8cfe

isochron: report: print packets that are missing on RX too
For PSFP selftests where we make a switch deliberately drop out of band
packets, it is useful to debug the dropped packets and see their TX
timestamps, for example.

But we can't, because 'isochron report' skips past missing seqids in the
receiver log.

Fix that by reporting those RX timestamps as zero.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/log.c
@@ -857,6 +857,7 @@ int isochron_print_stats(struct isochron_log *send_log,
 			 __s64 base_time, __s64 advance_time, __s64 shift_time,
 			 __s64 cycle_time, __s64 window_size)
 {
+	struct isochron_rcv_pkt_data dummy_rcv_pkt = {};
 	struct isochron_metric_stats sender_latency_ms;
 	struct isochron_metric_stats wakeup_latency_ms;
 	struct isochron_metric_stats driver_latency_ms;
@@ -890,12 +891,13 @@ int isochron_print_stats(struct isochron_log *send_log,
 			continue;
 		}
 
+		/* For packets that didn't reach the receiver, at least report
+		 * the TX timestamps and seqid for debugging purposes, and use
+		 * a dummy received packet with all RX timestamps set to zero
+		 */
 		rcv_pkt = isochron_rcv_log_find(rcv_log, send_pkt->seqid);
-		if (!rcv_pkt) {
-			if (!quiet)
-				fprintf(stderr, "seqid %u lost\n", seqid);
-			continue;
-		}
+		if (!rcv_pkt)
+			rcv_pkt = &dummy_rcv_pkt;
 
 		isochron_printf_vars_get(send_pkt, rcv_pkt, base_time,
 					 advance_time, shift_time, cycle_time,

commit 7cb2ff68ce6670bbf1031adca4c4c324f4584569

isochron: report: exclude missing packets from statistics calculations
Missing packets throw off the summary output, with the stats dependent
on RX timestamps still being calculated even if those are 0.

Fix that by only taking into consideration the packets that were
received.

Fixes: 22f254469d4a ("isochron: report: print packets that are missing on RX too")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

isochron/log.c
@@ -884,6 +884,7 @@ int isochron_print_stats(struct isochron_log *send_log,
 		struct isochron_send_pkt_data *send_pkt = &pkt_arr[seqid - 1];
 		struct isochron_rcv_pkt_data *rcv_pkt;
 		struct isochron_printf_variables v;
+		bool missing = false;
 
 		if (seqid != __be32_to_cpu(send_pkt->seqid))
 			/* Incomplete log, send_pkt->seqid is 0, exit */
@@ -894,8 +895,10 @@ int isochron_print_stats(struct isochron_log *send_log,
 		 * a dummy received packet with all RX timestamps set to zero
 		 */
 		rcv_pkt = isochron_rcv_log_find(rcv_log, send_pkt->seqid);
-		if (!rcv_pkt)
+		if (!rcv_pkt) {
 			rcv_pkt = &dummy_rcv_pkt;
+			missing = true;
+		}
 
 		isochron_printf_vars_get(send_pkt, rcv_pkt, base_time,
 					 advance_time, shift_time, cycle_time,
@@ -905,12 +908,17 @@ int isochron_print_stats(struct isochron_log *send_log,
 		if (rc)
 			goto out;
 
-		if (summary)
+		if (summary && !missing)
 			isochron_process_stat(&v, &stats, taprio, txtime);
 	}
 
 	if (!summary)
 		return 0;
+
+	if (!stats.frame_count) {
+		printf("Could not calculate statistics, no packets were received\n");
+		return 0;
+	}
 
 	stats.tx_sync_offset_mean /= stats.frame_count;
 	stats.rx_sync_offset_mean /= stats.frame_count;