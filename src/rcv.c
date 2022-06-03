NXP/isochron

commit fb5c44fad600ed5d1b203e28098972075b7f29e7

fix IPv4 data socket getting broken when CONFIG_IPV6=n
Similar to the management sockets introduced in the previous patch, we
have a problem with the receiver not being able to listen even for an
IPv4 address when IPv6 is compiled out of the kernel.

Fix the problem by extending the socket API with connection-less
functions, and also move the timestamping code from common.c there.

Fixes: 563b4d118668 ("rcv: properly support IPv6")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

src/rcv.c
@@ -41,6 +41,8 @@ struct isochron_rcv {
 	struct mnl_socket *rtnl;
 	struct sk *mgmt_listen_sock;
 	struct sk *mgmt_sock;
+	struct sk *l4_sock;
+	struct sk *l2_sock;
 	int l2_data_fd;
 	int l4_data_fd;
 	int data_timeout_fd;
@@ -226,29 +228,18 @@ static int multicast_listen(int fd, unsigned int if_index,
 	return -1;
 }
 
-static int prog_init_l2_data_fd(struct isochron_rcv *prog)
+static int prog_init_l2_sock(struct isochron_rcv *prog)
 {
-	int sockopt = 1;
 	int fd, rc;
 
 	if (!prog->l2)
 		return 0;
 
-	/* Open PF_PACKET socket, listening for the specified EtherType */
-	fd = socket(PF_PACKET, SOCK_RAW, htons(prog->etype));
-	if (fd < 0) {
-		perror("listener: data socket");
-		return -errno;
-	}
+	rc = sk_l2(prog->etype, &prog->l2_sock);
+	if (rc)
+		return rc;
 
-	/* Allow the socket to be reused, in case the connection
-	 * is closed prematurely
-	 */
-	rc = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(int));
-	if (rc < 0) {
-		perror("setsockopt");
-		goto out;
-	}
+	fd = sk_fd(prog->l2_sock);
 
 	/* Bind to device */
 	rc = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, prog->if_name,
@@ -280,13 +271,7 @@ static int prog_init_l2_data_fd(struct isochron_rcv *prog)
 		}
 	}
 
-	rc = sk_validate_ts_info(prog->if_name);
-	if (rc) {
-		errno = -rc;
-		goto out;
-	}
-
-	rc = sk_timestamping_init(fd, prog->if_name, true);
+	rc = sk_timestamping_init(prog->l2_sock, prog->if_name, true);
 	if (rc) {
 		errno = -rc;
 		goto out;
@@ -297,37 +282,22 @@ static int prog_init_l2_data_fd(struct isochron_rcv *prog)
 	return 0;
 
 out:
-	close(fd);
+	sk_close(prog->l2_sock);
 	return -errno;
 }
 
-static int prog_init_l4_data_fd(struct isochron_rcv *prog)
+static int prog_init_l4_sock(struct isochron_rcv *prog)
 {
-	struct sockaddr_in6 serv_data_addr = {
-		.sin6_family = AF_INET6,
-		.sin6_addr = in6addr_any,
-		.sin6_port = htons(prog->data_port),
-	};
-	int sockopt = 1;
 	int fd, rc;
 
 	if (!prog->l4)
 		return 0;
 
-	fd = socket(PF_INET6, SOCK_DGRAM, IPPROTO_UDP);
-	if (fd < 0) {
-		perror("listener: data socket");
-		return -errno;
-	}
+	rc = sk_bind_udp_any(prog->data_port, &prog->l4_sock);
+	if (rc)
+		return rc;
 
-	/* Allow the socket to be reused, in case the connection
-	 * is closed prematurely
-	 */
-	rc = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(int));
-	if (rc < 0) {
-		perror("setsockopt");
-		goto out;
-	}
+	fd = sk_fd(prog->l4_sock);
 
 	/* Bind to device */
 	rc = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, prog->if_name,
@@ -337,20 +307,7 @@ static int prog_init_l4_data_fd(struct isochron_rcv *prog)
 		goto out;
 	}
 
-	rc = bind(fd, (struct sockaddr *)&serv_data_addr,
-		  sizeof(serv_data_addr));
-	if (rc < 0) {
-		perror("bind");
-		goto out;
-	}
-
-	rc = sk_validate_ts_info(prog->if_name);
-	if (rc) {
-		errno = -rc;
-		goto out;
-	}
-
-	rc = sk_timestamping_init(fd, prog->if_name, true);
+	rc = sk_timestamping_init(prog->l4_sock, prog->if_name, true);
 	if (rc) {
 		errno = -rc;
 		goto out;
@@ -365,33 +322,35 @@ out:
 	return -errno;
 }
 
-static void prog_teardown_l2_data_fd(struct isochron_rcv *prog)
+static void prog_teardown_l2_sock(struct isochron_rcv *prog)
 {
-	if (!prog->l2)
+	if (!prog->l2_sock)
 		return;
 
 	if (is_multicast_ether_addr(prog->dest_mac))
 		multicast_listen(prog->l2_data_fd, prog->if_index,
 				 prog->dest_mac, false);
 
-	close(prog->l2_data_fd);
+	sk_close(prog->l2_sock);
+	prog->l2_sock = NULL;
 }
 
-static void prog_teardown_l4_data_fd(struct isochron_rcv *prog)
+static void prog_teardown_l4_sock(struct isochron_rcv *prog)
 {
-	if (!prog->l4)
+	if (!prog->l4_sock)
 		return;
 
-	close(prog->l4_data_fd);
+	sk_close(prog->l4_sock);
+	prog->l4_sock = NULL;
 }
 
-static int prog_data_event(struct isochron_rcv *prog, int fd, bool l2)
+static int prog_data_event(struct isochron_rcv *prog, struct sk *sock, bool l2)
 {
 	struct ethhdr *eth_hdr = (struct ethhdr *)prog->rcvbuf;
 	struct isochron_timestamp tstamp = {0};
 	ssize_t len;
 
-	len = sk_receive(fd, prog->rcvbuf, BUF_SIZ, &tstamp, 0, 0);
+	len = sk_recvmsg(sock, prog->rcvbuf, BUF_SIZ, &tstamp, 0, 0);
 	/* Suppress "Interrupted system call" message */
 	if (len < 0 && errno != EINTR) {
 		perror("recvfrom failed");
@@ -551,9 +510,9 @@ static int prog_update_l2_enabled(void *priv, void *ptr)
 	prog->l2 = f->enabled;
 
 	if (prog->l2)
-		rc = prog_init_l2_data_fd(prog);
+		rc = prog_init_l2_sock(prog);
 	else
-		prog_teardown_l2_data_fd(prog);
+		prog_teardown_l2_sock(prog);
 
 	return rc;
 }
@@ -570,9 +529,9 @@ static int prog_update_l4_enabled(void *priv, void *ptr)
 	prog->l4 = f->enabled;
 
 	if (prog->l4)
-		rc = prog_init_l4_data_fd(prog);
+		rc = prog_init_l4_sock(prog);
 	else
-		prog_teardown_l4_data_fd(prog);
+		prog_teardown_l4_sock(prog);
 
 	return rc;
 }
@@ -731,13 +690,13 @@ static int server_loop(struct isochron_rcv *prog)
 		}
 
 		if (l2_pfd >= 0 && pfd[l2_pfd].revents & (POLLIN | POLLERR | POLLPRI)) {
-			rc = prog_data_event(prog, prog->l2_data_fd, true);
+			rc = prog_data_event(prog, prog->l2_sock, true);
 			if (rc)
 				break;
 		}
 
 		if (l4_pfd >= 0 && pfd[l4_pfd].revents & (POLLIN | POLLERR | POLLPRI)) {
-			rc = prog_data_event(prog, prog->l4_data_fd, false);
+			rc = prog_data_event(prog, prog->l4_sock, false);
 			if (rc)
 				break;
 		}
@@ -938,6 +897,10 @@ static int prog_init(struct isochron_rcv *prog)
 	if (rc)
 		goto out_close_rtnl;
 
+	rc = sk_validate_ts_info(prog->if_name);
+	if (rc)
+		goto out_close_rtnl;
+
 	rc = prog_init_ptpmon(prog);
 	if (rc)
 		goto out_close_rtnl;
@@ -959,24 +922,24 @@ static int prog_init(struct isochron_rcv *prog)
 	if (rc)
 		goto out_teardown_sysmon;
 
-	rc = prog_init_l2_data_fd(prog);
+	rc = prog_init_l2_sock(prog);
 	if (rc)
 		goto out_teardown_mgmt_listen_sock;
 
-	rc = prog_init_l4_data_fd(prog);
+	rc = prog_init_l4_sock(prog);
 	if (rc)
-		goto out_teardown_l2_data_fd;
+		goto out_teardown_l2_sock;
 
 	rc = prog_init_data_timeout_fd(prog);
 	if (rc)
-		goto out_teardown_l4_data_fd;
+		goto out_teardown_l4_sock;
 
 	return 0;
 
-out_teardown_l4_data_fd:
-	prog_teardown_l4_data_fd(prog);
-out_teardown_l2_data_fd:
-	prog_teardown_l2_data_fd(prog);
+out_teardown_l4_sock:
+	prog_teardown_l4_sock(prog);
+out_teardown_l2_sock:
+	prog_teardown_l2_sock(prog);
 out_teardown_mgmt_listen_sock:
 	prog_teardown_mgmt_listen_sock(prog);
 out_teardown_sysmon:
@@ -1191,8 +1154,8 @@ static void prog_teardown(struct isochron_rcv *prog)
 	isochron_log_teardown(&prog->log);
 
 	prog_teardown_data_timeout_fd(prog);
-	prog_teardown_l4_data_fd(prog);
-	prog_teardown_l2_data_fd(prog);
+	prog_teardown_l4_sock(prog);
+	prog_teardown_l2_sock(prog);
 	prog_teardown_mgmt_listen_sock(prog);
 	prog_teardown_sysmon(prog);
 	prog_teardown_ptpmon(prog);

src/send.c
@@ -11,7 +11,6 @@
 #include <inttypes.h>
 #include <time.h>
 #include <linux/errqueue.h>
-#include <linux/if_packet.h>
 #include <linux/limits.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -292,7 +291,7 @@ static int prog_validate_tx_hwts(struct isochron_send *prog,
 	return 0;
 }
 
-/* Propagates the return code from sk_receive, i.e. the number of bytes
+/* Propagates the return code from sk_recvmsg, i.e. the number of bytes
  * read from the socket (if we could successfully read a timestamp),
  * or a negative error code.
  */
@@ -305,8 +304,8 @@ static int prog_poll_txtstamps(struct isochron_send *prog, int timeout)
 	__u64 txtime;
 	int len, rc;
 
-	len = sk_receive(prog->data_fd, err_pkt, BUF_SIZ, &tstamp, MSG_ERRQUEUE,
-			timeout);
+	len = sk_recvmsg(prog->data_sock, err_pkt, BUF_SIZ, &tstamp,
+			 MSG_ERRQUEUE, timeout);
 	if (len <= 0)
 		return len;
 
@@ -386,16 +385,16 @@ static int do_work(struct isochron_send *prog, int iteration, __s64 scheduled,
 	hdr->seqid = __cpu_to_be32(iteration);
 
 	if (prog->txtime)
-		*((__u64 *)CMSG_DATA(prog->cmsg)) = (__u64)(scheduled);
+		*((__u64 *)CMSG_DATA(prog->txtime_cmsg)) = (__u64)(scheduled);
 
 	rc = prog_log_packet_no_tstamp(prog, hdr);
 	if (rc)
 		return rc;
 
 	/* Send packet */
-	rc = sendmsg(prog->data_fd, &prog->msg, 0);
+	rc = sk_sendmsg(prog->data_sock, prog->msg, 0);
 	if (rc < 0)
-		return rc;
+		return -errno;
 
 	trace(prog, "send seqid %d end\n", iteration);
 
@@ -1147,7 +1146,7 @@ void isochron_send_teardown_sysmon(struct isochron_send *prog)
 	sysmon_destroy(prog->sysmon);
 }
 
-int isochron_send_init_data_fd(struct isochron_send *prog)
+int isochron_send_init_data_sock(struct isochron_send *prog)
 {
 	struct ifreq if_idx;
 	struct ifreq if_mac;
@@ -1155,24 +1154,13 @@ int isochron_send_init_data_fd(struct isochron_send *prog)
 
 	/* Open socket to send on */
 	if (prog->l2)
-		fd = socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW);
+		rc = sk_l2(prog->etype, &prog->data_sock);
 	else
-		fd = socket(prog->ip_destination.family, SOCK_DGRAM,
-			    IPPROTO_UDP);
-	if (fd < 0) {
-		perror("Failed to open data socket");
+		rc = sk_udp(&prog->ip_destination, &prog->data_sock);
+	if (rc)
 		goto out;
-	}
 
-	if (prog->l4 && strlen(prog->ip_destination.bound_if_name)) {
-		rc = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE,
-				prog->ip_destination.bound_if_name,
-				IFNAMSIZ - 1);
-		if (rc < 0) {
-			perror("setsockopt(SO_BINDTODEVICE) on data socket failed");
-			goto out_close;
-		}
-	}
+	fd = sk_fd(prog->data_sock);
 
 	rc = setsockopt(fd, SOL_SOCKET, SO_PRIORITY, &prog->priority,
 			sizeof(int));
@@ -1225,43 +1213,48 @@ int isochron_send_init_data_fd(struct isochron_send *prog)
 			goto out_close;
 		}
 
-		rc = sk_timestamping_init(fd, prog->if_name, true);
+		rc = sk_timestamping_init(prog->data_sock, prog->if_name, true);
 		if (rc < 0)
 			goto out_close;
 	}
 
-	if (prog->l2) {
-		/* Index of the network device */
-		prog->sockaddr.l2.sll_ifindex = if_idx.ifr_ifindex;
-		/* Address length */
-		prog->sockaddr.l2.sll_halen = ETH_ALEN;
-		/* Destination MAC */
-		ether_addr_copy(prog->sockaddr.l2.sll_addr, prog->dest_mac);
-		prog->sockaddr_size = sizeof(struct sockaddr_ll);
-	} else if (prog->ip_destination.family == AF_INET) {
-		prog->sockaddr.udp4.sin_addr = prog->ip_destination.addr;
-		prog->sockaddr.udp4.sin_port = htons(prog->data_port);
-		prog->sockaddr.udp4.sin_family = AF_INET;
-		prog->sockaddr_size = sizeof(struct sockaddr_in);
-	} else {
-		prog->sockaddr.udp6.sin6_addr = prog->ip_destination.addr6;
-		prog->sockaddr.udp6.sin6_port = htons(prog->data_port);
-		prog->sockaddr.udp6.sin6_family = AF_INET6;
-		prog->sockaddr_size = sizeof(struct sockaddr_in6);
+	if (prog->l2)
+		prog->sa = sk_addr_create_l2(prog->dest_mac,
+					     if_idx.ifr_ifindex);
+	else
+		prog->sa = sk_addr_create_udp(&prog->ip_destination,
+					      prog->data_port);
+	if (!prog->sa) {
+		errno = -ENOMEM;
+		goto out_close;
 	}
 
-	prog->data_fd = fd;
+	prog->msg = sk_msg_create(prog->sa, prog->sendbuf, prog->tx_len);
+	if (!prog->msg) {
+		errno = -ENOMEM;
+		goto out_free_sa;
+	}
+
+	if (prog->txtime)
+		prog->txtime_cmsg = sk_msg_add_cmsg(prog->msg, SOL_SOCKET,
+						    SCM_TXTIME,
+						    CMSG_LEN(sizeof(__u64)));
+
 	return 0;
 
+out_free_sa:
+	sk_addr_destroy(prog->sa);
 out_close:
-	close(fd);
+	sk_close(prog->data_sock);
 out:
 	return -errno;
 }
 
-void isochron_send_teardown_data_fd(struct isochron_send *prog)
+void isochron_send_teardown_data_sock(struct isochron_send *prog)
 {
-	close(prog->data_fd);
+	sk_addr_destroy(prog->sa);
+	sk_msg_destroy(prog->msg);
+	sk_close(prog->data_sock);
 }
 
 void isochron_send_init_data_packet(struct isochron_send *prog)
@@ -1305,25 +1298,6 @@ void isochron_send_init_data_packet(struct isochron_send *prog)
 		prog->sendbuf[i++] = 0xad;
 		prog->sendbuf[i++] = 0xbe;
 		prog->sendbuf[i++] = 0xef;
-	}
-
-	prog->iov.iov_base = prog->sendbuf;
-	prog->iov.iov_len = prog->tx_len;
-
-	memset(&prog->msg, 0, sizeof(prog->msg));
-	prog->msg.msg_name = (struct sockaddr *)&prog->sockaddr;
-	prog->msg.msg_namelen = prog->sockaddr_size;
-	prog->msg.msg_iov = &prog->iov;
-	prog->msg.msg_iovlen = 1;
-
-	if (prog->txtime) {
-		prog->msg.msg_control = prog->msg_control;
-		prog->msg.msg_controllen = sizeof(prog->msg_control);
-
-		prog->cmsg = CMSG_FIRSTHDR(&prog->msg);
-		prog->cmsg->cmsg_level = SOL_SOCKET;
-		prog->cmsg->cmsg_type = SCM_TXTIME;
-		prog->cmsg->cmsg_len = CMSG_LEN(sizeof(__u64));
 	}
 }
 
@@ -1426,7 +1400,7 @@ int prog_init(struct isochron_send *prog)
 	if (rc)
 		goto out_stats_socket_teardown;
 
-	rc = isochron_send_init_data_fd(prog);
+	rc = isochron_send_init_data_sock(prog);
 	if (rc)
 		goto out_stats_socket_teardown;
 
@@ -1434,7 +1408,7 @@ int prog_init(struct isochron_send *prog)
 
 	rc = prog_init_trace_mark(prog);
 	if (rc)
-		goto out_close_data_fd;
+		goto out_close_data_sock;
 
 	/* Prevent the process's virtual memory from being swapped out, by
 	 * locking all current and future pages
@@ -1474,8 +1448,8 @@ out_munlock:
 	munlockall();
 out_close_trace_mark_fd:
 	prog_teardown_trace_mark(prog);
-out_close_data_fd:
-	isochron_send_teardown_data_fd(prog);
+out_close_data_sock:
+	isochron_send_teardown_data_sock(prog);
 out_stats_socket_teardown:
 	prog_teardown_stats_socket(prog);
 out_close_rtnl:
@@ -1492,7 +1466,7 @@ static void prog_teardown(struct isochron_send *prog)
 	munlockall();
 
 	prog_teardown_trace_mark(prog);
-	isochron_send_teardown_data_fd(prog);
+	isochron_send_teardown_data_sock(prog);
 	prog_teardown_stats_socket(prog);
 	prog_rtnl_close(prog);
 }

commit db9dde62a39a98ecc129ca52cc816c42008cf46b

sk: fix broken L2 TX timestamping
Currently, L2 TX timestamps are not received through the socket error
queue beyond a certain point (200 packets or so).

Seemingly, this is because we should bind(2) to the PF_PACKET socket if
we want to receive the error packets with the timestamping cmsgs.

We never called bind(2) on the L2 socket in the history of the sender
program, but we also passed "protocol" as IPPROTO_RAW to the socket(2)
call. I'm not quite clear how this is interpreted by net/packet/af_packet.c
or if that was even a valid thing to do in the first place, but fact is
that TX timestamping used to work in this unbound socket configuration
that way.

What the blamed commit below changed was it replaced IPPROTO_RAW for the
sender with htons(ethertype), in the attempt to unify the sender with
the receiver socket creation code (for the receiver we did pass the
EtherType as the PF_PACKET protocol).

It's not really the fault of the commit per se, it's more of a lingering
bug that got exposed just now.

This change reshapes the unconnected socket API again. It replaces
sk_l2() with sk_bind_l2() so that the sender actually binds to the
protocol and address, making it able to recvmsg() the timestamps.
Interestingly, UDP data sockets do *not* need to bind(2) to be able to
get the timestamps, in fact binding is not even possible due to the
kernel not being able to assign us the destination IP address and port.
So sk_udp() remains, but sk_bind_udp_any() is now reimplemented as
sk_bind_udp() which is a bound form of sk_udp(), so we have more common
code.

Since sk_bind_l2() needs the L2 address to bind to, and the caller
doesn't really need that in any way, the change also hides the struct
sk_addr inside sk.c and removes it from the API, making struct sk_msg be
attached directly to a socket as far as callers are concerned.

Fixes: fb5c44fad600 ("fix IPv4 data socket getting broken when CONFIG_IPV6=n")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

src/rcv.c
@@ -229,6 +229,33 @@ static int multicast_listen(int fd, unsigned int if_index,
 	return -1;
 }
 
+static int if_get_ether_addr(const char *if_name, unsigned char *addr)
+{
+	struct ifreq if_mac;
+	int fd, rc;
+
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd < 0) {
+		perror("Failed to open socket");
+		return -errno;
+	}
+
+	memset(&if_mac, 0, sizeof(struct ifreq));
+	strcpy(if_mac.ifr_name, if_name);
+
+	rc = ioctl(fd, SIOCGIFHWADDR, &if_mac);
+	close(fd);
+
+	if (rc < 0) {
+		perror("SIOCGIFHWADDR");
+		return -errno;
+	}
+
+	ether_addr_copy(addr, (unsigned char *)if_mac.ifr_hwaddr.sa_data);
+
+	return 0;
+}
+
 static int prog_init_l2_sock(struct isochron_rcv *prog)
 {
 	int fd, rc;
@@ -236,33 +263,18 @@ static int prog_init_l2_sock(struct isochron_rcv *prog)
 	if (!prog->l2)
 		return 0;
 
-	rc = sk_l2(prog->etype, &prog->l2_sock);
+	if (is_zero_ether_addr(prog->dest_mac)) {
+		rc = if_get_ether_addr(prog->if_name, prog->dest_mac);
+		if (rc)
+			return rc;
+	}
+
+	rc = sk_bind_l2(prog->dest_mac, prog->etype, prog->if_name,
+			&prog->l2_sock);
 	if (rc)
 		return rc;
 
 	fd = sk_fd(prog->l2_sock);
-
-	/* Bind to device */
-	rc = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, prog->if_name,
-			IFNAMSIZ - 1);
-	if (rc < 0) {
-		perror("setsockopt(SO_BINDTODEVICE) on data socket failed");
-		goto out;
-	}
-
-	if (is_zero_ether_addr(prog->dest_mac)) {
-		struct ifreq if_mac;
-
-		memset(&if_mac, 0, sizeof(struct ifreq));
-		strcpy(if_mac.ifr_name, prog->if_name);
-		if (ioctl(fd, SIOCGIFHWADDR, &if_mac) < 0) {
-			perror("SIOCGIFHWADDR");
-			goto out;
-		}
-
-		ether_addr_copy(prog->dest_mac,
-			        (unsigned char *)if_mac.ifr_hwaddr.sa_data);
-	}
 
 	if (is_multicast_ether_addr(prog->dest_mac)) {
 		rc = multicast_listen(fd, prog->if_index, prog->dest_mac, true);
@@ -289,12 +301,13 @@ out:
 
 static int prog_init_l4_sock(struct isochron_rcv *prog)
 {
+	struct ip_address any = {};
 	int fd, rc;
 
 	if (!prog->l4)
 		return 0;
 
-	rc = sk_bind_udp_any(prog->data_port, &prog->l4_sock);
+	rc = sk_bind_udp(&any, prog->data_port, &prog->l4_sock);
 	if (rc)
 		return rc;
 

src/send.c
@@ -1154,9 +1154,11 @@ int isochron_send_init_data_sock(struct isochron_send *prog)
 
 	/* Open socket to send on */
 	if (prog->l2)
-		rc = sk_l2(prog->etype, &prog->data_sock);
+		rc = sk_bind_l2(prog->dest_mac, prog->etype, prog->if_name,
+				&prog->data_sock);
 	else
-		rc = sk_udp(&prog->ip_destination, &prog->data_sock);
+		rc = sk_udp(&prog->ip_destination, prog->data_port,
+			    &prog->data_sock);
 	if (rc)
 		goto out;
 
@@ -1218,22 +1220,11 @@ int isochron_send_init_data_sock(struct isochron_send *prog)
 			goto out_close;
 	}
 
-	if (prog->l2)
-		prog->sa = sk_addr_create_l2(prog->dest_mac,
-					     if_idx.ifr_ifindex);
-	else
-		prog->sa = sk_addr_create_udp(&prog->ip_destination,
-					      prog->data_port);
-	if (!prog->sa) {
-		errno = -ENOMEM;
-		goto out_close;
-	}
-
-	prog->msg = sk_msg_create(prog->sa, prog->sendbuf, prog->tx_len,
+	prog->msg = sk_msg_create(prog->data_sock, prog->sendbuf, prog->tx_len,
 				  CMSG_SPACE(sizeof(__s64)));
 	if (!prog->msg) {
 		errno = -ENOMEM;
-		goto out_free_sa;
+		goto out_close;
 	}
 
 	if (prog->txtime)
@@ -1243,8 +1234,6 @@ int isochron_send_init_data_sock(struct isochron_send *prog)
 
 	return 0;
 
-out_free_sa:
-	sk_addr_destroy(prog->sa);
 out_close:
 	sk_close(prog->data_sock);
 out:
@@ -1253,7 +1242,6 @@ out:
 
 void isochron_send_teardown_data_sock(struct isochron_send *prog)
 {
-	sk_addr_destroy(prog->sa);
 	sk_msg_destroy(prog->msg);
 	sk_close(prog->data_sock);
 }

src/sk.c
@@ -19,11 +19,6 @@
 #include "common.h"
 #include "sk.h"
 
-struct sk {
-	int family;
-	int fd;
-};
-
 struct sk_addr {
 	union {
 		struct sockaddr_ll l2;
@@ -38,6 +33,12 @@ struct sk_msg {
 	struct msghdr msghdr;
 	struct cmsghdr *last_cmsg;
 	char *msg_control;
+};
+
+struct sk {
+	int family;
+	int fd;
+	struct sk_addr *sa;
 };
 
 static int __sk_bind_ipv4(int fd, const struct in_addr *a, int port)
@@ -63,27 +64,10 @@ static int sk_bind_ipv4_any(int fd, int port)
 
 static int sk_bind_ipv4(int fd, const struct ip_address *ip, int port)
 {
-	int rc;
-
 	if (ip->family != AF_INET)
 		return sk_bind_ipv4_any(fd, port);
 
-	rc = __sk_bind_ipv4(fd, &ip->addr, port);
-	if (rc)
-		return rc;
-
-	if (strlen(ip->bound_if_name)) {
-		rc = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE,
-				ip->bound_if_name, IFNAMSIZ - 1);
-		if (rc < 0) {
-			fprintf(stderr,
-				"Failed to bind socket to device %s: %m\n",
-				ip->bound_if_name);
-			return rc;
-		}
-	}
-
-	return 0;
+	return __sk_bind_ipv4(fd, &ip->addr, port);
 }
 
 static int __sk_bind_ipv6(int fd, const struct in6_addr *a, int port)
@@ -107,27 +91,10 @@ static int sk_bind_ipv6_any(int fd, int port)
 
 static int sk_bind_ipv6(int fd, const struct ip_address *ip, int port)
 {
-	int rc;
-
 	if (ip->family != AF_INET6)
 		return sk_bind_ipv6_any(fd, port);
 
-	rc = __sk_bind_ipv6(fd, &ip->addr6, port);
-	if (rc)
-		return rc;
-
-	if (strlen(ip->bound_if_name)) {
-		rc = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE,
-				ip->bound_if_name, IFNAMSIZ - 1);
-		if (rc < 0) {
-			fprintf(stderr,
-				"Failed to bind socket to device %s: %m\n",
-				ip->bound_if_name);
-			return -errno;
-		}
-	}
-
-	return 0;
+	return __sk_bind_ipv6(fd, &ip->addr6, port);
 }
 
 int sk_listen_tcp(const struct ip_address *ip, int port, int backlog,
@@ -165,18 +132,29 @@ int sk_listen_tcp(const struct ip_address *ip, int port, int backlog,
 		goto out_close;
 	}
 
+	if (strlen(ip->bound_if_name)) {
+		rc = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE,
+				ip->bound_if_name, IFNAMSIZ - 1);
+		if (rc < 0) {
+			fprintf(stderr,
+				"Failed to bind socket to device %s: %m\n",
+				ip->bound_if_name);
+			goto out_close;
+		}
+	}
+
 	if (ipv4_fallback)
 		rc = sk_bind_ipv4(fd, ip, port);
 	else
 		rc = sk_bind_ipv6(fd, ip, port);
 	if (rc < 0) {
-		fprintf(stderr, "Failed to bind to TCP port %d: %m", port);
+		fprintf(stderr, "Failed to bind to TCP port %d: %m\n", port);
 		goto out_close;
 	}
 
 	rc = listen(fd, backlog);
 	if (rc < 0) {
-		fprintf(stderr, "Failed to listen on TCP port %d: %m", port);
+		fprintf(stderr, "Failed to listen on TCP port %d: %m\n", port);
 		goto out_close;
 	}
 
@@ -334,55 +312,15 @@ err:
 	return -errno;
 }
 
-int sk_bind_udp_any(int port, struct sk **sock)
+static void sk_addr_destroy(struct sk_addr *sa)
 {
-	bool ipv4_fallback = false;
-	int sockopt = 1;
-	int fd, rc;
-
-	*sock = calloc(1, sizeof(struct sk));
-	if (!(*sock))
-		return -ENOMEM;
-
-	fd = socket(PF_INET6, SOCK_DGRAM, 0);
-	if (fd < 0) {
-		fd = socket(PF_INET, SOCK_DGRAM, 0);
-		if (fd < 0) {
-			perror("Failed to create IPv6 or IPv4 socket");
-			free(*sock);
-			return fd;
-		}
-		ipv4_fallback = true;
-	}
-
-	rc = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(int));
-	if (rc < 0) {
-		perror("Failed to setsockopt(SO_REUSEADDR)");
-		goto out;
-	}
-
-	if (ipv4_fallback)
-		rc = sk_bind_ipv4_any(fd, port);
-	else
-		rc = sk_bind_ipv6_any(fd, port);
-	if (rc < 0) {
-		fprintf(stderr, "Failed to bind to UDP port %d: %m", port);
-		goto out;
-	}
-
-	(*sock)->fd = fd;
-	(*sock)->family = ipv4_fallback ? PF_INET : PF_INET6;
-
-	return 0;
-out:
-	close(fd);
-	free(*sock);
-	*sock = NULL;
-	return -errno;
+	free(sa);
 }
 
 void sk_close(struct sk *sock)
 {
+	if (sock->sa)
+		sk_addr_destroy(sock->sa);
 	close(sock->fd);
 	free(sock);
 }
@@ -412,25 +350,32 @@ int sk_fd(const struct sk *sock)
 	return sock->fd;
 }
 
-struct sk_addr *sk_addr_create_l2(const unsigned char addr[ETH_ALEN],
-				  int ifindex)
+static struct sk_addr *sk_addr_create_l2(const unsigned char addr[ETH_ALEN],
+					 __u16 ethertype, const char *if_name)
 {
+	int ifindex = if_nametoindex(if_name);
 	struct sk_addr *sa;
+
+	if (!ifindex) {
+		fprintf(stderr, "Could not determine ifindex of %s\n", if_name);
+		return NULL;
+	}
 
 	sa = calloc(1, sizeof(struct sk_addr));
 	if (!sa)
 		return NULL;
 
+	sa->u.l2.sll_protocol = __cpu_to_be16(ethertype);
 	sa->u.l2.sll_ifindex = ifindex;
 	sa->u.l2.sll_halen = ETH_ALEN;
+	sa->u.l2.sll_family = AF_PACKET;
 	ether_addr_copy(sa->u.l2.sll_addr, addr);
 	sa->sockaddr_size = sizeof(struct sockaddr_ll);
 
 	return sa;
 }
 
-struct sk_addr *sk_addr_create_udp(const struct ip_address *ip,
-				   int port)
+static struct sk_addr *sk_addr_create_udp(const struct ip_address *ip, int port)
 {
 	struct sk_addr *sa;
 
@@ -453,15 +398,14 @@ struct sk_addr *sk_addr_create_udp(const struct ip_address *ip,
 	return sa;
 }
 
-void sk_addr_destroy(struct sk_addr *sa)
-{
-	free(sa);
-}
-
-struct sk_msg *sk_msg_create(const struct sk_addr *sa, void *buf, size_t len,
+struct sk_msg *sk_msg_create(const struct sk *sock, void *buf, size_t len,
 			     size_t cmsg_len)
 {
+	struct sk_addr *sa = sock->sa;
 	struct sk_msg *msg;
+
+	if (!sa)
+		return NULL;
 
 	msg = calloc(1, sizeof(struct sk_msg));
 	if (!msg)
@@ -519,40 +463,86 @@ int sk_sendmsg(struct sk *sock, const struct sk_msg *msg, int flags)
 	return sendmsg(sock->fd, &msg->msghdr, flags);
 }
 
-int sk_l2(__u16 ethertype, struct sk **sock)
+int sk_bind_l2(const unsigned char addr[ETH_ALEN], __u16 ethertype,
+	       const char *if_name, struct sk **sock)
 {
-	int fd;
-
-	*sock = calloc(1, sizeof(struct sk));
-	if (!(*sock))
-		return -ENOMEM;
-
-	fd = socket(PF_PACKET, SOCK_RAW, htons(ethertype));
-	if (fd < 0) {
-		perror("Failed to create PF_PACKET socket");
-		free(*sock);
-		*sock = NULL;
-		return -errno;
-	}
-
-	(*sock)->fd = fd;
-	(*sock)->family = PF_PACKET;
-
-	return 0;
-}
-
-int sk_udp(const struct ip_address *dest, struct sk **sock)
-{
+	struct sk_addr *sa;
 	int fd, rc;
 
 	*sock = calloc(1, sizeof(struct sk));
 	if (!(*sock))
 		return -ENOMEM;
 
-	fd = socket(dest->family, SOCK_DGRAM, IPPROTO_UDP);
+	sa = sk_addr_create_l2(addr, ethertype, if_name);
+	if (!sa)
+		goto out_free_sock;
+
+	fd = socket(PF_PACKET, SOCK_RAW, htons(ethertype));
 	if (fd < 0) {
-		perror("Failed to create UDP socket");
-		goto out;
+		perror("Failed to create PF_PACKET socket");
+		goto out_free_sa;
+	}
+
+	/* Bind to device */
+	rc = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, if_name, IFNAMSIZ - 1);
+	if (rc < 0) {
+		fprintf(stderr, "Failed to bind L2 socket to device %s: %m",
+			if_name);
+		goto out_close;
+	}
+
+	rc = bind(fd, (struct sockaddr *)&sa->u.l2, sa->sockaddr_size);
+	if (rc)
+		goto out_close;
+
+	(*sock)->fd = fd;
+	(*sock)->family = PF_PACKET;
+	(*sock)->sa = sa;
+
+	return 0;
+
+out_close:
+	close(fd);
+out_free_sa:
+	free(sa);
+out_free_sock:
+	free(*sock);
+	*sock = NULL;
+	return -errno;
+}
+
+int sk_udp(const struct ip_address *dest, int port, struct sk **sock)
+{
+	bool ipv4_fallback = false;
+	struct sk_addr *sa;
+	int fd, rc;
+
+	*sock = calloc(1, sizeof(struct sk));
+	if (!(*sock))
+		return -ENOMEM;
+
+	sa = sk_addr_create_udp(dest, port);
+	if (!sa) {
+		errno = -ENOMEM;
+		goto out_free_sock;
+	}
+
+	if (dest->family) {
+		fd = socket(dest->family, SOCK_DGRAM, IPPROTO_UDP);
+		if (fd < 0) {
+			perror("Failed to create UDP socket");
+			goto out_free_sa;
+		}
+	} else {
+		fd = socket(PF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+		if (fd < 0) {
+			fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+			if (fd < 0) {
+				perror("Failed to create IPv6 or IPv4 UDP socket");
+				goto out_free_sa;
+			}
+			ipv4_fallback = true;
+		}
 	}
 
 	if (strlen(dest->bound_if_name)) {
@@ -560,22 +550,46 @@ int sk_udp(const struct ip_address *dest, struct sk **sock)
 				dest->bound_if_name, IFNAMSIZ - 1);
 		if (rc < 0) {
 			fprintf(stderr,
-				"Failed to bind UDP socket to device %s: %m\n",
+				"Failed to bind socket to device %s: %m\n",
 				dest->bound_if_name);
 			goto out_close;
 		}
 	}
 
 	(*sock)->fd = fd;
-	(*sock)->family = dest->family;
+	(*sock)->family = dest->family ? : ipv4_fallback ? PF_INET : PF_INET6;
+	(*sock)->sa = sa;
 
 	return 0;
 out_close:
 	close(fd);
-out:
+out_free_sa:
+	free(sa);
+out_free_sock:
 	free(*sock);
 	*sock = NULL;
 	return -errno;
+}
+
+int sk_bind_udp(const struct ip_address *dest, int port, struct sk **sock)
+{
+	int rc;
+
+	rc = sk_udp(dest, port, sock);
+	if (rc)
+		return rc;
+
+	if ((*sock)->family == AF_INET)
+		rc = sk_bind_ipv4((*sock)->fd, dest, port);
+	else
+		rc = sk_bind_ipv6((*sock)->fd, dest, port);
+	if (rc) {
+		fprintf(stderr, "Failed to bind to UDP port %d: %m\n", port);
+		sk_close(*sock);
+		*sock = NULL;
+	}
+
+	return rc;
 }
 
 static void init_ifreq(struct ifreq *ifreq, struct hwtstamp_config *cfg,

src/sk.h
@@ -16,7 +16,6 @@ struct isochron_timestamp {
 };
 
 struct sk;
-struct sk_addr;
 struct sk_msg;
 
 /* Connection-oriented */
@@ -28,15 +27,11 @@ ssize_t sk_recv(const struct sk *sock, void *buf, size_t len, int flags);
 ssize_t sk_send(const struct sk *sock, const void *buf, size_t count);
 
 /* Connection-less */
-int sk_bind_udp_any(int port, struct sk **sock);
-int sk_l2(__u16 ethertype, struct sk **sock);
-int sk_udp(const struct ip_address *dest, struct sk **sock);
-struct sk_addr *sk_addr_create_l2(const unsigned char addr[ETH_ALEN],
-				  int ifindex);
-struct sk_addr *sk_addr_create_udp(const struct ip_address *ip,
-				   int port);
-void sk_addr_destroy(struct sk_addr *sa);
-struct sk_msg *sk_msg_create(const struct sk_addr *sa, void *buf, size_t len,
+int sk_udp(const struct ip_address *dest, int port, struct sk **sock);
+int sk_bind_udp(const struct ip_address *dest, int port, struct sk **sock);
+int sk_bind_l2(const unsigned char addr[ETH_ALEN], __u16 ethertype,
+	       const char *if_name, struct sk **sock);
+struct sk_msg *sk_msg_create(const struct sk *sock, void *buf, size_t len,
 			     size_t cmsg_len);
 void sk_msg_destroy(struct sk_msg *msg);
 struct cmsghdr *sk_msg_add_cmsg(struct sk_msg *msg, int level, int type,