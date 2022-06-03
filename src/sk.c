NXP/isochron

commit fb5c44fad600ed5d1b203e28098972075b7f29e7

fix IPv4 data socket getting broken when CONFIG_IPV6=n
Similar to the management sockets introduced in the previous patch, we
have a problem with the receiver not being able to listen even for an
IPv4 address when IPv6 is compiled out of the kernel.

Fix the problem by extending the socket API with connection-less
functions, and also move the timestamping code from common.c there.

Fixes: 563b4d118668 ("rcv: properly support IPv6")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

src/sk.c
@@ -2,9 +2,16 @@
 /* Copyright 2022 NXP */
 #include <arpa/inet.h>
 #include <errno.h>
+#include <linux/errqueue.h>
+#include <linux/ethtool.h>
+#include <linux/if_packet.h>
+#include <linux/net_tstamp.h>
+#include <linux/sockios.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <sys/poll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
@@ -15,6 +22,21 @@
 struct sk {
 	int family;
 	int fd;
+};
+
+struct sk_addr {
+	union {
+		struct sockaddr_ll l2;
+		struct sockaddr_in udp4;
+		struct sockaddr_in6 udp6;
+	} u;
+	size_t sockaddr_size;
+};
+
+struct sk_msg {
+	struct iovec iov;
+	struct msghdr msghdr;
+	char msg_control[CMSG_SPACE(sizeof(__s64))];
 };
 
 static int sk_bind_ipv4_any(int fd, int port)
@@ -237,6 +259,52 @@ err:
 	return -errno;
 }
 
+int sk_bind_udp_any(int port, struct sk **sock)
+{
+	bool ipv4_fallback = false;
+	int sockopt = 1;
+	int fd, rc;
+
+	*sock = calloc(1, sizeof(struct sk));
+	if (!(*sock))
+		return -ENOMEM;
+
+	fd = socket(PF_INET6, SOCK_DGRAM, 0);
+	if (fd < 0) {
+		fd = socket(PF_INET, SOCK_DGRAM, 0);
+		if (fd < 0) {
+			perror("Failed to create IPv6 or IPv4 socket");
+			free(*sock);
+			return fd;
+		}
+		ipv4_fallback = true;
+	}
+
+	rc = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(int));
+	if (rc < 0) {
+		perror("Failed to setsockopt(SO_REUSEADDR)");
+		goto out;
+	}
+
+	if (ipv4_fallback)
+		rc = sk_bind_ipv4_any(fd, port);
+	else
+		rc = sk_bind_ipv6_any(fd, port);
+	if (rc < 0) {
+		fprintf(stderr, "Failed to bind to UDP port %d: %m", port);
+		goto out;
+	}
+
+	(*sock)->fd = fd;
+	(*sock)->family = ipv4_fallback ? PF_INET : PF_INET6;
+
+	return 0;
+out:
+	close(fd);
+	free(*sock);
+	return -errno;
+}
+
 void sk_close(struct sk *sock)
 {
 	close(sock->fd);
@@ -266,4 +334,446 @@ ssize_t sk_send(const struct sk *sock, const void *buf, size_t count)
 int sk_fd(const struct sk *sock)
 {
 	return sock->fd;
+}
+
+struct sk_addr *sk_addr_create_l2(const unsigned char addr[ETH_ALEN],
+				  int ifindex)
+{
+	struct sk_addr *sa;
+
+	sa = calloc(1, sizeof(struct sk_addr));
+	if (!sa)
+		return NULL;
+
+	sa->u.l2.sll_ifindex = ifindex;
+	sa->u.l2.sll_halen = ETH_ALEN;
+	ether_addr_copy(sa->u.l2.sll_addr, addr);
+	sa->sockaddr_size = sizeof(struct sockaddr_ll);
+
+	return sa;
+}
+
+struct sk_addr *sk_addr_create_udp(const struct ip_address *ip,
+				   int port)
+{
+	struct sk_addr *sa;
+
+	sa = calloc(1, sizeof(struct sk_addr));
+	if (!sa)
+		return NULL;
+
+	if (ip->family == AF_INET) {
+		sa->u.udp4.sin_addr = ip->addr;
+		sa->u.udp4.sin_port = htons(port);
+		sa->u.udp4.sin_family = AF_INET;
+		sa->sockaddr_size = sizeof(struct sockaddr_in);
+	} else {
+		sa->u.udp6.sin6_addr = ip->addr6;
+		sa->u.udp6.sin6_port = htons(port);
+		sa->u.udp6.sin6_family = AF_INET6;
+		sa->sockaddr_size = sizeof(struct sockaddr_in6);
+	}
+
+	return sa;
+}
+
+void sk_addr_destroy(struct sk_addr *sa)
+{
+	free(sa);
+}
+
+struct sk_msg *sk_msg_create(const struct sk_addr *sa, void *buf, size_t len)
+{
+	struct sk_msg *msg;
+
+	msg = calloc(1, sizeof(struct sk_msg));
+	if (!msg)
+		return NULL;
+
+	msg->iov.iov_base = buf;
+	msg->iov.iov_len = len;
+
+	msg->msghdr.msg_name = (struct sockaddr *)&sa->u;
+	msg->msghdr.msg_namelen = sa->sockaddr_size;
+	msg->msghdr.msg_iov = &msg->iov;
+	msg->msghdr.msg_iovlen = 1;
+
+	return msg;
+}
+
+void sk_msg_destroy(struct sk_msg *msg)
+{
+	free(msg);
+}
+
+struct cmsghdr *sk_msg_add_cmsg(struct sk_msg *msg, int level, int type,
+				size_t len)
+{
+	struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg->msghdr);
+
+	msg->msghdr.msg_control = msg->msg_control;
+	msg->msghdr.msg_controllen = sizeof(msg->msg_control);
+
+	cmsg->cmsg_level = level;
+	cmsg->cmsg_type = type;
+	cmsg->cmsg_level = len;
+
+	return cmsg;
+}
+
+int sk_sendmsg(struct sk *sock, const struct sk_msg *msg, int flags)
+{
+	return sendmsg(sock->fd, &msg->msghdr, flags);
+}
+
+int sk_l2(__u16 ethertype, struct sk **sock)
+{
+	int fd;
+
+	*sock = calloc(1, sizeof(struct sk));
+	if (!(*sock))
+		return -ENOMEM;
+
+	fd = socket(PF_PACKET, SOCK_RAW, htons(ethertype));
+	if (fd < 0) {
+		perror("Failed to create PF_PACKET socket");
+		free(*sock);
+		return -errno;
+	}
+
+	(*sock)->fd = fd;
+	(*sock)->family = PF_PACKET;
+
+	return 0;
+}
+
+int sk_udp(const struct ip_address *dest, struct sk **sock)
+{
+	int fd, rc;
+
+	*sock = calloc(1, sizeof(struct sk));
+	if (!(*sock))
+		return -ENOMEM;
+
+	fd = socket(dest->family, SOCK_DGRAM, IPPROTO_UDP);
+	if (fd < 0) {
+		perror("Failed to create UDP socket");
+		goto out;
+	}
+
+	if (strlen(dest->bound_if_name)) {
+		rc = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE,
+				dest->bound_if_name, IFNAMSIZ - 1);
+		if (rc < 0) {
+			perror("Failed to setsockopt(SO_BINDTODEVICE) on UDP socket");
+			goto out_close;
+		}
+	}
+
+	(*sock)->fd = fd;
+	(*sock)->family = dest->family;
+
+	return 0;
+out_close:
+	close(fd);
+out:
+	free(*sock);
+	return -errno;
+}
+
+static void init_ifreq(struct ifreq *ifreq, struct hwtstamp_config *cfg,
+		       const char *if_name)
+{
+	memset(ifreq, 0, sizeof(*ifreq));
+	memset(cfg, 0, sizeof(*cfg));
+
+	strcpy(ifreq->ifr_name, if_name);
+
+	ifreq->ifr_data = (void *) cfg;
+}
+
+static int hwts_init(int fd, const char *if_name, int rx_filter, int tx_type)
+{
+	struct hwtstamp_config cfg;
+	struct ifreq ifreq;
+	int rc;
+
+	init_ifreq(&ifreq, &cfg, if_name);
+
+	cfg.tx_type   = tx_type;
+	cfg.rx_filter = rx_filter;
+	rc = ioctl(fd, SIOCSHWTSTAMP, &ifreq);
+	if (rc < 0) {
+		perror("ioctl SIOCSHWTSTAMP failed");
+		return -errno;
+	}
+
+	if (cfg.tx_type != tx_type)
+		fprintf(stderr, "tx_type   %d not %d\n",
+			cfg.tx_type, tx_type);
+	if (cfg.rx_filter != rx_filter)
+		fprintf(stderr, "rx_filter %d not %d\n",
+			cfg.rx_filter, rx_filter);
+	if (cfg.tx_type != tx_type || cfg.rx_filter != rx_filter)
+		fprintf(stderr,
+			"The current filter does not match the required\n");
+
+	return 0;
+}
+
+int sk_timestamping_init(struct sk *sock, const char *if_name, bool on)
+{
+	int rc, filter, flags, tx_type;
+	int fd = sock->fd;
+
+	flags = SOF_TIMESTAMPING_TX_HARDWARE |
+		SOF_TIMESTAMPING_RX_HARDWARE |
+		SOF_TIMESTAMPING_TX_SOFTWARE |
+		SOF_TIMESTAMPING_RX_SOFTWARE |
+		SOF_TIMESTAMPING_TX_SCHED |
+		SOF_TIMESTAMPING_SOFTWARE |
+		SOF_TIMESTAMPING_RAW_HARDWARE |
+		SOF_TIMESTAMPING_OPT_TX_SWHW |
+		SOF_TIMESTAMPING_OPT_ID;
+
+	filter = HWTSTAMP_FILTER_ALL;
+
+	if (on)
+		tx_type = HWTSTAMP_TX_ON;
+	else
+		tx_type = HWTSTAMP_TX_OFF;
+
+	rc = hwts_init(fd, if_name, filter, tx_type);
+	if (rc)
+		return rc;
+
+	rc = setsockopt(fd, SOL_SOCKET, SO_TIMESTAMPING,
+			&flags, sizeof(flags));
+	if (rc < 0) {
+		perror("ioctl SO_TIMESTAMPING failed");
+		return -1;
+	}
+
+	flags = 1;
+	rc = setsockopt(fd, SOL_SOCKET, SO_SELECT_ERR_QUEUE,
+			&flags, sizeof(flags));
+	if (rc < 0) {
+		perror("SO_SELECT_ERR_QUEUE failed");
+		return rc;
+	}
+
+	return 0;
+}
+
+int sk_recvmsg(struct sk *sock, void *buf, int buflen,
+	       struct isochron_timestamp *tstamp, int flags, int timeout)
+{
+	struct iovec iov = { buf, buflen };
+	struct timespec *ts;
+	struct cmsghdr *cm;
+	struct msghdr msg;
+	char control[256];
+	int fd = sock->fd;
+	ssize_t len;
+	int rc = 0;
+
+	memset(control, 0, sizeof(control));
+	memset(&msg, 0, sizeof(msg));
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = control;
+	msg.msg_controllen = sizeof(control);
+
+	if (flags == MSG_ERRQUEUE) {
+		struct pollfd pfd = { fd, POLLPRI, 0 };
+
+		rc = poll(&pfd, 1, timeout);
+		if (rc == 0) {
+			return 0;
+		} else if (rc < 0) {
+			perror("poll for tx timestamp failed");
+			return rc;
+		} else if (!(pfd.revents & POLLPRI)) {
+			fprintf(stderr, "poll woke up on non ERR event\n");
+			return -1;
+		}
+		/* On success a positive number is returned */
+	}
+
+	len = recvmsg(fd, &msg, flags);
+	/* Suppress "Interrupted system call" message */
+	if (len < 1 && errno != EINTR)
+		perror("recvmsg failed");
+
+	for (cm = CMSG_FIRSTHDR(&msg); cm != NULL; cm = CMSG_NXTHDR(&msg, cm)) {
+		int level = cm->cmsg_level;
+		int type  = cm->cmsg_type;
+
+		if (level == SOL_SOCKET && type == SCM_TIMESTAMPING) {
+			struct scm_timestamping *tss;
+
+			if (cm->cmsg_len < sizeof(*ts) * 3) {
+				fprintf(stderr, "short SO_TIMESTAMPING message\n");
+				return -1;
+			}
+
+			tss = (struct scm_timestamping *)CMSG_DATA(cm);
+
+			if (tstamp) {
+				tstamp->sw = tss->ts[0];
+				tstamp->hw = tss->ts[2];
+			}
+		} else if ((level == SOL_PACKET && type == PACKET_TX_TIMESTAMP) ||
+			   (level == SOL_IP && type == IP_RECVERR) ||
+			   (level == SOL_IPV6 && type == IPV6_RECVERR)) {
+			struct sock_extended_err *sock_err;
+			char txtime_buf[TIMESPEC_BUFSIZ];
+			__u64 txtime;
+
+			sock_err = (struct sock_extended_err *)CMSG_DATA(cm);
+			if (!sock_err)
+				continue;
+
+			switch (sock_err->ee_origin) {
+			case SO_EE_ORIGIN_TIMESTAMPING:
+				if (!tstamp)
+					break;
+
+				tstamp->tskey = sock_err->ee_data;
+				tstamp->tstype = sock_err->ee_info;
+				break;
+			case SO_EE_ORIGIN_TXTIME:
+				txtime = ((__u64)sock_err->ee_data << 32) +
+					 sock_err->ee_info;
+
+				if (tstamp)
+					tstamp->txtime = ns_to_timespec(txtime);
+
+				ns_sprintf(txtime_buf, txtime);
+
+				switch (sock_err->ee_code) {
+				case SO_EE_CODE_TXTIME_INVALID_PARAM:
+					fprintf(stderr,
+						"packet with txtime %s dropped due to invalid params\n",
+						txtime_buf);
+					break;
+				case SO_EE_CODE_TXTIME_MISSED:
+					fprintf(stderr,
+						"packet with txtime %s dropped due to missed deadline\n",
+						txtime_buf);
+					break;
+				default:
+					return -1;
+				}
+				break;
+			default:
+				pr_err(-sock_err->ee_errno,
+				       "unknown socket error %d, origin %d code %d: %m\n",
+				       sock_err->ee_errno, sock_err->ee_origin,
+				       sock_err->ee_code);
+				break;
+			}
+		} else {
+			fprintf(stderr, "unknown cmsg level %d type %d\n",
+				level, type);
+		}
+	}
+
+	return len;
+}
+
+/**
+ * Contains timestamping information returned by the GET_TS_INFO ioctl.
+ * @valid:            set to non-zero when the info struct contains valid data.
+ * @phc_index:        index of the PHC device.
+ * @so_timestamping:  supported time stamping modes.
+ * @tx_types:         driver level transmit options for the HWTSTAMP ioctl.
+ * @rx_filters:       driver level receive options for the HWTSTAMP ioctl.
+ */
+struct sk_ts_info {
+	int valid;
+	int phc_index;
+	unsigned int so_timestamping;
+	unsigned int tx_types;
+	unsigned int rx_filters;
+};
+
+static int sk_get_ts_info(const char *name, struct sk_ts_info *sk_info)
+{
+	struct ethtool_ts_info info;
+	struct ifreq ifr;
+	int fd, err;
+
+	memset(sk_info, 0, sizeof(struct sk_ts_info));
+
+	memset(&ifr, 0, sizeof(ifr));
+	memset(&info, 0, sizeof(info));
+	info.cmd = ETHTOOL_GET_TS_INFO;
+	strcpy(ifr.ifr_name, name);
+	ifr.ifr_data = (char *) &info;
+
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd < 0) {
+		fprintf(stderr, "socket failed: %m\n");
+		return -errno;
+	}
+
+	err = ioctl(fd, SIOCETHTOOL, &ifr);
+	if (err < 0) {
+		fprintf(stderr, "ioctl SIOCETHTOOL failed: %m\n");
+		close(fd);
+		return -errno;
+	}
+
+	close(fd);
+
+	/* copy the necessary data to sk_info */
+	sk_info->valid = 1;
+	sk_info->phc_index = info.phc_index;
+	sk_info->so_timestamping = info.so_timestamping;
+	sk_info->tx_types = info.tx_types;
+	sk_info->rx_filters = info.rx_filters;
+
+	return 0;
+}
+
+int sk_validate_ts_info(const char *if_name)
+{
+	struct sk_ts_info ts_info;
+	int rc;
+
+	/* check if device is a valid ethernet device */
+	rc = sk_get_ts_info(if_name, &ts_info);
+	if (rc)
+		return rc;
+
+	if (!ts_info.valid)
+		return -EINVAL;
+
+	if (!(ts_info.so_timestamping & SOF_TIMESTAMPING_TX_HARDWARE)) {
+		fprintf(stderr,
+			"Driver not capable of SOF_TIMESTAMPING_TX_HARDWARE, continuing anyway\n");
+	}
+
+	if (!(ts_info.so_timestamping & SOF_TIMESTAMPING_RX_HARDWARE)) {
+		fprintf(stderr,
+			"Driver not capable of SOF_TIMESTAMPING_RX_HARDWARE, continuing anyway\n");
+	}
+
+	if (!(ts_info.so_timestamping & SOF_TIMESTAMPING_TX_SOFTWARE)) {
+		fprintf(stderr,
+			"Driver not capable of SOF_TIMESTAMPING_TX_SOFTWARE, continuing anyway\n");
+	}
+
+	if (!(ts_info.so_timestamping & SOF_TIMESTAMPING_RX_SOFTWARE)) {
+		fprintf(stderr,
+			"Driver not capable of SOF_TIMESTAMPING_RX_SOFTWARE, continuing anyway\n");
+	}
+
+	if (!(ts_info.so_timestamping & SOF_TIMESTAMPING_SOFTWARE)) {
+		fprintf(stderr,
+			"Driver not capable of SOF_TIMESTAMPING_SOFTWARE, continuing anyway\n");
+	}
+
+	return 0;
 }

src/common.c
@@ -5,16 +5,12 @@
  * - The linuxptp project
  */
 #include <time.h>
-#include <linux/ethtool.h>
-#include <linux/net_tstamp.h>
 #include <netinet/ether.h>
 #include <linux/sockios.h>
-#include <linux/errqueue.h>
 #include <sys/syscall.h>
 #include <sys/ioctl.h>
 #include <arpa/inet.h>
 #include <linux/if.h>
-#include <sys/poll.h>
 #include <sys/stat.h>
 #include <sys/timex.h>
 #include <sys/types.h>
@@ -94,301 +90,6 @@ void ns_sprintf(char *buf, __s64 ns)
 	struct timespec ts = ns_to_timespec(ns);
 
 	snprintf(buf, TIMESPEC_BUFSIZ, "%ld.%09ld", ts.tv_sec, ts.tv_nsec);
-}
-
-static void init_ifreq(struct ifreq *ifreq, struct hwtstamp_config *cfg,
-		       const char *if_name)
-{
-	memset(ifreq, 0, sizeof(*ifreq));
-	memset(cfg, 0, sizeof(*cfg));
-
-	strcpy(ifreq->ifr_name, if_name);
-
-	ifreq->ifr_data = (void *) cfg;
-}
-
-/**
- * Contains timestamping information returned by the GET_TS_INFO ioctl.
- * @valid:            set to non-zero when the info struct contains valid data.
- * @phc_index:        index of the PHC device.
- * @so_timestamping:  supported time stamping modes.
- * @tx_types:         driver level transmit options for the HWTSTAMP ioctl.
- * @rx_filters:       driver level receive options for the HWTSTAMP ioctl.
- */
-struct sk_ts_info {
-	int valid;
-	int phc_index;
-	unsigned int so_timestamping;
-	unsigned int tx_types;
-	unsigned int rx_filters;
-};
-
-static int sk_get_ts_info(const char *name, struct sk_ts_info *sk_info)
-{
-	struct ethtool_ts_info info;
-	struct ifreq ifr;
-	int fd, err;
-
-	memset(sk_info, 0, sizeof(struct sk_ts_info));
-
-	memset(&ifr, 0, sizeof(ifr));
-	memset(&info, 0, sizeof(info));
-	info.cmd = ETHTOOL_GET_TS_INFO;
-	strcpy(ifr.ifr_name, name);
-	ifr.ifr_data = (char *) &info;
-
-	fd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (fd < 0) {
-		fprintf(stderr, "socket failed: %m\n");
-		return -errno;
-	}
-
-	err = ioctl(fd, SIOCETHTOOL, &ifr);
-	if (err < 0) {
-		fprintf(stderr, "ioctl SIOCETHTOOL failed: %m\n");
-		close(fd);
-		return -errno;
-	}
-
-	close(fd);
-
-	/* copy the necessary data to sk_info */
-	sk_info->valid = 1;
-	sk_info->phc_index = info.phc_index;
-	sk_info->so_timestamping = info.so_timestamping;
-	sk_info->tx_types = info.tx_types;
-	sk_info->rx_filters = info.rx_filters;
-
-	return 0;
-}
-
-int sk_validate_ts_info(const char *if_name)
-{
-	struct sk_ts_info ts_info;
-	int rc;
-
-	/* check if device is a valid ethernet device */
-	rc = sk_get_ts_info(if_name, &ts_info);
-	if (rc)
-		return rc;
-
-	if (!ts_info.valid)
-		return -EINVAL;
-
-	if (!(ts_info.so_timestamping & SOF_TIMESTAMPING_TX_HARDWARE)) {
-		fprintf(stderr,
-			"Driver not capable of SOF_TIMESTAMPING_TX_HARDWARE, continuing anyway\n");
-	}
-
-	if (!(ts_info.so_timestamping & SOF_TIMESTAMPING_RX_HARDWARE)) {
-		fprintf(stderr,
-			"Driver not capable of SOF_TIMESTAMPING_RX_HARDWARE, continuing anyway\n");
-	}
-
-	if (!(ts_info.so_timestamping & SOF_TIMESTAMPING_TX_SOFTWARE)) {
-		fprintf(stderr,
-			"Driver not capable of SOF_TIMESTAMPING_TX_SOFTWARE, continuing anyway\n");
-	}
-
-	if (!(ts_info.so_timestamping & SOF_TIMESTAMPING_RX_SOFTWARE)) {
-		fprintf(stderr,
-			"Driver not capable of SOF_TIMESTAMPING_RX_SOFTWARE, continuing anyway\n");
-	}
-
-	if (!(ts_info.so_timestamping & SOF_TIMESTAMPING_SOFTWARE)) {
-		fprintf(stderr,
-			"Driver not capable of SOF_TIMESTAMPING_SOFTWARE, continuing anyway\n");
-	}
-
-	return 0;
-}
-
-static int hwts_init(int fd, const char *if_name, int rx_filter, int tx_type)
-{
-	struct hwtstamp_config cfg;
-	struct ifreq ifreq;
-	int rc;
-
-	init_ifreq(&ifreq, &cfg, if_name);
-
-	cfg.tx_type   = tx_type;
-	cfg.rx_filter = rx_filter;
-	rc = ioctl(fd, SIOCSHWTSTAMP, &ifreq);
-	if (rc < 0) {
-		perror("ioctl SIOCSHWTSTAMP failed");
-		return -errno;
-	}
-
-	if (cfg.tx_type != tx_type)
-		fprintf(stderr, "tx_type   %d not %d\n",
-			cfg.tx_type, tx_type);
-	if (cfg.rx_filter != rx_filter)
-		fprintf(stderr, "rx_filter %d not %d\n",
-			cfg.rx_filter, rx_filter);
-	if (cfg.tx_type != tx_type || cfg.rx_filter != rx_filter)
-		fprintf(stderr,
-			"The current filter does not match the required\n");
-
-	return 0;
-}
-
-int sk_timestamping_init(int fd, const char *if_name, bool on)
-{
-	int rc, filter, flags, tx_type;
-
-	flags = SOF_TIMESTAMPING_TX_HARDWARE |
-		SOF_TIMESTAMPING_RX_HARDWARE |
-		SOF_TIMESTAMPING_TX_SOFTWARE |
-		SOF_TIMESTAMPING_RX_SOFTWARE |
-		SOF_TIMESTAMPING_TX_SCHED |
-		SOF_TIMESTAMPING_SOFTWARE |
-		SOF_TIMESTAMPING_RAW_HARDWARE |
-		SOF_TIMESTAMPING_OPT_TX_SWHW |
-		SOF_TIMESTAMPING_OPT_ID;
-
-	filter = HWTSTAMP_FILTER_ALL;
-
-	if (on)
-		tx_type = HWTSTAMP_TX_ON;
-	else
-		tx_type = HWTSTAMP_TX_OFF;
-
-	rc = hwts_init(fd, if_name, filter, tx_type);
-	if (rc)
-		return rc;
-
-	rc = setsockopt(fd, SOL_SOCKET, SO_TIMESTAMPING,
-			&flags, sizeof(flags));
-	if (rc < 0) {
-		perror("ioctl SO_TIMESTAMPING failed");
-		return -1;
-	}
-
-	flags = 1;
-	rc = setsockopt(fd, SOL_SOCKET, SO_SELECT_ERR_QUEUE,
-			&flags, sizeof(flags));
-	if (rc < 0) {
-		perror("SO_SELECT_ERR_QUEUE failed");
-		return rc;
-	}
-
-	return 0;
-}
-
-int sk_receive(int fd, void *buf, int buflen, struct isochron_timestamp *tstamp,
-	       int flags, int timeout)
-{
-	struct iovec iov = { buf, buflen };
-	struct timespec *ts;
-	struct cmsghdr *cm;
-	struct msghdr msg;
-	char control[256];
-	ssize_t len;
-	int rc = 0;
-
-	memset(control, 0, sizeof(control));
-	memset(&msg, 0, sizeof(msg));
-	msg.msg_iov = &iov;
-	msg.msg_iovlen = 1;
-	msg.msg_control = control;
-	msg.msg_controllen = sizeof(control);
-
-	if (flags == MSG_ERRQUEUE) {
-		struct pollfd pfd = { fd, POLLPRI, 0 };
-
-		rc = poll(&pfd, 1, timeout);
-		if (rc == 0) {
-			return 0;
-		} else if (rc < 0) {
-			perror("poll for tx timestamp failed");
-			return rc;
-		} else if (!(pfd.revents & POLLPRI)) {
-			fprintf(stderr, "poll woke up on non ERR event\n");
-			return -1;
-		}
-		/* On success a positive number is returned */
-	}
-
-	len = recvmsg(fd, &msg, flags);
-	/* Suppress "Interrupted system call" message */
-	if (len < 1 && errno != EINTR)
-		perror("recvmsg failed");
-
-	for (cm = CMSG_FIRSTHDR(&msg); cm != NULL; cm = CMSG_NXTHDR(&msg, cm)) {
-		int level = cm->cmsg_level;
-		int type  = cm->cmsg_type;
-
-		if (level == SOL_SOCKET && type == SCM_TIMESTAMPING) {
-			struct scm_timestamping *tss;
-
-			if (cm->cmsg_len < sizeof(*ts) * 3) {
-				fprintf(stderr, "short SO_TIMESTAMPING message\n");
-				return -1;
-			}
-
-			tss = (struct scm_timestamping *)CMSG_DATA(cm);
-
-			if (tstamp) {
-				tstamp->sw = tss->ts[0];
-				tstamp->hw = tss->ts[2];
-			}
-		} else if ((level == SOL_PACKET && type == PACKET_TX_TIMESTAMP) ||
-			   (level == SOL_IP && type == IP_RECVERR) ||
-			   (level == SOL_IPV6 && type == IPV6_RECVERR)) {
-			struct sock_extended_err *sock_err;
-			char txtime_buf[TIMESPEC_BUFSIZ];
-			__u64 txtime;
-
-			sock_err = (struct sock_extended_err *)CMSG_DATA(cm);
-			if (!sock_err)
-				continue;
-
-			switch (sock_err->ee_origin) {
-			case SO_EE_ORIGIN_TIMESTAMPING:
-				if (!tstamp)
-					break;
-
-				tstamp->tskey = sock_err->ee_data;
-				tstamp->tstype = sock_err->ee_info;
-				break;
-			case SO_EE_ORIGIN_TXTIME:
-				txtime = ((__u64)sock_err->ee_data << 32) +
-					 sock_err->ee_info;
-
-				if (tstamp)
-					tstamp->txtime = ns_to_timespec(txtime);
-
-				ns_sprintf(txtime_buf, txtime);
-
-				switch (sock_err->ee_code) {
-				case SO_EE_CODE_TXTIME_INVALID_PARAM:
-					fprintf(stderr,
-						"packet with txtime %s dropped due to invalid params\n",
-						txtime_buf);
-					break;
-				case SO_EE_CODE_TXTIME_MISSED:
-					fprintf(stderr,
-						"packet with txtime %s dropped due to missed deadline\n",
-						txtime_buf);
-					break;
-				default:
-					return -1;
-				}
-				break;
-			default:
-				pr_err(-sock_err->ee_errno,
-				       "unknown socket error %d, origin %d code %d: %m\n",
-				       sock_err->ee_errno, sock_err->ee_origin,
-				       sock_err->ee_code);
-				break;
-			}
-		} else {
-			fprintf(stderr, "unknown cmsg level %d type %d\n",
-				level, type);
-		}
-	}
-
-	return len;
 }
 
 static const char * const trace_marker_paths[] = {

src/common.h
@@ -138,22 +138,9 @@ struct vlan_ethhdr {
 #define VLAN_VID_MASK		0x0fff /* VLAN Identifier */
 #define VLAN_N_VID		4096
 
-struct isochron_timestamp {
-	struct timespec hw;
-	struct timespec sw;
-	struct timespec sched;
-	struct timespec txtime;
-	__u32 tskey;
-	__u32 tstype;
-};
-
 ssize_t read_exact(int fd, void *buf, size_t count);
 ssize_t write_exact(int fd, const void *buf, size_t count);
 
-int sk_validate_ts_info(const char *if_name);
-int sk_timestamping_init(int fd, const char *if_name, bool on);
-int sk_receive(int fd, void *buf, int buflen, struct isochron_timestamp *tstamp,
-	       int flags, int timeout);
 __s64 timespec_to_ns(const struct timespec *ts);
 struct timespec ns_to_timespec(__s64 ns);
 void mac_addr_sprintf(char *buf, unsigned char *addr);

src/daemon.c
@@ -63,9 +63,9 @@ static int prog_prepare_session(struct isochron_send *send)
 	if (rc)
 		return rc;
 
-	rc = isochron_send_init_data_fd(send);
+	rc = isochron_send_init_data_sock(send);
 	if (rc)
-		goto err_init_data_fd;
+		goto err_init_data_sock;
 
 	isochron_send_init_data_packet(send);
 
@@ -94,8 +94,8 @@ err_start_threads:
 err_update_session_start:
 	isochron_log_teardown(&send->log);
 err_log_init:
-	isochron_send_teardown_data_fd(send);
-err_init_data_fd:
+	isochron_send_teardown_data_sock(send);
+err_init_data_sock:
 	return rc;
 }
 
@@ -109,7 +109,7 @@ static void isochron_teardown_sender(struct isochron_daemon *prog)
 	if (prog->test_running) {
 		isochron_send_stop_threads(send);
 		isochron_log_teardown(&send->log);
-		isochron_send_teardown_data_fd(send);
+		isochron_send_teardown_data_sock(send);
 		prog->test_running = false;
 	}
 

commit e08423529dea4e0a4a814f7ff2ab97167dfe1285

sk: fix invalid cmsg_level and cmsg_len in sk_msg_add_cmsg
This is a copy-paste dud which results in SO_TXTIME cmsgs returning
-EINVAL when we try to send them.

Fixes: fb5c44fad600 ("fix IPv4 data socket getting broken when CONFIG_IPV6=n")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

src/sk.c
@@ -421,7 +421,7 @@ struct cmsghdr *sk_msg_add_cmsg(struct sk_msg *msg, int level, int type,
 	cmsg = CMSG_FIRSTHDR(&msg->msghdr);
 	cmsg->cmsg_level = level;
 	cmsg->cmsg_type = type;
-	cmsg->cmsg_level = len;
+	cmsg->cmsg_len = len;
 
 	return cmsg;
 }